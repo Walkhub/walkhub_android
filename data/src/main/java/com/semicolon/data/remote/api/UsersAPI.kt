b35eec96bc46825547482f765296874e11ae2312
임세현 <sh00710000@gmail.com>
2022-01-06 00:24:02 +0900

    ⚡️ :: update retrofit interface

--- data/src/main/java/com/semicolon/data/remote/api/UsersAPI.kt
+++ data/src/main/java/com/semicolon/data/remote/api/UsersAPI.kt
@@ -1,27 +1,95 @@
 package com.semicolon.data.remote.api
 
-import com.semicolon.data.remote.request.VerifyRequest
-import retrofit2.http.Body
-import retrofit2.http.GET
+import com.semicolon.data.remote.request.users.*
+import com.semicolon.data.remote.request.users.inquirymypage.InquiryMypageResponse
+import com.semicolon.data.remote.request.users.inquiryownerbadges.InquiryOwnerBadgesResponse
+import com.semicolon.data.remote.request.users.userInquiryProfile.UserInquiryProfileResponse
+import retrofit2.http.*
 
 interface UsersAPI {
 
     // 전화번호 인증(회원가입)
-    @GET("users/verify")
-    suspend fun verifyPhoneNumber(
-        @Body verifyRequest: VerifyRequest
+    @POST("users/signup/verification-codes")
+    suspend fun verifyPhoneNumberRegister(
+        @Body verifyPhoneNumberRegisterRequest: VerifyPhoneNumberRegisterRequest
     ) : Void
 
     // 전화번호 인증(패스워드)
+    @POST("users/passwords/verification-codes")
+    suspend fun verifyPhoneNumberPassword(
+        @Body verifyPhoneNumberPasswordRequest: VerifyPhoneNumberPasswordRequest
+    ) : Void
 
     // 유저 회원가입
+    @POST("users")
+    suspend fun userRegister(
+        @Body userRegisterReqeust: UserRegisterRequest
+    ) : UserRegisterResponse
 
     // 유저 로그인
+    @POST("users/auth")
+    suspend fun userLogin(
+        @Body userLoginRequest: UserLoginRequest
+    ) : UserLoginResponse
 
     // 토큰 재발급
+    @PUT("users/reissue")
+    suspend fun userReissue(
+        @Header("x-refresh-token") refreshToken: String
+    ) : UserReissueResponse
 
     // 유저 비밀번호 변경
+    @PATCH("users/password")
+    suspend fun userChangePassword(
+        @Body userChangePasswordRequest: UserChangePasswordRequest
+    ) : Void
 
-    //  유저 프로필 조회
+
+    // 유저 프로필 조회
+    @GET("users/{user-id}")
+    suspend fun userInquiryProfile(
+        @Header("Authorization") accessToken: String,
+        @Path("user-id") userId: Int
+    ) : UserInquiryProfileResponse
+
+    // 마이 페이지 조회
+    @GET("users")
+    suspend fun inquiryMypage(
+        @Header("Authorization") accessToken: String
+    ) : InquiryMypageResponse
+
+    // 소유한 뱃지 목록 조회
+    @GET("user/{user-id}/badges")
+    suspend fun inquiryOwnerBadges(
+        @Header("Authorization") accessToken: String,
+        @Path("user-id") userId: Int
+    ) : InquiryOwnerBadgesResponse
+
+    // 대표 뱃지 설정
+    @PUT("users/badges/{badge-id}")
+    suspend fun settingRepresentativeBadge(
+        @Header("Authorization") accessToken: String,
+        @Path("badge-id") badgeId: Int
+    ) : Void
+
+    // 내 정보 수정
+    @PATCH("users")
+    suspend fun updateProfile(
+        @Header("Authorization") accessToken: String,
+        @Body updateProfileRequest: UpdateProfileRequest
+    ) : Void
+
+    // 유저 아이디 찾기
+    @GET("users/accounts/{phone-number}")
+    suspend fun findUserAccounts(
+        @Path("phone_number") phoneNumber: String
+    ) : FindUserAccountsResponse
+
+    // 건강 정보 입력
+    @PUT("users/health")
+    suspend fun inputHealth(
+        @Header("Authorization") accessToken: String,
+        @Body inputHealthRequest: InputHealthRequest
+    ) : Void
 
 }
