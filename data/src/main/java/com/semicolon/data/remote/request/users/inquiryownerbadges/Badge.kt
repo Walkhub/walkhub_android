d444f35797ca8e2f13525a383df30fe8229838f6
임세현 <sh00710000@gmail.com>
2022-01-06 00:20:05 +0900

    📑 :: inquriy owner badges response data class

---
+++ data/src/main/java/com/semicolon/data/remote/request/users/inquiryownerbadges/Badge.kt
@@ -0,0 +1,7 @@
+package com.semicolon.data.remote.request.users.inquiryownerbadges
+
+data class Badge(
+    val id: Int,
+    val image: String,
+    val name: String
+)
---
+++ data/src/main/java/com/semicolon/data/remote/request/users/inquiryownerbadges/InquiryOwnerBadgesResponse.kt
@@ -0,0 +1,5 @@
+package com.semicolon.data.remote.request.users.inquiryownerbadges
+
+data class InquiryOwnerBadgesResponse(
+    val badge_list: List<Badge>
+)
