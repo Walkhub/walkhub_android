041beee1b6d9f6a865bf7b8e2eb5d0746c6841a6
유현명 <aahspringaa4@naver.com>
2022-01-06 17:17:37 +0900

    📑 :: notice detail response data class

---
+++ data/src/main/java/com/semicolon/data/remote/response/notice/noticedetail/NoticeDetailResponse.kt
@@ -0,0 +1,11 @@
+package com.semicolon.data.remote.response.notice.noticedetail
+
+import com.google.gson.annotations.SerializedName
+
+data class NoticeDetailResponse(
+    @SerializedName("title") val title: String,
+    @SerializedName("content") val content: String,
+    @SerializedName("created_at") val createdAt: String,
+    @SerializedName("isMine") val isMine: Boolean,
+    @SerializedName("writer") val noticeWriter: NoticeDetailWriter
+)
