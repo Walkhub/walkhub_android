7a3f70f1f0470d0247f8261931dea7686ec97618
Jaewon <turitus@naver.com>
2022-01-06 23:14:14 +0900

    ⚡️:: update challenge detail api

--- data/src/main/java/com/semicolon/data/remote/api/ChallengeApi.kt
+++ data/src/main/java/com/semicolon/data/remote/api/ChallengeApi.kt
@@ -1,8 +1,13 @@
 package com.semicolon.data.remote.api
 
+import com.semicolon.data.remote.response.challenge.ChallengeListResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ChallengeApi {
     @GET("challenges/lists")
-    suspend fun getChallenges()
+    suspend fun getChallenges(): ChallengeListResponse
+
+    @GET("challenges/{challenge-id}")
+    suspend fun getChallengeDetail(@Path("challenge-id") challengeId: Long)
 }
