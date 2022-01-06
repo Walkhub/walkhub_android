5961d9f086c47ea3e2e95a19eb3043f41395b505
Leesin0222 <dragon02229@naver.com>
2022-01-06 22:12:09 +0900

    ⚰️ :: Delete Dataclass in issuing the teacher's authentication code

--- data/src/main/java/com/semicolon/data/remote/response/IssuingTeacherAuthCode.kt
+++
@@ -1,7 +0,0 @@
-package com.semicolon.data.remote.response
-
-import com.google.gson.annotations.SerializedName
-
-data class IssuingTeacherAuthCode(
-    @SerializedName("code") val code: String
-)
