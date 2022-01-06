7efc1a0d6ee914d59a33442426f85dbd574f09a9
Leesin0222 <dragon02229@naver.com>
2022-01-06 12:53:42 +0900

    📑 :: School Search Data class

---
+++ data/src/main/java/com/semicolon/data/remote/response/SchoolX.kt
@@ -0,0 +1,12 @@
+package com.semicolon.data.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+//search school
+data class SchoolX(
+    @SerializedName("agency_code") val agencyCode: String,
+    @SerializedName("logo_image_url") val logoImageUrl: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("rank") val rank : Int,
+    @SerializedName("walk_count") val walkCount: Int
+)
---
+++ data/src/main/java/com/semicolon/data/remote/response/SearchSchool.kt
@@ -0,0 +1,7 @@
+package com.semicolon.data.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class SearchSchool(
+    @SerializedName("school_list") val schoolList: List<SchoolX>
+)
