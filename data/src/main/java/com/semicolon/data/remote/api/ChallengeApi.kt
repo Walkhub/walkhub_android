c66e1456fb935b81853873d43edc89d2dfdfcda7
Jaewon <turitus@naver.com>
2022-01-06 23:24:20 +0900

    ⚡️:: participate challenge api

--- data/src/main/java/com/semicolon/data/remote/api/ChallengeApi.kt
+++ data/src/main/java/com/semicolon/data/remote/api/ChallengeApi.kt
@@ -3,6 +3,7 @@ package com.semicolon.data.remote.api
 import com.semicolon.data.remote.response.challenge.ChallengeDetailResponse
 import com.semicolon.data.remote.response.challenge.ChallengeListResponse
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface ChallengeApi {
@@ -11,4 +12,7 @@ interface ChallengeApi {
 
     @GET("challenges/{challenge-id}")
     suspend fun getChallengeDetail(@Path("challenge-id") challengeId: Long): ChallengeDetailResponse
+
+    @POST("challenges/{challenge-id}")
+    suspend fun postParticipateChallenge(@Path("challenge-id") challengeId: Long)
 }
