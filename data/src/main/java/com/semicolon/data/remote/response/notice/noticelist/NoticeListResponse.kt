e263777e21a3bab4e5a4daab451cebd61efdea76
유현명 <aahspringaa4@naver.com>
2022-01-06 17:15:27 +0900

    📑 :: notice list response data class

---
+++ data/src/main/java/com/semicolon/data/remote/response/notice/noticelist/NoticeListResponse.kt
@@ -0,0 +1,7 @@
+package com.semicolon.data.remote.response.notice.noticelist
+
+import com.google.gson.annotations.SerializedName
+
+data class NoticeListResponse(
+    @SerializedName("notice_list") val noticeList: NoticeListValue
+)
