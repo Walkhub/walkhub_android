191c84b6c4435232feaaa6f2735e508bb29ceaa8
유현명 <aahspringaa4@naver.com>
2022-01-07 14:19:26 +0900

    ♻️ :: 공지사항 리스트 중첩 클래스 사용

--- data/src/main/java/com/semicolon/data/remote/response/notice/NoticeListResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.semicolon.data.remote.response.notice
-
-import com.google.gson.annotations.SerializedName
-
-data class NoticeListResponse(
-    @SerializedName("notice_list") val noticeList: List<NoticeListValue>
-)
