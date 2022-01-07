4238cfbd8df2e3384a6f57a769163157190469aa
유현명 <aahspringaa4@naver.com>
2022-01-07 13:19:33 +0900

    ⚡️ :: 공지사항 리스트 값 List 형식으로 변경

--- data/src/main/java/com/semicolon/data/remote/response/notice/noticelist/NoticeListResponse.kt
+++ data/src/main/java/com/semicolon/data/remote/response/notice/noticelist/NoticeListResponse.kt
@@ -3,5 +3,5 @@ package com.semicolon.data.remote.response.notice.noticelist
 import com.google.gson.annotations.SerializedName
 
 data class NoticeListResponse(
-    @SerializedName("notice_list") val noticeList: NoticeListValue
+    @SerializedName("notice_list") val noticeList: List<NoticeListValue>
 )
