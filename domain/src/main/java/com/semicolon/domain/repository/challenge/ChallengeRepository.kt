9a17b94a07ef0cea0ce1d27a1c50994a7b9a9ed5
Jaewon <turitus@naver.com>
2022-01-07 15:29:24 +0900

    ⚡️:: get challenge detail usecase

--- domain/src/main/java/com/semicolon/domain/repository/challenge/ChallengeRepository.kt
+++ domain/src/main/java/com/semicolon/domain/repository/challenge/ChallengeRepository.kt
@@ -1,7 +1,10 @@
 package com.semicolon.domain.repository.challenge
 
 import com.semicolon.domain.entity.challenge.Challenge
+import com.semicolon.domain.entity.challenge.ChallengeDetail
 
 interface ChallengeRepository {
     suspend fun fetchChallenges(): List<Challenge>
+
+    suspend fun fetchChallengeDetail(id: Int): ChallengeDetail
 }
