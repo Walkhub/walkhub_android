06eefbdfd9f518f1a589bec5e235a9c73e84a257
유현명 <aahspringaa4@naver.com>
2022-01-07 14:39:07 +0900

    ⚰️ :: isMine 필요없음

--- data/src/main/java/com/semicolon/data/remote/response/notice/NoticeDetailResponse.kt
+++ data/src/main/java/com/semicolon/data/remote/response/notice/NoticeDetailResponse.kt
@@ -6,6 +6,5 @@ data class NoticeDetailResponse(
     @SerializedName("title") val title: String,
     @SerializedName("content") val content: String,
     @SerializedName("created_at") val createdAt: String,
-    @SerializedName("isMine") val isMine: Boolean,
     @SerializedName("writer") val noticeWriter: NoticeWriter
 )
