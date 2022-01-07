7225586aec5f3bb3adc4ac2d79618a3b9e853126
Jaewon <turitus@naver.com>
2022-01-08 00:50:08 +0900

    ⚡️:: cache util

--- data/src/main/java/com/semicolon/data/repository/challenge/ChallengeRepositoryImpl.kt
+++ data/src/main/java/com/semicolon/data/repository/challenge/ChallengeRepositoryImpl.kt
@@ -7,17 +7,21 @@ import com.semicolon.domain.entity.challenge.Challenge
 import com.semicolon.domain.entity.challenge.ChallengeDetail
 import com.semicolon.domain.entity.challenge.ChallengeParticipant
 import com.semicolon.domain.repository.challenge.ChallengeRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class ChallengeRepositoryImpl @Inject constructor(
     private val challengeLocalDataSource: ChallengeLocalDataSource,
     private val challengeRemoteDateSource: ChallengeRemoteDateSource
 ): ChallengeRepository {
-    override suspend fun fetchChallenges(): Flow<List<Challenge>> =
+    override suspend fun fetchChallenges(): Flow<List<Challenge>> = withContext(Dispatchers.IO) {
         OfflineCacheUtil<List<Challenge>>()
             .remoteData { challengeRemoteDateSource.fetchChallenges() }
             .createFlow()
+    }
+
 
 
     override suspend fun fetchChallengeDetail(id: Int): ChallengeDetail {
