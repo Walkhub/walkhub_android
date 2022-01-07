4ddee3ed902bd8fbbf2da5f6c9708bd105aea225
유현명 <aahspringaa4@naver.com>
2022-01-07 13:18:44 +0900

    ♻️ :: 불필요한 띄어쓰기 & 미사용 Import 삭제

--- data/src/main/java/com/semicolon/data/remote/api/NoticesApi.kt
+++ data/src/main/java/com/semicolon/data/remote/api/NoticesApi.kt
@@ -1,6 +1,5 @@
 package com.semicolon.data.remote.api
 
-import com.semicolon.data.remote.request.notice.CreateNoticeRequest
 import com.semicolon.data.remote.response.notice.noticedetail.NoticeDetailResponse
 import com.semicolon.data.remote.response.notice.noticelist.NoticeListResponse
 import retrofit2.http.Body
@@ -9,11 +8,8 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface NoticesApi {
-
     @GET("/notices/list")
-    suspend fun loadNoticeList(
-
-    ): NoticeListResponse
+    suspend fun loadNoticeList(): NoticeListResponse
 
     @GET("/notices/{notice-id}")
     suspend fun loadNoticeDetail(
