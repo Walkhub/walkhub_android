4263e3658971a8e3cee9ea51e3f619010436621d
minjun8946 <minjun8946@daum.net>
2022-01-09 03:56:59 +0900

    ⚡️:: add response

--- data/src/main/java/com/semicolon/data/remote/api/ImagesApi.kt
+++ data/src/main/java/com/semicolon/data/remote/api/ImagesApi.kt
@@ -1,17 +1,16 @@
 package com.semicolon.data.remote.api
 
-import android.media.Image
+import com.semicolon.data.remote.response.images.ImagesResponse
 import okhttp3.MultipartBody
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
 
 interface ImagesApi {
-
     @Multipart
     @POST("/images")
     suspend fun postImages(
-         @Part images : List<MultipartBody.Part>
-    )
+        @Part images: List<MultipartBody.Part>
+    ): ImagesResponse
 
 }
