03b858266a27ac95926ba9486bed2ca891e85bea
유현명 <aahspringaa4@naver.com>
2022-01-10 08:58:31 +0900

    ♻️ :: Notice Class 이름 수정

--- data/src/main/java/com/semicolon/data/remote/response/notice/NoticeListResponse.kt
+++ data/src/main/java/com/semicolon/data/remote/response/notice/NoticeListResponse.kt
@@ -2,7 +2,7 @@ package com.semicolon.data.remote.response.notice
 
 import com.google.gson.annotations.SerializedName
 
-data class Notice(
+data class NoticeListResponse(
     @SerializedName("notice_list") val noticeList: List<NoticeListValue>
 
 ){
