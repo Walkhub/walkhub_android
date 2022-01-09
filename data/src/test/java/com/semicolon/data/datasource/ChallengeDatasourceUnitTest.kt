e737916f5cfdabd8ada1ca4a8ccb5215b9ec0d69
Jaewon <turitus@naver.com>
2022-01-09 19:32:04 +0900

    📑:: challenge datasource test file

---
+++ data/src/test/java/com/semicolon/data/datasource/ChallengeDatasourceUnitTest.kt
@@ -0,0 +1,10 @@
+package com.semicolon.data.datasource
+
+import com.semicolon.data.remote.api.ChallengeApi
+import org.mockito.Mock
+
+class ChallengeDatasourceUnitTest {
+    @Mock
+    private lateinit var challengeApi: ChallengeApi
+    
+}
