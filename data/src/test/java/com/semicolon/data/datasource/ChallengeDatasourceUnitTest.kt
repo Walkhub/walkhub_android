a54a1a64e7043e8e6b4151c77f0080731ec1f504
Jaewon <turitus@naver.com>
2022-01-09 20:13:23 +0900

    🧪:: challenge data source remote test

--- data/src/test/java/com/semicolon/data/datasource/ChallengeDatasourceUnitTest.kt
+++ data/src/test/java/com/semicolon/data/datasource/ChallengeDatasourceUnitTest.kt
@@ -1,13 +1,22 @@
 package com.semicolon.data.datasource
 
+import com.google.gson.annotations.SerializedName
+import com.semicolon.data.BaseTest
 import com.semicolon.data.datasource.challenge.local.ChallengeLocalDataSource
+import com.semicolon.data.datasource.challenge.local.ChallengeLocalDataSourceImpl
 import com.semicolon.data.datasource.challenge.remote.ChallengeRemoteDateSource
+import com.semicolon.data.datasource.challenge.remote.ChallengeRemoteDateSourceImpl
 import com.semicolon.data.local.database.challenge.ChallengeDao
 import com.semicolon.data.remote.api.ChallengeApi
-import org.junit.Before
+import com.semicolon.data.remote.response.challenge.ChallengeListResponse
+import com.semicolon.data.remote.response.challenge.toEntity
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import java.time.LocalDateTime
 
-class ChallengeDatasourceUnitTest {
+class ChallengeDatasourceUnitTest : BaseTest() {
     @Mock
     private lateinit var challengeApi: ChallengeApi
 
@@ -18,8 +27,39 @@ class ChallengeDatasourceUnitTest {
 
     private lateinit var challengeLocalDataSource: ChallengeLocalDataSource
 
-    @Before
-    fun init() {
+    override fun init() {
+        challengeRemoteDataSource = ChallengeRemoteDateSourceImpl(challengeApi)
+        challengeLocalDataSource = ChallengeLocalDataSourceImpl(challengeDao)
+    }
 
+    @Test
+    fun `RemoteDatasource 챌린지 가저오기 성공`() {
+        runBlocking {
+            val challengeListMock = ArrayList<ChallengeListResponse.ChallengeResponse>().apply {
+                add(
+                    ChallengeListResponse.ChallengeResponse(
+                        12,
+                        "도전!",
+                        LocalDateTime.MIN,
+                        LocalDateTime.MAX,
+                        "https://image",
+                        "ALL"
+                    )
+                )
+            }
+            val challengeResponseMock = ChallengeListResponse(challengeListMock)
+            `when`(challengeApi.getChallenges())
+                .thenReturn(challengeResponseMock)
+
+            val testValue = challengeRemoteDataSource.fetchChallenges()
+            assert(testValue == challengeResponseMock.toEntity())
+        }
+    }
+
+    @Test
+    fun `LocalDatasource 챌린지 가져오기 성공`() {
+        runBlocking {
+
+        }
     }
 }
