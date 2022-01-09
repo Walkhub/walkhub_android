94bd34ab0d73ada693c36cf13818c716abdf04e4
Jaewon <turitus@naver.com>
2022-01-09 19:43:03 +0900

    📑:: walk hub database

---
+++ data/src/main/java/com/semicolon/data/local/datasource/WalkHubDataBase.kt
@@ -0,0 +1,8 @@
+package com.semicolon.data.local.datasource
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [], version = 1)
+abstract class WalkHubDataBase : RoomDatabase(){
+}
