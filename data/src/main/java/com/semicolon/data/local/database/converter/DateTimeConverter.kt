6dc6fee72584c80a29ea3a01f56a89237f70dbf1
Jaewon <turitus@naver.com>
2022-01-10 14:21:53 +0900

    ♻️:: Date time converter

---
+++ data/src/main/java/com/semicolon/data/local/database/converter/DateTimeConverter.kt
@@ -0,0 +1,21 @@
+package com.semicolon.data.local.database.converter
+
+import androidx.room.TypeConverter
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.util.*
+
+class DateTimeConverter {
+
+    @TypeConverter
+    fun dataTimeToString(localDateTime: LocalDateTime?): String? =
+        localDateTime?.toString()
+
+    @TypeConverter
+    fun stringToDate(string: String?): Date? =
+        if (string != null) {
+            SimpleDateFormat("yyyy-MM-dd`T`HH-mm-ss", Locale.KOREA).parse(string)
+        } else {
+            null
+        }
+}
