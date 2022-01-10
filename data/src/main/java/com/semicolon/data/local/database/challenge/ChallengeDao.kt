9d8d4336be37d996c4550bc710da79f73bbb56c6
Jaewon <turitus@naver.com>
2022-01-10 13:24:48 +0900

    ⚡️:: challenge dao

--- data/src/main/java/com/semicolon/data/local/database/challenge/ChallengeDao.kt
+++ data/src/main/java/com/semicolon/data/local/database/challenge/ChallengeDao.kt
@@ -1,8 +1,17 @@
 package com.semicolon.data.local.database.challenge
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.semicolon.data.local.database.challenge.entity.ChallengeDatabaseEntity
 
 @Dao
 interface ChallengeDao {
 
+    @Query("SELECT * FROM challenge")
+    fun fetchChallenges(): List<ChallengeDatabaseEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun saveChallenges(challenges: List<ChallengeDatabaseEntity>)
 }
