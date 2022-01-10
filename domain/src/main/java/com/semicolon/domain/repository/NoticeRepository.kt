a577a71fb1e6d7a7a32df714d266dc9001c9efba
유현명 <aahspringaa4@naver.com>
2022-01-10 15:26:45 +0900

    📑:: Notice Repository 구현

---
+++ domain/src/main/java/com/semicolon/domain/repository/NoticeRepository.kt
@@ -0,0 +1,11 @@
+package com.semicolon.domain.repository
+
+import com.semicolon.domain.entity.notice.NoticeDetailEntity
+import com.semicolon.domain.entity.notice.NoticeEntity
+import kotlinx.coroutines.flow.Flow
+
+interface NoticeRepository{
+    suspend fun loadNoticeList(): Flow<List<NoticeEntity>>
+
+    suspend fun loadNoticeDetail(): Flow<NoticeDetailEntity>
+}
