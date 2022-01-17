984539cd9a820d0cf7328d44420d6a82e73c73d5
Leesin0222 <dragon02229@naver.com>
2022-01-17 23:41:18 +0900

    ⚡ :: RemoteRankDataSource

---
+++ data/src/main/java/com/semicolon/data/remote/datasource/RemoteRankDataSource.kt
@@ -0,0 +1,19 @@
+package com.semicolon.data.remote.datasource
+
+import com.semicolon.data.remote.response.ranks.inquiryRank.school.SchoolRankResponse
+import com.semicolon.data.remote.response.ranks.inquiryRank.user.UserRankResponse
+import com.semicolon.data.remote.response.ranks.search.school.SearchSchoolResponse
+import com.semicolon.data.remote.response.ranks.search.user.SearchUserResponse
+import retrofit2.http.Query
+
+
+interface RemoteCheckRank {
+
+    suspend fun fetchUserRank(scope: String, dataType: String, sort: String): UserRankResponse
+
+    suspend fun fetchSchoolRank(dateType: String, sort: String): SchoolRankResponse
+
+    suspend fun searchUser(name: String): SearchUserResponse
+
+    suspend fun searchSchool(name: String): SearchSchoolResponse
+}
