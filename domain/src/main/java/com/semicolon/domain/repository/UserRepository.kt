69a4dec1b9bdf3d08b8b2213216a5be4511864cb
임세현 <sh00710000@gmail.com>
2022-01-19 13:29:35 +0900

    ⚡️ :: sign in & sign up repository

--- domain/src/main/java/com/semicolon/domain/repository/UserRepository.kt
+++ domain/src/main/java/com/semicolon/domain/repository/UserRepository.kt
@@ -3,11 +3,21 @@ package com.semicolon.domain.repository
 import com.semicolon.domain.entity.challenge.ChallengeEntity
 import com.semicolon.domain.entity.challenge.ChallengeDetailEntity
 import com.semicolon.domain.entity.challenge.ChallengeParticipantEntity
+import com.semicolon.domain.param.user.PostUserSignInParam
+import com.semicolon.domain.param.user.PostUserSignUpParam
 import com.semicolon.domain.param.user.VerifyPhoneNumberSignUpParam
 import kotlinx.coroutines.flow.Flow
 
 interface UserRepository {
     suspend fun verifyUserPhoneNumber(
         verifyPhoneNumberSignUpParam: VerifyPhoneNumberSignUpParam
+    )
+
+    suspend fun postUserSignUp(
+        postUserSignUpParam: PostUserSignUpParam
+    )
+
+    suspend fun postUserSignIn(
+        postUserSignInParam: PostUserSignInParam
     )
 }
