2a69ceaa2493729665f91b4ceb19eb8b4292724b
임세현 <sh00710000@gmail.com>
2022-01-19 12:42:25 +0900

    🐛 :: toRequest return value

--- data/src/main/java/com/semicolon/data/repository/UserRepositoryImpl.kt
+++ data/src/main/java/com/semicolon/data/repository/UserRepositoryImpl.kt
@@ -1,15 +1,10 @@
 package com.semicolon.data.repository
 
-import com.semicolon.data.local.datasource.LocalChallengeDataSource
 import com.semicolon.data.local.datasource.LocalUserDataSource
 import com.semicolon.data.remote.datasource.RemoteUserDataSource
 import com.semicolon.data.remote.request.users.VerifyPhoneNumberSignUpRequest
-import com.semicolon.data.util.OfflineCacheUtil
-import com.semicolon.domain.entity.challenge.ChallengeParticipantEntity
 import com.semicolon.domain.param.user.VerifyPhoneNumberSignUpParam
 import com.semicolon.domain.repository.UserRepository
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
@@ -21,9 +16,9 @@ class UserRepositoryImpl @Inject constructor(
         verifyPhoneNumberSignUpParam: VerifyPhoneNumberSignUpParam
     ) = remoteChallengeDateSource.verifyUserPhoneNumber(verifyPhoneNumberSignUpParam.toRequest())
 
-    private fun VerifyPhoneNumberSignUpParam.toRequest() {
+
+    fun VerifyPhoneNumberSignUpParam.toRequest() =
         VerifyPhoneNumberSignUpRequest(
             phoneNumber = phoneNumber
         )
-    }
 }
