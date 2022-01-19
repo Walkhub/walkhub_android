61b7faf9cf364548b94d2ff9986268dec1994ea7
임세현 <sh00710000@gmail.com>
2022-01-19 12:21:09 +0900

    📑 :: verify user phonenumber usecase

---
+++ domain/src/main/java/com/semicolon/domain/usecase/user/VerifyUserPhoneNumberUseCase.kt
@@ -0,0 +1,17 @@
+package com.semicolon.domain.usecase.user
+
+import com.semicolon.domain.param.user.VerifyPhoneNumberSignUpParam
+import com.semicolon.domain.repository.ChallengeRepository
+import com.semicolon.domain.repository.UserRepository
+import com.semicolon.domain.usecase.UseCase
+import javax.inject.Inject
+
+class VerifyUserPhoneNumberUseCase @Inject constructor(
+    private val userRepository: UserRepository
+): UseCase<VerifyPhoneNumberSignUpParam, Unit>() {
+
+    override suspend fun execute(data: VerifyPhoneNumberSignUpParam) {
+        userRepository.verifyUserPhoneNumber(data)
+    }
+
+}
