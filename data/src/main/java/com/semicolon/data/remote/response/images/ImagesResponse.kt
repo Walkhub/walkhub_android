b73407db2392c9ed8be18c367868a54dcdbcaf93
minjun8946 <minjun8946@daum.net>
2022-01-20 02:49:01 +0900

    ⚰️:: package name change

--- data/src/main/java/com/semicolon/data/remote/response/images/ImagesResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.semicolon.data.remote.response.images
-
-import com.google.gson.annotations.SerializedName
-
-data class ImagesResponse(
-    @SerializedName("image_url") val imageUrl: List<String>
-)
