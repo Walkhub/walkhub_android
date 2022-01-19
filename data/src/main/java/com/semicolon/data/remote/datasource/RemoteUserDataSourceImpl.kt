a4f8832531e1737ef8ff76e9aff7f0fae4ca06d3
임세현 <sh00710000@gmail.com>
2022-01-19 12:22:59 +0900

    📑 :: remote user datasource impl

---
+++ data/src/main/java/com/semicolon/data/remote/datasource/RemoteUserDataSourceImpl.kt
@@ -0,0 +1,17 @@
+package com.semicolon.data.remote.datasource
+
+import com.semicolon.data.remote.api.UserApi
+import com.semicolon.data.remote.request.users.VerifyPhoneNumberSignUpRequest
+import com.semicolon.data.util.HttpHandler
+import javax.inject.Inject
+
+class RemoteUserDataSourceImpl @Inject constructor(
+    private val userApi: UserApi
+) : RemoteUserDataSource {
+
+    override suspend fun verifyUserPhoneNumber(
+        verifyPhoneNumberSignUpRequest: VerifyPhoneNumberSignUpRequest
+    ) = HttpHandler<Unit>()
+        .httpRequest { userApi.verifyPhoneNumberSignUp(verifyPhoneNumberSignUpRequest) }
+        .sendRequest()
+}
