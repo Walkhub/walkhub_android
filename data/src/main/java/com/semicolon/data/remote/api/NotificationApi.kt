59a30fee994f39b14cb6f6fdc3540a47dfab4a1e
유현명 <aahspringaa4@naver.com>
2022-01-20 14:53:05 +0900

    📑:: 알림 리스트 Api Interface 구현

---
+++ data/src/main/java/com/semicolon/data/remote/api/NotificationApi.kt
@@ -0,0 +1,14 @@
+package com.semicolon.data.remote.api
+
+import com.semicolon.data.remote.response.notification.NotificationListResponse
+import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.Path
+
+interface NotificationApi {
+    @GET("/notifications")
+    suspend fun loadNotifications(): NotificationListResponse
+
+    @PATCH("/notifications/{notification-id}")
+    suspend fun sendReadNotification(@Path("notification-id") notificationId: Int)
+}
