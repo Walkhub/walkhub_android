a482b391a0be0574586d220fe60b15c0fddcaf14
shw0471 <shw0471@naver.com>
2022-01-22 18:34:17 +0900

    ⚡️ :: update LocalExerciseDataSourceImpl class

---
+++ data/src/main/java/com/semicolon/data/local/datasource/LocalExerciseDataSourceImpl.kt
@@ -0,0 +1,67 @@
+package com.semicolon.data.local.datasource
+
+import com.google.android.gms.fitness.data.DataType
+import com.google.android.gms.fitness.data.Field
+import com.semicolon.data.local.entity.exercise.LocationRecordEntity
+import com.semicolon.data.local.entity.exercise.WalkRecordEntity
+import com.semicolon.data.local.storage.FitnessDataStorage
+import com.semicolon.domain.entity.exercise.DailyExerciseEntity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class LocalExerciseDataSourceImpl @Inject constructor(
+    private val fitnessDataStorage: FitnessDataStorage
+) : LocalExerciseDataSource {
+
+    override suspend fun fetchDailyExerciseRecordAsFlow(): Flow<DailyExerciseEntity> =
+        flow {
+            repeat(Int.MAX_VALUE) {
+                delay(1000)
+                val startTime: Long = 0 // TODO("나중에 할거야")
+                val endTime: Long = 0 // TODO("이거도 나중에 할거야")
+                val data = fitnessDataStorage.fetchExerciseRecord(
+                    startTimeAsMilli = startTime,
+                    endTimeAsMilli = endTime
+                )
+                emit(
+                    DailyExerciseEntity(
+                        stepCount = data.result.getDataSet(DataType.AGGREGATE_STEP_COUNT_DELTA).dataPoints[0]
+                            .getValue(Field.FIELD_STEPS).asInt(),
+                        exerciseTimeAsMilli = data.result.getDataSet(DataType.AGGREGATE_STEP_COUNT_DELTA).dataPoints[0]
+                            .getValue(Field.FIELD_DURATION).asInt().toLong() * 1000 * 60,
+                        traveledDistanceAsMeter = data.result.getDataSet(DataType.AGGREGATE_STEP_COUNT_DELTA).dataPoints[0]
+                            .getValue(Field.FIELD_DISTANCE).asInt(),
+                        burnedKilocalories = data.result.getDataSet(DataType.AGGREGATE_STEP_COUNT_DELTA).dataPoints[0]
+                            .getValue(Field.FIELD_CALORIES).asFloat()
+                    )
+                )
+            }
+        }
+
+    override suspend fun fetchLocationRecord(
+        startTimeAsMilli: Long,
+        endTimeAsMilli: Long
+    ): List<LocationRecordEntity> {
+        val data = fitnessDataStorage.fetchLocationRecord(startTimeAsMilli, endTimeAsMilli)
+        return data.result.getDataSet(DataType.TYPE_LOCATION_SAMPLE).dataPoints.map {
+            val latitude = it.getValue(Field.FIELD_LATITUDE).asString().toDouble()
+            val longitude = it.getValue(Field.FIELD_LONGITUDE).asString().toDouble()
+            LocationRecordEntity(latitude = latitude, longitude = longitude)
+        }
+    }
+
+    override suspend fun fetchWalkRecord(
+        startTimeAsMilli: Long,
+        endTimeAsMilli: Long
+    ): WalkRecordEntity {
+        val data = fitnessDataStorage.fetchExerciseRecord(startTimeAsMilli, endTimeAsMilli)
+        return WalkRecordEntity(
+            traveledDistanceAsMeter = data.result.getDataSet(DataType.AGGREGATE_STEP_COUNT_DELTA).dataPoints[0]
+                .getValue(Field.FIELD_STEPS).asInt(),
+            walkCount = data.result.getDataSet(DataType.AGGREGATE_DISTANCE_DELTA).dataPoints[0]
+                .getValue(Field.FIELD_DISTANCE).asInt()
+        )
+    }
+}
