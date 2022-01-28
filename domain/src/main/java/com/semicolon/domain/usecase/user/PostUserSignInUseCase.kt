35458c5b430047905011ae5c318265b0aded9e9f
saehyun <sh00710000@gmail.com>
2022-01-28 19:34:45 +0900

    ⚡️ :: PostUserSignInUseCase return value

--- domain/src/main/java/com/semicolon/domain/usecase/user/PostUserSignInUseCase.kt
+++ domain/src/main/java/com/semicolon/domain/usecase/user/PostUserSignInUseCase.kt
@@ -1,16 +1,17 @@
 package com.semicolon.domain.usecase.user
 
+import com.semicolon.domain.entity.users.UserSignInEntity
 import com.semicolon.domain.param.user.PostUserSignInParam
 import com.semicolon.domain.repository.UserRepository
 import com.semicolon.domain.usecase.UseCase
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class PostUserSignInUseCase @Inject constructor(
     private val userRepository: UserRepository
-) : UseCase<PostUserSignInParam, Unit>() {
+) : UseCase<PostUserSignInParam, Flow<UserSignInEntity>>() {
 
-    override suspend fun execute(data: PostUserSignInParam) {
+    override suspend fun execute(data: PostUserSignInParam) : Flow<UserSignInEntity> =
         userRepository.postUserSignIn(data)
-    }
 
 }
