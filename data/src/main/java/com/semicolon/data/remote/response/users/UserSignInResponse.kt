001fba51ebb0be74fe167bfd93518dd4f5bab58a
saehyun <sh00710000@gmail.com>
2022-01-28 19:33:14 +0900

    ⚡️ :: UserSignInResponse toEntity

--- data/src/main/java/com/semicolon/data/remote/response/users/UserSignInResponse.kt
+++ data/src/main/java/com/semicolon/data/remote/response/users/UserSignInResponse.kt
@@ -1,9 +1,17 @@
 package com.semicolon.data.remote.response.users
 
 import com.google.gson.annotations.SerializedName
+import com.semicolon.domain.entity.users.UserSignInEntity
 
 data class UserSignInResponse(
     @SerializedName("access_token") val accessToken: String,
     @SerializedName("expired_at") val expiredAt: String,
     @SerializedName("refresh_token") val refreshToken: String
 )
+
+fun UserSignInResponse.toEntity() =
+    UserSignInEntity(
+        accessToken = accessToken,
+        expiredAt = expiredAt,
+        refreshToken = refreshToken
+    )
