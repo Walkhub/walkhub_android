6bc2bc829dec63d3a833a064eb95f307a56ed22f
saehyun <sh00710000@gmail.com>
2022-01-28 19:34:23 +0900

    ⚡️ :: sign in &  change password repository impl

--- data/src/main/java/com/semicolon/data/repository/UserRepositoryImpl.kt
+++ data/src/main/java/com/semicolon/data/repository/UserRepositoryImpl.kt
@@ -2,11 +2,18 @@ package com.semicolon.data.repository
 
 import com.semicolon.data.local.datasource.LocalUserDataSource
 import com.semicolon.data.remote.datasource.RemoteUserDataSource
+import com.semicolon.data.remote.request.users.UserChangePasswordRequest
 import com.semicolon.data.remote.request.users.UserSignInRequest
 import com.semicolon.data.remote.request.users.UserSignUpRequest
 import com.semicolon.data.remote.request.users.VerifyPhoneNumberSignUpRequest
 import com.semicolon.data.remote.response.users.UserSignInResponse
+import com.semicolon.data.remote.response.users.toEntity
+import com.semicolon.data.util.OfflineCacheUtil
+import com.semicolon.domain.entity.challenge.ChallengeDetailEntity
+import com.semicolon.domain.entity.users.UserMypageEntity
+import com.semicolon.domain.entity.users.UserProfileEntity
 import com.semicolon.domain.entity.users.UserSignInEntity
+import com.semicolon.domain.param.user.PatchUserChangePasswordParam
 import com.semicolon.domain.param.user.PostUserSignInParam
 import com.semicolon.domain.param.user.PostUserSignUpParam
 import com.semicolon.domain.param.user.VerifyPhoneNumberSignUpParam
@@ -28,6 +35,19 @@ class UserRepositoryImpl @Inject constructor(
         postUserSignUpParam: PostUserSignUpParam
     ) = remoteChallengeDateSource.postUserSignUp(postUserSignUpParam.toRequest())
 
+    override suspend fun postUserSignIn(
+        postUserSignInParam: PostUserSignInParam
+    ): Flow<UserSignInEntity> = flow {
+        emit(remoteChallengeDateSource.postUserSignIn(postUserSignInParam.toRequest()).toEntity())
+    }
+
+    override suspend fun patchUserChangePassword(
+        patchUserChangePasswordParam: PatchUserChangePasswordParam
+    ) = remoteChallengeDateSource.patchUserChangePassword(patchUserChangePasswordParam.toRequest())
+
+    override suspend fun fetchMypage(): Flow<UserMypageEntity> {
+        TODO("Not yet implemented")
+    }
 
     fun VerifyPhoneNumberSignUpParam.toRequest() =
         VerifyPhoneNumberSignUpRequest(
@@ -41,5 +61,20 @@ class UserRepositoryImpl @Inject constructor(
             name = name,
             phoneNumber = phoneNumber,
             authCode = authCode
+        )
+
+    fun PostUserSignInParam.toRequest() =
+        UserSignInRequest(
+            accountId = accountId,
+            password = password,
+            deviceToken = deviceToken
+        )
+
+    fun PatchUserChangePasswordParam.toRequest() =
+        UserChangePasswordRequest(
+            accountId = accountId,
+            phoneNumber = phoneNumber,
+            authCode = authCode,
+            newPassword = newPassword
         )
 }
