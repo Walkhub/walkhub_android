c58a4e634f0a5926a33941f8bcac7cabc18ec3b0
saehyun <sh00710000@gmail.com>
2022-01-28 19:32:19 +0900

    ⚡️ :: sign in & change password & user profile datasource impl

--- data/src/main/java/com/semicolon/data/remote/datasource/RemoteUserDataSourceImpl.kt
+++ data/src/main/java/com/semicolon/data/remote/datasource/RemoteUserDataSourceImpl.kt
@@ -1,8 +1,12 @@
 package com.semicolon.data.remote.datasource
 
 import com.semicolon.data.remote.api.UserApi
+import com.semicolon.data.remote.request.users.UserChangePasswordRequest
+import com.semicolon.data.remote.request.users.UserSignInRequest
 import com.semicolon.data.remote.request.users.UserSignUpRequest
 import com.semicolon.data.remote.request.users.VerifyPhoneNumberSignUpRequest
+import com.semicolon.data.remote.response.users.FetchUserProfileResponse
+import com.semicolon.data.remote.response.users.UserSignInResponse
 import com.semicolon.data.util.HttpHandler
 import javax.inject.Inject
 
@@ -20,5 +24,23 @@ class RemoteUserDataSourceImpl @Inject constructor(
         userSignUpRequest: UserSignUpRequest
     ) = HttpHandler<Unit>()
         .httpRequest { userApi.userSignUp(userSignUpRequest) }
+        .sendRequest()
+
+    override suspend fun postUserSignIn(
+        userSignInRequest: UserSignInRequest
+    ) = HttpHandler<UserSignInResponse>()
+        .httpRequest { userApi.userSignIn(userSignInRequest) }
+        .sendRequest()
+
+    override suspend fun patchUserChangePassword(
+        userChangePasswordRequest: UserChangePasswordRequest
+    ) = HttpHandler<Unit>()
+        .httpRequest { userApi.userChangePassword(userChangePasswordRequest) }
+        .sendRequest()
+
+    override suspend fun fetchUserProfile(
+        userId: Int
+    ) = HttpHandler<FetchUserProfileResponse>()
+        .httpRequest { userApi.fetchUserProfile(userId) }
         .sendRequest()
 }
