ba3eca0df6ba5076bb356b175fcac4acd5d96b06
saehyun <sh00710000@gmail.com>
2022-01-28 19:31:46 +0900

    ⚡️ :: sign in & change password  & user profile datasource

--- data/src/main/java/com/semicolon/data/remote/datasource/RemoteUserDataSource.kt
+++ data/src/main/java/com/semicolon/data/remote/datasource/RemoteUserDataSource.kt
@@ -1,7 +1,11 @@
 package com.semicolon.data.remote.datasource
 
+import com.semicolon.data.remote.request.users.UserChangePasswordRequest
+import com.semicolon.data.remote.request.users.UserSignInRequest
 import com.semicolon.data.remote.request.users.UserSignUpRequest
 import com.semicolon.data.remote.request.users.VerifyPhoneNumberSignUpRequest
+import com.semicolon.data.remote.response.users.UserSignInResponse
+import com.semicolon.data.remote.response.users.FetchUserProfileResponse
 
 interface RemoteUserDataSource {
     suspend fun verifyUserPhoneNumber(
@@ -11,4 +15,16 @@ interface RemoteUserDataSource {
     suspend fun postUserSignUp(
         userSignUpRequest: UserSignUpRequest
     )
+
+    suspend fun postUserSignIn(
+        userSignInRequest: UserSignInRequest
+    ) : UserSignInResponse
+
+    suspend fun patchUserChangePassword(
+        userChangePasswordRequest: UserChangePasswordRequest
+    )
+
+    suspend fun fetchUserProfile(
+        userId: Int
+    ) : FetchUserProfileResponse
 }
