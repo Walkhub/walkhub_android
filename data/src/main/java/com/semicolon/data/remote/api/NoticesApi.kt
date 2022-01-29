40878b950c8057eeb54ec0fcdfd82c077a0ddc0c
유현명 <aahspringaa4@naver.com>
2022-01-29 18:32:39 +0900

    ⚰️:: 공지사항 자세히 보기 관련 기능 삭제

--- data/src/main/java/com/semicolon/data/remote/api/NoticesApi.kt
+++ data/src/main/java/com/semicolon/data/remote/api/NoticesApi.kt
@@ -1,16 +1,9 @@
 package com.semicolon.data.remote.api
 
-import com.semicolon.data.remote.response.notice.NoticeDetailResponse
 import com.semicolon.data.remote.response.notice.NoticeListResponse
 import retrofit2.http.GET
-import retrofit2.http.Path
 
 interface NoticesApi {
     @GET("/notices/list")
     suspend fun loadNoticeList(): NoticeListResponse
-
-    @GET("/notices/{notice-id}")
-    suspend fun loadNoticeDetail(
-        @Path("notice-id") noticeId: Int
-    ): NoticeDetailResponse
 }
--- domain/src/main/java/com/semicolon/domain/repository/NoticeRepository.kt
+++ domain/src/main/java/com/semicolon/domain/repository/NoticeRepository.kt
@@ -1,11 +1,8 @@
 package com.semicolon.domain.repository
 
-import com.semicolon.domain.entity.notice.NoticeDetailEntity
 import com.semicolon.domain.entity.notice.NoticeEntity
 import kotlinx.coroutines.flow.Flow
 
 interface NoticeRepository{
     suspend fun loadNoticeList(data: Int): Flow<List<NoticeEntity>>
-
-    suspend fun loadNoticeDetail(data: Int): Flow<NoticeDetailEntity>
 }
