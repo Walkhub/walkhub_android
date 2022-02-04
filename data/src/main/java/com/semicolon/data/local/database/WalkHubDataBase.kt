49fbb027a738987b46b9a9fb886f8b08d9893607
saehyun <sh00710000@gmail.com>
2022-02-04 18:57:28 +0900

    ⚡️ :: user database

--- data/src/main/java/com/semicolon/data/local/database/WalkHubDataBase.kt
+++ data/src/main/java/com/semicolon/data/local/database/WalkHubDataBase.kt
@@ -3,14 +3,23 @@ package com.semicolon.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.semicolon.data.local.dao.ChallengeDao
+import com.semicolon.data.local.dao.UserDao
 import com.semicolon.data.local.entity.challenge.ChallengeRoomEntity
+import com.semicolon.data.local.entity.user.UserMyPageRoomEntity
+import com.semicolon.data.local.entity.user.UserOwnBadgeRoomEntity
+import com.semicolon.data.local.entity.user.UserProfileRoomEntity
 
 @Database(
     entities = [
         ChallengeRoomEntity::class,
-        ChallengeRoomEntity::class
+        ChallengeRoomEntity::class,
+        UserMyPageRoomEntity::class,
+        UserProfileRoomEntity::class,
+        UserOwnBadgeRoomEntity::class
     ], version = 1, exportSchema = false
 )
+
 abstract class WalkHubDataBase : RoomDatabase() {
     abstract fun challengeDao(): ChallengeDao
+    abstract fun userDao(): UserDao
 }
