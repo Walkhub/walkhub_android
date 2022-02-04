a9ac313438f01e59ce3e009d55b457ff09cf6a9f
saehyun <sh00710000@gmail.com>
2022-02-04 19:00:42 +0900

    ⚡️ :: fetch own badge response toEntity

--- data/src/main/java/com/semicolon/data/remote/response/users/FetchOwnBadgeResponse.kt
+++ data/src/main/java/com/semicolon/data/remote/response/users/FetchOwnBadgeResponse.kt
@@ -1,6 +1,7 @@
 package com.semicolon.data.remote.response.users
 
 import com.google.gson.annotations.SerializedName
+import com.semicolon.domain.entity.users.UserOwnBadgeEntity
 
 data class FetchOwnBadgeResponse(
     @SerializedName("badge_list") val badgeList: List<Badge>
@@ -10,4 +11,16 @@ data class FetchOwnBadgeResponse(
         @SerializedName("image") val image: String,
         @SerializedName("name") val name: String
     )
+
+    fun Badge.toEntity() =
+        UserOwnBadgeEntity.Badge(
+            id = id,
+            image = image,
+            name = name
+        )
 }
+
+fun FetchOwnBadgeResponse.toEntity() =
+    UserOwnBadgeEntity(
+        badgeList = badgeList.map { it.toEntity() }
+    )
