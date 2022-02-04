09a9d5221f709497e708197fae9ceee589023973
saehyun <sh00710000@gmail.com>
2022-02-04 18:59:20 +0900

    ⚡️ :: user own badge room entity

---
+++ data/src/main/java/com/semicolon/data/local/entity/user/UserOwnBadgeRoomEntity.kt
@@ -0,0 +1,42 @@
+package com.semicolon.data.local.entity.user
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+import com.semicolon.domain.entity.users.UserOwnBadgeEntity
+
+@Entity(tableName = "ownBadge")
+data class UserOwnBadgeRoomEntity(
+    @PrimaryKey (autoGenerate = true) val id: Int,
+    @SerializedName("badge_list") val badgeList: List<Badge>
+) {
+    data class Badge(
+        @SerializedName("id") val id: Int,
+        @SerializedName("image") val image: String,
+        @SerializedName("name") val name: String
+    )
+
+    fun Badge.toEntity() =
+        UserOwnBadgeEntity.Badge (
+            id = id,
+            image = image,
+            name = name
+        )
+}
+
+fun UserOwnBadgeRoomEntity.toEntity() =
+    UserOwnBadgeEntity(
+        badgeList = badgeList.map { it.toEntity() }
+    )
+
+fun UserOwnBadgeEntity.Badge.toDbEntity() =
+    UserOwnBadgeRoomEntity.Badge(
+        id = id,
+        image = image,
+        name = name
+    )
+
+fun UserOwnBadgeEntity.toDbEntity() =
+    UserOwnBadgeRoomEntity(
+        badgeList = badgeList.map { it.toDbEntity() }
+    )
