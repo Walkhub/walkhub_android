12a401cd53ac00d07d500d6db3366b140ec1580c
유현명 <aahspringaa4@naver.com>
2022-02-04 14:11:14 +0900

    ♻️:: Usecase data 값 변경

--- domain/src/main/java/com/semicolon/domain/usecase/notice/FetchNoticeListUseCase.kt
+++ domain/src/main/java/com/semicolon/domain/usecase/notice/FetchNoticeListUseCase.kt
@@ -1,6 +1,7 @@
 package com.semicolon.domain.usecase.notice
 
 import com.semicolon.domain.entity.notice.NoticeEntity
+import com.semicolon.domain.enum.NoticeType
 import com.semicolon.domain.repository.NoticeRepository
 import com.semicolon.domain.usecase.UseCase
 import kotlinx.coroutines.flow.Flow
@@ -9,8 +10,8 @@ import javax.inject.Inject
 
 class FetchNoticeListUseCase @Inject constructor(
     private val noticeRepository: NoticeRepository
-) : UseCase<Int, Flow<List<NoticeEntity>>>() {
+) : UseCase<NoticeType, Flow<List<NoticeEntity>>>() {
 
-    override suspend fun execute(data: Int): Flow<List<NoticeEntity>> =
+    override suspend fun execute(data: NoticeType): Flow<List<NoticeEntity>> =
         noticeRepository.loadNoticeList(data)
 }
