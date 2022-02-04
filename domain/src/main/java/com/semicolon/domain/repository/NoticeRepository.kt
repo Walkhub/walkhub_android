430ea45aeca16d287f59a34fec000b1ab07fe3aa
유현명 <aahspringaa4@naver.com>
2022-02-04 12:12:55 +0900

    ♻️:: 파라미터 값 변경

--- domain/src/main/java/com/semicolon/domain/repository/NoticeRepository.kt
+++ domain/src/main/java/com/semicolon/domain/repository/NoticeRepository.kt
@@ -1,8 +1,9 @@
 package com.semicolon.domain.repository
 
 import com.semicolon.domain.entity.notice.NoticeEntity
+import com.semicolon.domain.enum.NoticeType
 import kotlinx.coroutines.flow.Flow
 
 interface NoticeRepository{
-    suspend fun loadNoticeList(data: Int): Flow<List<NoticeEntity>>
+    suspend fun loadNoticeList(noticeType: NoticeType): Flow<List<NoticeEntity>>
 }
