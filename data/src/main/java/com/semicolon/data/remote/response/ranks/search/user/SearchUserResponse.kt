57c4c8b8c07d1fed767e5ce488e6d302816b080f
Leesin0222 <dragon02229@naver.com>
2022-02-05 16:41:17 +0900

    ⚡ :: SearchUserResponse 다시작성

--- data/src/main/java/com/semicolon/data/remote/response/ranks/search/user/SearchUserResponse.kt
+++ data/src/main/java/com/semicolon/data/remote/response/ranks/search/user/SearchUserResponse.kt
@@ -4,9 +4,10 @@ import com.google.gson.annotations.SerializedName
 
 data class SearchUserResponse(
     @SerializedName("user_list") val userList: List<UserInfo>
-
 ) {
     data class UserInfo(
+        @SerializedName("class_num") val classNum: Int,
+        @SerializedName("grade") val grade: Int,
         @SerializedName("name") val name: String,
         @SerializedName("profile_image_url") val profileImageUrl: String,
         @SerializedName("rank") val rank: Int,
