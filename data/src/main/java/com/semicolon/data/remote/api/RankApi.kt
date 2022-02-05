503f4bf827b5b7017e781a9b96f39b2e76b756ab
Leesin0222 <dragon02229@naver.com>
2022-02-05 16:42:36 +0900

    ⚡ :: 바뀐 명세에 따라 interface 수정

--- data/src/main/java/com/semicolon/data/remote/api/RankApi.kt
+++ data/src/main/java/com/semicolon/data/remote/api/RankApi.kt
@@ -1,38 +1,55 @@
 package com.semicolon.data.remote.api
 
+import com.semicolon.data.remote.response.ranks.OurSchoolUserRankResponse
 import com.semicolon.data.remote.response.ranks.inquiryRank.school.SchoolRankResponse
 import com.semicolon.data.remote.response.ranks.inquiryRank.user.UserRankResponse
 import com.semicolon.data.remote.response.ranks.search.school.SearchSchoolResponse
 import com.semicolon.data.remote.response.ranks.search.user.SearchUserResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface RankApi {
-    //유저랭킹조회
-    @GET("/ranks/users")
-    suspend fun fetchUserRank(
-        @Query("scope") scope: String,
-        @Query("dateType") dateType: String,
-        @Query("sort") sort: String
-    ): UserRankResponse
 
     //학교랭킹조회
     @GET("/ranks/schools")
     suspend fun fetchSchoolRank(
-        @Query("dateType") dateType: String,
-        @Query("sort") sort: String
+        @Query("dateType") dateType: String
     ): SchoolRankResponse
 
-    //유저검색
-    @GET("/ranks/search/users")
-    suspend fun searchUser(
-        @Query("name") name: String
-    ): SearchUserResponse
-
     //학교검색
-    @GET("/ranks/search/schools")
+    @GET("/ranks/schools/search?name={school-name}")
     suspend fun searchSchool(
         @Query("name") name: String
     ): SearchSchoolResponse
+
+    //유저랭킹조회
+    @GET("/ranks/users/{agency-code}")
+    suspend fun fetchUserRank(
+        @Path("agency-code") agencyCode: String,
+
+        @Query("scope") scope: String,
+        @Query("dateType") dateType: String
+    ): UserRankResponse
+
+    //우리학교 유저 랭킹
+    @GET("/ranks/users/my-school")
+    suspend fun fetchOurSchoolUserRank(
+        @Query("scope") scope: String,
+        @Query("dateType") dateType: String
+    ): OurSchoolUserRankResponse
+
+    //유저검색
+    @GET("/ranks/users/search")
+    suspend fun searchUser(
+        @Query("name") name: String,
+        @Query("scope") scope: String,
+        @Query("agencyCode") agencyCode: String,
+        @Query("grade") grade: Int,
+        @Query("classNum") classNum:Int
+    ): SearchUserResponse
+
+
+
 
 }
