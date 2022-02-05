7f9f18854b41efb906138bffe23f00b0e456c63e
minjun8946 <minjun8946@daum.net>
2022-02-05 23:14:31 +0900

    ⚡️:: SearchSchoolResponseEntity

--- data/src/main/java/com/semicolon/data/remote/response/school/SearchSchoolResponse.kt
+++ data/src/main/java/com/semicolon/data/remote/response/school/SearchSchoolResponse.kt
@@ -1,13 +1,24 @@
 package com.semicolon.data.remote.response.school
 
 import com.google.gson.annotations.SerializedName
+import com.semicolon.domain.entity.school.SearchSchoolEntity
 
 data class SearchSchoolResponse(
     @SerializedName("school_list") val schoolList: List<SchoolInfo>
 ) {
     data class SchoolInfo(
         @SerializedName("agency_code") val agencyCode: String,
-        @SerializedName("name") val name: String,
+        @SerializedName("school_name") val schoolName: String,
         @SerializedName("logo_image_url") val logoImageUrl: String,
     )
 }
+
+fun SearchSchoolResponse.SchoolInfo.toEntity() =
+    SearchSchoolEntity(
+        agencyCode = agencyCode,
+        schoolName = schoolName,
+        logoImageUrl = logoImageUrl
+    )
+
+fun SearchSchoolResponse.toListEntity() =
+    schoolList.map { it.toEntity() }
