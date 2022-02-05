f759926c9e1fa2cab715ef41c39d370a533e366f
Leesin0222 <dragon02229@naver.com>
2022-02-05 23:30:47 +0900

    ⚡ :: Update datasource

--- data/src/main/java/com/semicolon/data/remote/datasource/RemoteRankDataSource.kt
+++ data/src/main/java/com/semicolon/data/remote/datasource/RemoteRankDataSource.kt
@@ -1,5 +1,6 @@
 package com.semicolon.data.remote.datasource
 
+import com.semicolon.data.remote.response.ranks.OurSchoolUserRankResponse
 import com.semicolon.data.remote.response.ranks.inquiryRank.school.SchoolRankResponse
 import com.semicolon.data.remote.response.ranks.inquiryRank.user.UserRankResponse
 import com.semicolon.data.remote.response.ranks.search.school.SearchSchoolResponse
@@ -7,12 +8,15 @@ import com.semicolon.data.remote.response.ranks.search.user.SearchUserResponse
 
 
 interface RemoteRankDataSource {
-
-    suspend fun fetchUserRank(scope: String, dataType: String, sort: String): UserRankResponse
-
-    suspend fun fetchSchoolRank(dateType: String, sort: String): SchoolRankResponse
-
-    suspend fun searchUser(name: String): SearchUserResponse
+    suspend fun fetchSchoolRank(dateType: String): SchoolRankResponse
 
     suspend fun searchSchool(name: String): SearchSchoolResponse
+
+    suspend fun fetchUserRank(agencyCode: String, scope: String, dateType: String): UserRankResponse
+
+    suspend fun fetchOurSchoolUserRank(scope: String, dateType: String): OurSchoolUserRankResponse
+
+    suspend fun searchUser(name: String, scope: String, agencyCode: String, grade: Int, classNum: Int): SearchUserResponse
+
+
 }
