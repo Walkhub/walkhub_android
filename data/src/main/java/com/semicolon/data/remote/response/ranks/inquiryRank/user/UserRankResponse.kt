bc58b68898900d9d227744951c3739c65ef8b6c7
Leesin0222 <dragon02229@naver.com>
2022-02-05 16:40:15 +0900

    ⚡ :: UserRankResponse 다시 작성

--- data/src/main/java/com/semicolon/data/remote/response/ranks/inquiryRank/user/UserRankResponse.kt
+++ data/src/main/java/com/semicolon/data/remote/response/ranks/inquiryRank/user/UserRankResponse.kt
@@ -2,19 +2,12 @@ package com.semicolon.data.remote.response.ranks.inquiryRank.user
 
 import com.google.gson.annotations.SerializedName
 
-
 data class UserRankResponse(
-    @SerializedName("my_rank") val myRank: MyRank,
-    @SerializedName("rank_list") val userRankList: List<UserRank>
+    @SerializedName("rank_list") val rankList: List<UserRank>
 ) {
     data class UserRank(
-        @SerializedName("name") val name: String,
-        @SerializedName("profile_image_url") val profileImageUrl: String,
-        @SerializedName("user_id") val userId: Int,
-        @SerializedName("walk_count") val walkCount: Int
-    )
-
-    data class MyRank(
+        @SerializedName("class_num") val classNum: Int,
+        @SerializedName("grade") val grade: Int,
         @SerializedName("name") val name: String,
         @SerializedName("profile_image_url") val profileImageUrl: String,
         @SerializedName("rank") val rank: Int,
