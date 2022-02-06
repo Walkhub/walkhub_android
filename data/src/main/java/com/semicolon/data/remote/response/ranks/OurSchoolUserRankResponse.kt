c43de299f5f07a72a854ee460d3fd8f8a8bb3718
Leesin0222 <dragon02229@naver.com>
2022-02-06 21:51:41 +0900

    ♻️ :: MyRanking 제거

--- data/src/main/java/com/semicolon/data/remote/response/ranks/OurSchoolUserRankResponse.kt
+++ data/src/main/java/com/semicolon/data/remote/response/ranks/OurSchoolUserRankResponse.kt
@@ -3,20 +3,10 @@ package com.semicolon.data.remote.response.ranks
 import com.google.gson.annotations.SerializedName
 
 data class OurSchoolUserRankResponse(
-    @SerializedName("my_ranking") val myRanking: MyRanking,
+    @SerializedName("my_ranking") val myRanking: Ranking,
     @SerializedName("ranking_list")val rankingList: List<Ranking>
 )
 {
-    data class MyRanking(
-        @SerializedName("class_num")val classNum: Int,
-        @SerializedName("grade") val grade: Int,
-        @SerializedName("name") val name: String,
-        @SerializedName("profile_image_url") val profileImageUrl: String,
-        @SerializedName("ranking") val ranking: Int,
-        @SerializedName("user_id") val userId: Int,
-        @SerializedName("walk_count") val walkCount: Int
-    )
-
     data class Ranking(
         @SerializedName("class_num") val classNum: Int,
         @SerializedName("grade") val grade: Int,
