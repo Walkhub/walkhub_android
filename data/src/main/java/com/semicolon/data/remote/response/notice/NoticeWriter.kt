539e88dd6909bfe10ed3096c5f0047dda1abad42
유현명 <aahspringaa4@naver.com>
2022-02-06 22:06:26 +0900

    ⚰️:: NoticeWriter

--- data/src/main/java/com/semicolon/data/remote/response/notice/NoticeWriter.kt
+++
@@ -1,9 +0,0 @@
-package com.semicolon.data.remote.response.notice
-
-import com.google.gson.annotations.SerializedName
-
-data class NoticeWriter (
-    @SerializedName("id") val writerId: Int,
-    @SerializedName("name") val writerName: String,
-    @SerializedName("profile_url") val profileUrl: String
-)
