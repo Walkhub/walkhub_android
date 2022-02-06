15db05f273be998f09b5cb89630b8cc03f6bda2b
Leesin0222 <dragon02229@naver.com>
2022-02-06 21:49:37 +0900

    ⚡ :: Update RankRepository

--- domain/src/main/java/com/semicolon/domain/repository/RankRepository.kt
+++ domain/src/main/java/com/semicolon/domain/repository/RankRepository.kt
@@ -1,19 +1,20 @@
 package com.semicolon.domain.repository
 
-import com.semicolon.domain.entity.rank.OurSchoolUserRankEntity
-import com.semicolon.domain.entity.rank.SchoolRankEntity
-import com.semicolon.domain.entity.rank.SearchSchoolEntity
-import com.semicolon.domain.entity.rank.UserRankEntity
+import com.semicolon.domain.entity.rank.*
+import com.semicolon.domain.enum.DateType
+import com.semicolon.domain.param.rank.FetchOurSchoolUserRankParam
+import com.semicolon.domain.param.rank.FetchUserRankParam
+import com.semicolon.domain.param.rank.SearchUserParam
 import kotlinx.coroutines.flow.Flow
 
 interface RankRepository {
-    suspend fun fetchSchoolRank(dateType: String): Flow<SchoolRankEntity>
+    suspend fun fetchSchoolRank(dateType: DateType): Flow<SchoolRankEntity>
 
     suspend fun searchSchool(name: String): Flow<SearchSchoolEntity>
 
-    suspend fun fetchUserRank(agencyCode:String, scope: String, dateType: String): Flow<UserRankEntity>
+    suspend fun fetchUserRank(fetchUserRankParam: FetchUserRankParam): Flow<UserRankEntity>
 
-    suspend fun fetchOurSchoolUserRank(scope: String, dateType: String): Flow<OurSchoolUserRankEntity>
+    suspend fun fetchOurSchoolUserRank(fetchOurSchoolUserRankParam: FetchOurSchoolUserRankParam): Flow<OurSchoolUserRankEntity>
 
-    suspend fun searchUser(name: String, scope: String, agencyCode: String, grade:Int, classNum:Int): Flow<UserRankEntity>
+    suspend fun searchUser(searchUserParam: SearchUserParam): Flow<SearchUserEntity>
 }
