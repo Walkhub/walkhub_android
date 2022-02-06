76932874e30c423af750a92d35761c20d8365cfd
유현명 <aahspringaa4@naver.com>
2022-02-06 22:09:53 +0900

    ⚰️:: 미사용 Import 및 문법 삭제

--- data/src/main/java/com/semicolon/data/remote/api/NoticesApi.kt
+++ data/src/main/java/com/semicolon/data/remote/api/NoticesApi.kt
@@ -1,6 +1,5 @@
 package com.semicolon.data.remote.api
 
-import com.semicolon.data.remote.response.notice.NoticeDetailResponse
 import com.semicolon.data.remote.response.notice.NoticeListResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
