5667009448066e806117cd37547c2448fa6b573b
Leesin0222 <dragon02229@naver.com>
2022-02-06 21:52:56 +0900

    ♻️ :: MyRanking 제거

--- domain/src/main/java/com/semicolon/domain/entity/rank/OurSchoolUserRankEntity.kt
+++ domain/src/main/java/com/semicolon/domain/entity/rank/OurSchoolUserRankEntity.kt
@@ -1,19 +1,9 @@
 package com.semicolon.domain.entity.rank
 
 data class OurSchoolUserRankEntity(
-    val myRanking: MyRanking,
+    val myRanking: Ranking,
     val rankingList: List<Ranking>
 ) {
-    data class MyRanking(
-        val classNum: Int,
-        val grade: Int,
-        val name: String,
-        val profileImageUrl: String,
-        val ranking: Int,
-        val userId: Int,
-        val walkCount: Int
-    )
-
     data class Ranking(
         val classNum: Int,
         val grade: Int,
