800f198f563f71cd86dda73e1a2b59d01c7bb8c5
minjun8946 <minjun8946@daum.net>
2022-02-07 21:06:36 +0900

    🐛:: line change

--- domain/src/main/java/com/semicolon/domain/usecase/school/SearchSchoolUseCase.kt
+++ domain/src/main/java/com/semicolon/domain/usecase/school/SearchSchoolUseCase.kt
@@ -9,6 +9,7 @@ import javax.inject.Inject
 class SearchSchoolUseCase @Inject constructor(
     private val searchSchoolRepository: SchoolRepository
 ) : UseCase<String, Flow<List<SearchSchoolEntity>>>() {
+
     override suspend fun execute(data: String): Flow<List<SearchSchoolEntity>> =
         searchSchoolRepository.searchSchool(data)
 }
