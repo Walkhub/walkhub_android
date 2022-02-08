291f152018812766d9e4b93305f2ee41499dfa38
Leesin0222 <dragon02229@naver.com>
2022-02-08 20:10:42 +0900

    ♻️ :: 코드 정리

--- domain/src/main/java/com/semicolon/domain/param/rank/FetchUserRankParam.kt
+++ domain/src/main/java/com/semicolon/domain/param/rank/FetchUserRankParam.kt
@@ -3,8 +3,8 @@ package com.semicolon.domain.param.rank
 import com.semicolon.domain.enum.DateType
 import com.semicolon.domain.enum.RankScope
 
-data class FetchUserRankParam (
-    val agencyCode:String,
+data class FetchUserRankParam(
+    val agencyCode: String,
     val scope: RankScope,
     val dateType: DateType
 )
