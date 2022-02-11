9692f68a91a058e5f7dfcfe07fe590cec4536d62
Jaewon <turitus@naver.com>
2022-02-12 00:34:30 +0900

    🧪 :: fetchChallenges 테스트

--- data/src/test/java/com/semicolon/data/datasource/ChallengeRemoteDatasourceUnitTest.kt
+++ data/src/test/java/com/semicolon/data/datasource/ChallengeRemoteDatasourceUnitTest.kt
@@ -3,16 +3,19 @@ package com.semicolon.data.datasource
 import com.semicolon.data.remote.api.ChallengeApi
 import com.semicolon.data.remote.datasource.RemoteChallengeDateSource
 import com.semicolon.data.remote.datasource.RemoteChallengeDateSourceImpl
+import com.semicolon.data.remote.response.challenge.ChallengeListResponse
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 
 class ChallengeRemoteDatasourceUnitTest {
 
-    private lateinit var remoteChallengeDatasource: RemoteChallengeDateSource
-
     @Mock
     private lateinit var challengeApi: ChallengeApi
+
+    private lateinit var remoteChallengeDatasource: RemoteChallengeDateSource
 
     @Before
     fun init() {
@@ -21,7 +24,30 @@ class ChallengeRemoteDatasourceUnitTest {
 
     @Test
     fun testFetchChallenges() {
+        val challengeList = listOf(
+            ChallengeListResponse.ChallengeResponse(
+                1,
+                "김재원",
+                "2022/2/12T12:14:00",
+                "2022/2/17T12:14:00",
+                "https://testImageUrl",
+                "ALL",
+                "ALL",
+                "WALK"
+            )
+        )
 
+        val challengeResponse = ChallengeListResponse(challengeList)
+
+        runBlocking {
+            `when`(challengeApi.getChallenges()).thenReturn(
+                challengeResponse
+            )
+
+            val datasourceValue = remoteChallengeDatasource.fetchChallenges()
+            val testResult = datasourceValue == challengeResponse
+            assert(testResult)
+        }
     }
 
     @Test
@@ -41,6 +67,6 @@ class ChallengeRemoteDatasourceUnitTest {
 
     @Test
     fun testFetchMyChallenges() {
-        
+
     }
 }
