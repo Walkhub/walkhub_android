e92727ffc7bd5cbb026adb41962dc0fe92a22615
Jaewon <turitus@naver.com>
2022-02-12 03:31:39 +0900

    🧪 :: challenge local test

--- data/src/test/java/com/semicolon/data/datasource/LocalChallengeDataSourceUnitTest.kt
+++ data/src/test/java/com/semicolon/data/datasource/LocalChallengeDataSourceUnitTest.kt
@@ -1,20 +1,48 @@
 package com.semicolon.data.datasource
 
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
 import com.semicolon.data.local.dao.ChallengeDao
 import com.semicolon.data.local.datasource.LocalChallengeDataSource
 import com.semicolon.data.local.datasource.LocalChallengeDataSourceImpl
+import com.semicolon.data.local.entity.challenge.ChallengeRoomEntity
+import com.semicolon.data.local.entity.challenge.toEntity
+import com.semicolon.data.remote.response.challenge.ChallengeListResponse
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
+import org.threeten.bp.LocalDateTime
 
 class LocalChallengeDataSourceUnitTest {
 
     private val challengeDao = mock<ChallengeDao>()
 
-    private val localChallengeDataSource: LocalChallengeDataSource = LocalChallengeDataSourceImpl(challengeDao)
+    private val localChallengeDataSource: LocalChallengeDataSource =
+        LocalChallengeDataSourceImpl(challengeDao)
+
+    private val challengeList = listOf(
+        ChallengeRoomEntity(
+            1,
+            "삼천보걷기",
+            "2022-2-12`T`12:14:00",
+            "2022-2-17`T`12:14:00",
+            "https://testImageUrl",
+            "ALL",
+            "ALL",
+            "WALK"
+        )
+    )
 
     @Test
     fun testFetchChallenges() {
+        runBlocking {
+            whenever(challengeDao.fetchChallenges()).thenReturn(challengeList)
 
+
+            val dataSource = localChallengeDataSource.fetchChallenges()
+            assertEquals(dataSource, challengeList.toEntity())
+        }
     }
 
     @Test
@@ -39,6 +67,6 @@ class LocalChallengeDataSourceUnitTest {
 
     @Test
     fun testSaveParticipants() {
-        
+
     }
 }
