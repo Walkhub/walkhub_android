d4032d159acc46cd93df37b95e33533f4cd672d4
Leesin0222 <dragon02229@naver.com>
2022-02-12 02:50:25 +0900

    ⚡:: localRankDataSource 작성

---
+++ data/src/main/java/com/semicolon/data/local/datasource/LocalRankDataSource.kt
@@ -0,0 +1,23 @@
+package com.semicolon.data.local.datasource
+
+
+import com.semicolon.data.local.entity.rank.*
+import com.semicolon.domain.entity.rank.*
+
+interface LocalRankDataSource {
+
+    suspend fun fetchSchoolRank(): SchoolRankEntity
+    suspend fun insertSchoolRank(schoolRank: SchoolRankEntity)
+
+    suspend fun searchSchool(): SearchSchoolEntity
+    suspend fun insertSearchSchool(searchSchool: SearchSchoolEntity)
+
+    suspend fun fetchUserRank(): UserRankEntity
+    suspend fun insertUserRank(userRank: UserRankEntity)
+
+    suspend fun fetchOurSchoolUserRank(): OurSchoolUserRankEntity
+    suspend fun insertOurSchoolUserRank(ourSchoolUserRank: OurSchoolUserRankEntity)
+
+    suspend fun searchUser(): SearchUserEntity
+    suspend fun insertSearchUser(searchUser: SearchUserEntity)
+}
