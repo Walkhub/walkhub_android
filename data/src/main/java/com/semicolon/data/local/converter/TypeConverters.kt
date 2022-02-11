6eab968197d3cdd51f9f2cb46a460da5bb2ca1ac
임세현 <sh00710000@gmail.com>
2022-02-11 20:43:22 +0900

    ⚡️ :: calories list type converter

--- data/src/main/java/com/semicolon/data/local/converter/TypeConverters.kt
+++ data/src/main/java/com/semicolon/data/local/converter/TypeConverters.kt
@@ -2,11 +2,11 @@ package com.semicolon.data.local.converter
 
 import androidx.room.ProvidedTypeConverter
 import androidx.room.TypeConverter
+import com.semicolon.data.local.entity.user.FetchCaloriesLevelRoomEntity
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 
-//@ProvidedTypeConverter
 //class BadgeListTypeConverter(
 //    private val moshi: Moshi
 //) {
@@ -26,3 +26,23 @@ import com.squareup.moshi.Types
 //    }
 //
 //}
+
+@ProvidedTypeConverter
+class CaloriesListTypeConverter(
+    private val moshi: Moshi
+) {
+
+    @TypeConverter
+    fun fromString(value: String): List<FetchCaloriesLevelRoomEntity.CaloriesLevel>? {
+        val listType = Types.newParameterizedType(List::class.java, FetchCaloriesLevelRoomEntity.CaloriesLevel::class.java)
+        val adapter: JsonAdapter<List<FetchCaloriesLevelRoomEntity.CaloriesLevel>> = moshi.adapter(listType)
+        return adapter.fromJson(value)
+    }
+
+    @TypeConverter
+    fun fromList(type: List<FetchCaloriesLevelRoomEntity.CaloriesLevel>): String {
+        val listType = Types.newParameterizedType(List::class.java, FetchCaloriesLevelRoomEntity.CaloriesLevel::class.java)
+        val adapter: JsonAdapter<List<FetchCaloriesLevelRoomEntity.CaloriesLevel>> = moshi.adapter(listType)
+        return adapter.toJson(type)
+    }
+}
