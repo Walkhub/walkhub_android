f03f807c8d4f4b9aa21c20441531df906f611193
임세현 <sh00710000@gmail.com>
2022-02-11 22:26:52 +0900

    ⚰️ :: 주석 제거

--- data/src/main/java/com/semicolon/data/local/converter/TypeConverters.kt
+++ data/src/main/java/com/semicolon/data/local/converter/TypeConverters.kt
@@ -7,26 +7,6 @@ import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 
-//class BadgeListTypeConverter(
-//    private val moshi: Moshi
-//) {
-//
-//    @TypeConverter
-//    fun fromString(value: String): List<UserOwnBadgeRoomEntity.Badge>? {
-//        val listType = Types.newParameterizedType(List::class.java, UserOwnBadgeRoomEntity.Badge::class.java)
-//        val adapter: JsonAdapter<List<UserOwnBadgeRoomEntity.Badge>> = moshi.adapter(listType)
-//        return adapter.fromJson(value)
-//    }
-//
-//    @TypeConverter
-//    fun fromImage(type: List<UserOwnBadgeRoomEntity.Badge>): String {
-//        val listType = Types.newParameterizedType(List::class.java, UserOwnBadgeRoomEntity.Badge::class.java)
-//        val adapter: JsonAdapter<List<UserOwnBadgeRoomEntity.Badge>> = moshi.adapter(listType)
-//        return adapter.toJson(type)
-//    }
-//
-//}
-
 @ProvidedTypeConverter
 class CaloriesListTypeConverter(
     private val moshi: Moshi
--- data/src/main/java/com/semicolon/data/remote/api/UserApi.kt
+++ data/src/main/java/com/semicolon/data/remote/api/UserApi.kt
@@ -6,85 +6,71 @@ import retrofit2.http.*
 
 interface UserApi {
 
-    // 반 가입하기 o
     @POST("users/classes/{section-id}")
     suspend fun signUpClass(
         @Path("section_id") groupId: Int,
         @Body signUpClassRequest: SignUpClassRequest
     )
 
-    // 토큰 재발급 o
     @PATCH("users/token")
     suspend fun userReissue(
         @Header("Refresh-Token") refreshToken: String
     ): UserReissueResponse
 
-    // 건강 정보 입력 o
     @PATCH("users/health")
     suspend fun patchUserHealth(
         @Body patchUserHealthRequest: PatchUserHealthRequest
     )
 
-    // 내 정보 수정 o
     @PATCH("users")
     suspend fun updateProfile(
         @Body updateProfileRequest: UpdateProfileRequest
     )
 
-    // 학교 정보 수정 o
     @PATCH("users/school")
     suspend fun patchSchool(
         @Body school_id: Int
     )
 
-    // 유저 아이디 찾기 o
     @GET("users/accounts/{phone-number}")
     suspend fun findUserAccount(
         @Path("phone_number") phoneNumber: String
     ): FindUserAccountResponse
 
-    // 유저 프로필 조회 o
     @GET("users/{user-id}")
     suspend fun fetchUserProfile(
         @Path("user-id") userId: Int
     ): FetchUserProfileResponse
 
-    // 마이 페이지 조회 o
     @GET("users")
     suspend fun fetchMyPage(
     ): FetchMyPageResponse
 
-    // 유저 비밀번호 변경 o
     @PATCH("users/password")
     suspend fun userChangePassword(
         @Body userChangePasswordRequest: UserChangePasswordRequest
     )
 
-    // 유저 로그인 o
     @POST("users/token")
     suspend fun userSignIn(
         @Body userSignInRequest: UserSignInRequest
     ): UserSignInResponse
 
-    // 전화번호 인증 o
     @POST("users/verification-codes")
     suspend fun verifyPhoneNumberSignUp(
         @Body verifyPhoneNumberSignUpRequest: VerifyPhoneNumberSignUpRequest
     )
 
-    // 유저 회원가입 o
     @POST("users")
     suspend fun userSignUp(
         @Body userSignUpRequest: UserSignUpRequest
     ): UserSignUpResponse
 
-    // 하루 목표 걸음 수 변경
     @PATCH("users/goal")
     suspend fun patchDailyWalkGoal(
         @Body patchDailyWalkGoalRequest: PatchDailyWalkGoalRequest
     )
-
-    // 칼로리 레벨 목록 조회
+    
     @GET("users/levels/list")
     suspend fun fetchCaloriesLevelList(): FetchCaloriesLevelResponse
 }
