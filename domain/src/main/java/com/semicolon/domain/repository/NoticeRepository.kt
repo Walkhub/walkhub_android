d145cf7e92c5325be7b2285f721050e2136e43e4
유현명 <aahspringaa4@naver.com>
2022-02-12 12:35:56 +0900

    🐛:: 병합 오류 수정

--- domain/src/main/java/com/semicolon/domain/repository/NoticeRepository.kt
+++ domain/src/main/java/com/semicolon/domain/repository/NoticeRepository.kt
@@ -4,16 +4,6 @@ import com.semicolon.domain.entity.notice.NoticeEntity
 import com.semicolon.domain.enum.NoticeType
 import kotlinx.coroutines.flow.Flow
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 interface NoticeRepository{
-    suspend fun loadNoticeList(): Flow<List<NoticeEntity>>
-=======
-interface NoticeRepository {
     suspend fun fetchNoticeList(noticeType: NoticeType): Flow<List<NoticeEntity>>
->>>>>>> develop
-=======
-interface NoticeRepository {
-    suspend fun fetchNoticeList(noticeType: NoticeType): Flow<List<NoticeEntity>>
->>>>>>> 60_Notice_data
 }
