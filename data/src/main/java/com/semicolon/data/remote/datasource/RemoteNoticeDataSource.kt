0293658e49b87a1a158620aca5538fdb9e98cd80
유현명 <aahspringaa4@naver.com>
2022-02-12 12:56:38 +0900

    ⚡️:: scope 매개변수 등록

--- data/src/main/java/com/semicolon/data/remote/datasource/RemoteNoticeDataSource.kt
+++ data/src/main/java/com/semicolon/data/remote/datasource/RemoteNoticeDataSource.kt
@@ -2,7 +2,8 @@ package com.semicolon.data.remote.datasource
 
 import com.semicolon.data.remote.response.challenge.ChallengeListResponse
 import com.semicolon.data.remote.response.notice.NoticeListResponse
+import com.semicolon.domain.enum.NoticeType
 
 interface RemoteNoticeDataSource {
-    suspend fun fetchNoticeList(): NoticeListResponse
+    suspend fun fetchNoticeList(scope: String): NoticeListResponse
 }
