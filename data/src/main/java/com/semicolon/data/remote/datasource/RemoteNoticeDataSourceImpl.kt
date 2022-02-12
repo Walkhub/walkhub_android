426425b2e60f63086173204c12a79a8a84de74b0
유현명 <aahspringaa4@naver.com>
2022-02-12 12:57:12 +0900

    ⚡️:: 매개변수 수정 & 함수명 수정

--- data/src/main/java/com/semicolon/data/remote/datasource/RemoteNoticeDataSourceImpl.kt
+++ data/src/main/java/com/semicolon/data/remote/datasource/RemoteNoticeDataSourceImpl.kt
@@ -4,14 +4,17 @@ import com.semicolon.data.remote.api.NoticesApi
 import com.semicolon.data.remote.response.image.ImagesResponse
 import com.semicolon.data.remote.response.notice.NoticeListResponse
 import com.semicolon.data.util.HttpHandler
+import com.semicolon.domain.enum.NoticeType
 import okhttp3.MultipartBody
 import javax.inject.Inject
 
 class RemoteNoticeDataSourceImpl @Inject constructor(
     private val noticesApi: NoticesApi
 ): RemoteNoticeDataSource {
-    override suspend fun fetchNoticeList(): NoticeListResponse =
+    override suspend fun fetchNoticeList(
+        scope: String
+    ): NoticeListResponse =
         HttpHandler<NoticeListResponse>()
-            .httpRequest { noticesApi.loadNoticeList() }
+            .httpRequest { noticesApi.fetchNoticeList(scope) }
             .sendRequest()
 }
