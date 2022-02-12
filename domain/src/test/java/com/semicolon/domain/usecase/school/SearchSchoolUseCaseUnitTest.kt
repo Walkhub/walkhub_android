2e0b7b0170d7f290771ffbfef31a5104295e997c
minjun8946 <minjun8946@daum.net>
2022-02-13 01:30:22 +0900

    🧪::  search school use case unit test

---
+++ domain/src/test/java/com/semicolon/domain/usecase/school/SearchSchoolUseCaseUnitTest.kt
@@ -0,0 +1,44 @@
+package com.semicolon.domain.usecase.school
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import com.semicolon.domain.entity.school.SearchSchoolEntity
+import com.semicolon.domain.repository.SchoolRepository
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SearchSchoolUseCaseUnitTest {
+
+    private val schoolRepository = mock<SchoolRepository>()
+
+    private val searchSchoolUseCase = SearchSchoolUseCase(schoolRepository)
+
+    @Test
+    fun testSearchSchoolUseCase() {
+        val schoolName = "my school"
+
+        val searchSchoolResponse =
+            listOf(
+                SearchSchoolEntity(
+                    "code",
+                    schoolName,
+                    "image"
+                )
+            )
+
+        runBlocking {
+            whenever(schoolRepository.searchSchool(schoolName)).thenReturn(
+                flow {
+                    emit(searchSchoolResponse)
+                }
+            )
+
+            val useCaseResult = searchSchoolUseCase.execute(schoolName)
+            useCaseResult.collect {
+                assertEquals(it, searchSchoolResponse)
+            }
+        }
+    }
+}
