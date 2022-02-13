2cf64fd3bce67eb681ec371238e43d51867a8394
Jaewon <turitus@naver.com>
2022-02-14 02:31:36 +0900

    🧪 :: test method

--- domain/src/test/java/com/semicolon/domain/usecase/notice/FetchNoticeListUseCaseUnitTest.kt
+++ domain/src/test/java/com/semicolon/domain/usecase/notice/FetchNoticeListUseCaseUnitTest.kt
@@ -2,10 +2,15 @@ package com.semicolon.domain.usecase.notice
 
 import com.nhaarman.mockitokotlin2.mock
 import com.semicolon.domain.repository.NoticeRepository
+import org.junit.Test
 
 class FetchNoticeListUseCaseUnitTest {
 
     private val noticeRepository = mock<NoticeRepository>()
 
     private val fetchNoticeListUseCase = FetchNoticeListUseCase(noticeRepository)
+
+    @Test
+    fun testFetchNoticeListUseCase() {
+    }
 }
