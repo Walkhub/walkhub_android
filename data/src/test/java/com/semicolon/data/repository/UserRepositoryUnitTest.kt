9065bd659ee3198508c752895167cfe7bd9bf8f7
minjun8946 <minjun8946@daum.net>
2022-02-14 02:39:53 +0900

    🧪 :: testPostUserSignUp()

--- data/src/test/java/com/semicolon/data/repository/UserRepositoryUnitTest.kt
+++ data/src/test/java/com/semicolon/data/repository/UserRepositoryUnitTest.kt
@@ -5,7 +5,9 @@ import com.nhaarman.mockitokotlin2.whenever
 import com.semicolon.data.local.datasource.LocalUserDataSource
 import com.semicolon.data.remote.datasource.RemoteImagesDataSource
 import com.semicolon.data.remote.datasource.RemoteUserDataSource
+import com.semicolon.data.remote.request.users.UserSignUpRequest
 import com.semicolon.data.remote.request.users.VerifyPhoneNumberSignUpRequest
+import com.semicolon.domain.param.user.PostUserSignUpParam
 import com.semicolon.domain.param.user.VerifyPhoneNumberSignUpParam
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
@@ -23,10 +25,27 @@ class UserRepositoryUnitTest {
 
     @Test
     fun testVerifyUserPhoneNumber() {
+        runBlocking {
+            val repositoryResult =
+                userRepository.verifyUserPhoneNumber(VerifyPhoneNumberSignUpParam("010-2100-2936"))
+            assertEquals(Unit, repositoryResult)
+        }
+    }
+
+    @Test
+    fun testPostUserSignUp() {
+        val request = PostUserSignUpParam(
+            "13",
+            "dlwodnjs0310",
+            "이재원",
+            "010-2100-2936",
+            "abcd",
+            "대덕소프트웨어마이스터고"
+        )
 
         runBlocking {
-            val repositoryResult = userRepository.verifyUserPhoneNumber(VerifyPhoneNumberSignUpParam("010-2100-2936"))
-            assertEquals(Unit,repositoryResult)
+            val repositoryResult = userRepository.postUserSignUp(request)
+            assertEquals(Unit, repositoryResult)
         }
     }
 }
