2c743d39b2aaced18713257e5593bb0fa00ea4ba
shw0471 <shw0471@naver.com>
2022-02-14 04:52:41 +0900

    ⚡️ :: google oauth & permission request

--- presentation/src/main/java/com/semicolon/walkhub/ui/login/LoginActivity.kt
+++ presentation/src/main/java/com/semicolon/walkhub/ui/login/LoginActivity.kt
@@ -1,12 +1,72 @@
 package com.semicolon.walkhub.ui.login
 
+import android.Manifest
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.nms_android_v1.base.BaseActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.fitness.FitnessOptions
+import com.google.android.gms.fitness.data.DataType
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import com.semicolon.walkhub.R
+import com.semicolon.walkhub.databinding.ActivityLoginBinding
+import android.widget.Toast
 
-class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+import com.semicolon.walkhub.ui.MainActivity
+
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>(
+    R.layout.activity_login
+) {
+
+    override fun initView() {
+        requestPermissions()
+        fitSignIn()
     }
+
+    private fun requestPermissions() {
+        val permissionListener = object : PermissionListener {
+            override fun onPermissionGranted() {
+            }
+
+            override fun onPermissionDenied(deniedPermissions: List<String>) {
+                finish()
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            TedPermission.create()
+                .setPermissionListener(permissionListener)
+                .setPermissions(
+                    Manifest.permission.ACTIVITY_RECOGNITION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ).check()
+        } else {
+            TedPermission.create()
+                .setPermissionListener(permissionListener)
+                .setPermissions(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ).check()
+        }
+    }
+
+    private fun fitSignIn() {
+        val fitnessOptions = FitnessOptions.builder()
+            .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA)
+            .addDataType(DataType.AGGREGATE_MOVE_MINUTES)
+            .addDataType(DataType.AGGREGATE_DISTANCE_DELTA)
+            .addDataType(DataType.AGGREGATE_CALORIES_EXPENDED)
+            .addDataType(DataType.TYPE_LOCATION_SAMPLE)
+            .build()
+        GoogleSignIn.requestPermissions(
+            this, 0,
+            GoogleSignIn.getAccountForExtension(this, fitnessOptions)
+        )
+    }
+
+
 }
