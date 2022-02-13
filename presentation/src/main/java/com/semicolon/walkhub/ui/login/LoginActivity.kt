8c4622a8528195267924ceadf3b28c3c2220821b
shw0471 <shw0471@naver.com>
2022-02-14 04:10:31 +0900

    📑 :: add LoginActivity

---
+++ presentation/src/main/java/com/semicolon/walkhub/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.semicolon.walkhub.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.semicolon.walkhub.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
