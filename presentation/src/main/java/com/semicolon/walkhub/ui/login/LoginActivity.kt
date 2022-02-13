d2340b833667ab395b394e6ab233f02ecc41d42b
shw0471 <shw0471@naver.com>
2022-02-14 07:04:05 +0900

    🐛 :: fix google sign in logic

--- presentation/src/main/java/com/semicolon/walkhub/ui/login/LoginActivity.kt
+++ presentation/src/main/java/com/semicolon/walkhub/ui/login/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.semicolon.walkhub.ui.login
 
 import android.Manifest
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,22 +14,41 @@ import com.gun0912.tedpermission.normal.TedPermission
 import com.semicolon.walkhub.R
 import com.semicolon.walkhub.databinding.ActivityLoginBinding
 import android.widget.Toast
+import com.jakewharton.threetenabp.AndroidThreeTen
+import com.semicolon.data.local.datasource.LocalExerciseDataSource
+import com.semicolon.data.local.datasource.LocalExerciseDataSourceImpl
+import com.semicolon.data.local.storage.ExerciseInfoDataStorage
+import com.semicolon.data.local.storage.ExerciseInfoDataStorageImpl
+import com.semicolon.data.local.storage.FitnessDataStorageImpl
 
 import com.semicolon.walkhub.ui.MainActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(
     R.layout.activity_login
 ) {
+    private val fitnessOptions =
+        FitnessOptions.builder()
+            .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA)
+            .addDataType(DataType.AGGREGATE_MOVE_MINUTES)
+            .addDataType(DataType.AGGREGATE_DISTANCE_DELTA)
+            .addDataType(DataType.AGGREGATE_CALORIES_EXPENDED)
+            .addDataType(DataType.TYPE_LOCATION_SAMPLE)
+            .build()
 
     override fun initView() {
         requestPermissions()
-        fitSignIn()
     }
 
     private fun requestPermissions() {
         val permissionListener = object : PermissionListener {
             override fun onPermissionGranted() {
+                fitSignIn()
             }
 
             override fun onPermissionDenied(deniedPermissions: List<String>) {
@@ -55,18 +75,10 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(
     }
 
     private fun fitSignIn() {
-        val fitnessOptions = FitnessOptions.builder()
-            .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA)
-            .addDataType(DataType.AGGREGATE_MOVE_MINUTES)
-            .addDataType(DataType.AGGREGATE_DISTANCE_DELTA)
-            .addDataType(DataType.AGGREGATE_CALORIES_EXPENDED)
-            .addDataType(DataType.TYPE_LOCATION_SAMPLE)
-            .build()
         GoogleSignIn.requestPermissions(
             this, 0,
-            GoogleSignIn.getAccountForExtension(this, fitnessOptions)
+            GoogleSignIn.getAccountForExtension(this, fitnessOptions),
+            fitnessOptions
         )
     }
-
-
 }
