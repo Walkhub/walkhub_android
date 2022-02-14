ca32987a095e3d6477c82c3d2224024fe6ce0196
유현명 <aahspringaa4@naver.com>
2022-02-14 10:33:11 +0900

    ⚡️:: FCM 알림 받기 구현

---
+++ presentation/src/main/java/com/semicolon/walkhub/fcm/FCMManager.kt
@@ -0,0 +1,72 @@
+package com.semicolon.walkhub.fcm
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.ContentValues.TAG
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.semicolon.walkhub.R
+
+class FCMManager : FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+        var token = token
+        super.onNewToken(token)
+
+        token = FirebaseMessaging.getInstance().token.result
+
+        FirebaseMessaging.getInstance().token.addOnSuccessListener { }
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        if (remoteMessage.data.isNotEmpty()) {
+            sendNotification(remoteMessage)
+        }
+    }
+
+    private fun sendNotification(remoteMessage: RemoteMessage) {
+        val title = remoteMessage.data["title"]
+        val message = remoteMessage.data["message"]
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = "채널"
+            val channelName = "채널명"
+            val notificationChannel = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            val channelMessage = NotificationChannel(
+                channel, channelName,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            channelMessage.description = "채널에 대한 설명."
+            channelMessage.enableLights(true)
+            channelMessage.enableVibration(true)
+            channelMessage.setShowBadge(false)
+            channelMessage.vibrationPattern = longArrayOf(100, 200, 100, 200)
+            notificationChannel.createNotificationChannel(channelMessage)
+            val notificationBuilder = NotificationCompat.Builder(this, channel)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setChannelId(channel)
+                .setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_SOUND or Notification.DEFAULT_VIBRATE)
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(9999, notificationBuilder.build())
+
+        } else {
+            val notificationBuilder = NotificationCompat.Builder(this, "")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_SOUND or Notification.DEFAULT_VIBRATE)
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(9999, notificationBuilder.build())
+        }
+    }
+}
