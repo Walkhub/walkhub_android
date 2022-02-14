cfb42679cf18f9e8b25594d74fd6d64ddce83e31
유현명 <aahspringaa4@naver.com>
2022-02-14 13:12:51 +0900

    ♻️:: 중첩 클래스에 중첩 클래스

--- domain/src/main/java/com/semicolon/domain/entity/notice/NoticeEntity.kt
+++ domain/src/main/java/com/semicolon/domain/entity/notice/NoticeEntity.kt
@@ -3,14 +3,18 @@ package com.semicolon.domain.entity.notice
 import java.time.LocalDateTime
 
 data class NoticeEntity(
-    val noticeId: Int,
-    val title: String,
-    val createdAt: LocalDateTime,
-    val noticeWriter: NoticeWriterEntity
-){
-    data class NoticeWriterEntity(
-        val writerId: Int,
-        val writerName: String,
-        val profileUrl: String
-    )
+    val noticeValueEntity: List<NoticeValueEntity>
+) {
+    data class NoticeValueEntity(
+        val noticeId: Int,
+        val title: String,
+        val createdAt: LocalDateTime,
+        val noticeWriter: NoticeWriterEntity
+    ) {
+        data class NoticeWriterEntity(
+            val writerId: Int,
+            val writerName: String,
+            val profileUrl: String
+        )
+    }
 }
