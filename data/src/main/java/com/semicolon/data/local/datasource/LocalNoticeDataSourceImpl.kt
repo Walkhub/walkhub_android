022f31d620ccfc33cadb87429880339fb8b1dabe
유현명 <aahspringaa4@naver.com>
2022-02-14 13:09:32 +0900

    🐛:: 중첩 리스트 & 반환 클래스 오류 수정

--- data/src/main/java/com/semicolon/data/local/datasource/LocalNoticeDataSourceImpl.kt
+++ data/src/main/java/com/semicolon/data/local/datasource/LocalNoticeDataSourceImpl.kt
@@ -1,6 +1,7 @@
 package com.semicolon.data.local.datasource
 
 import com.semicolon.data.local.dao.NoticeDao
+import com.semicolon.data.local.entity.notice.NoticeListRoomEntity
 import com.semicolon.data.local.entity.notice.toDbEntity
 import com.semicolon.data.local.entity.notice.toEntity
 import com.semicolon.domain.entity.notice.NoticeEntity
@@ -10,9 +11,9 @@ class LocalNoticeDataSourceImpl @Inject constructor(
     private val noticeDao: NoticeDao
 ): LocalNoticeDataSource {
 
-    override suspend fun fetchNoticeList(): List<NoticeEntity> =
+    override suspend fun fetchNoticeList(): NoticeListRoomEntity =
         noticeDao.fetchNoticeList().toEntity()
 
-    override suspend fun saveNoticeList(notice: List<NoticeEntity>): List<NoticeEntity> =
+    override suspend fun saveNoticeList(notice: NoticeEntity): NoticeEntity =
         noticeDao.saveNoticeList(notice.toDbEntity())
 }
