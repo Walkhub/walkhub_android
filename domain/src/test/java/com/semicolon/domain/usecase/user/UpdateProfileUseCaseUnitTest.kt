1e43aedc3b96e32c39171653c24575bc0562ccd6
minjun8946 <minjun8946@daum.net>
2022-02-16 00:32:58 +0900

    🧪 :: testUpdateProfileUseCase()

--- domain/src/test/java/com/semicolon/domain/usecase/user/UpdateProfileUseCaseUnitTest.kt
+++ domain/src/test/java/com/semicolon/domain/usecase/user/UpdateProfileUseCaseUnitTest.kt
@@ -1,4 +1,30 @@
 package com.semicolon.domain.usecase.user
 
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
+import com.semicolon.domain.param.user.UpdateProfileParam
+import com.semicolon.domain.repository.UserRepository
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
 class UpdateProfileUseCaseUnitTest {
+
+    private val userRepository = mock<UserRepository>()
+
+    private val updateProfileUseCase = UpdateProfileUseCase(userRepository)
+
+    private val updateProfileParam = mock<UpdateProfileParam>()
+
+    @Test
+    fun testUpDateProfileUseCase() {
+
+        runBlocking {
+            val useCaseResult = updateProfileUseCase.execute(updateProfileParam)
+            assertEquals(Unit,useCaseResult)
+
+            verify(userRepository, times(1)).updateProfile(updateProfileParam)
+        }
+    }
 }
