15a733e0b83030522f2392662160debb0fc53200
minjun8946 <minjun8946@daum.net>
2022-02-15 16:20:18 +0900

    🧪 :: testPatchSchoolUseCase()

--- domain/src/test/java/com/semicolon/domain/usecase/user/PatchSchoolUseCaseUnitTest.kt
+++ domain/src/test/java/com/semicolon/domain/usecase/user/PatchSchoolUseCaseUnitTest.kt
@@ -1,4 +1,23 @@
 package com.semicolon.domain.usecase.user
 
+import com.nhaarman.mockitokotlin2.mock
+import com.semicolon.domain.repository.UserRepository
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
 class PatchSchoolUseCaseUnitTest {
+
+    private val userRepository = mock<UserRepository>()
+
+    private val patchSchoolUseCase= PatchSchoolUseCase(userRepository)
+
+    @Test
+    fun testPatchSchoolUseCase() {
+        val schoolId = 1
+        runBlocking {
+            val useCaseResult = patchSchoolUseCase.execute(schoolId)
+            assertEquals(Unit,useCaseResult)
+        }
+    }
 }
