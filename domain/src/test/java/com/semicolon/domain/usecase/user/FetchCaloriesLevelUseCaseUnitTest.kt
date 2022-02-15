b9d771b6cd80e4bcc47b41716590b8cd7fac863b
minjun8946 <minjun8946@daum.net>
2022-02-15 15:25:05 +0900

    🧪 :: testFetchCaloriesLevelUseCase

--- domain/src/test/java/com/semicolon/domain/usecase/user/FetchCaloriesLevelUseCaseUnitTest.kt
+++ domain/src/test/java/com/semicolon/domain/usecase/user/FetchCaloriesLevelUseCaseUnitTest.kt
@@ -1,4 +1,37 @@
 package com.semicolon.domain.usecase.user
 
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import com.semicolon.domain.entity.users.FetchCaloriesLevelEntity
+import com.semicolon.domain.repository.UserRepository
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
 class FetchCaloriesLevelUseCaseUnitTest {
+
+    private val userRepository = mock<UserRepository>()
+
+    private val fetchCaloriesLevelUseCase = FetchCaloriesLevelUseCase(userRepository)
+
+    private val fetchCaloriesLevelEntity = mock<FetchCaloriesLevelEntity>()
+
+    @Test
+    fun testFetchCaloriesLevelUseCase() {
+
+        runBlocking {
+            whenever(userRepository.fetchCaloriesLevel()).thenReturn(
+                flow {
+                    emit(fetchCaloriesLevelEntity)
+                }
+            )
+
+            fetchCaloriesLevelUseCase.execute(Unit)
+                .collect {
+                    assertEquals(fetchCaloriesLevelEntity,it)
+                }
+        }
+    }
 }
