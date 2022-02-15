9286a6f8d43df6534067e5d66f5f7218c02c1bd5
Leesin0222 <dragon02229@naver.com>
2022-02-15 15:46:59 +0900

    ⚡ :: SettinFragment 설정

---
+++ presentation/src/main/java/com/semicolon/walkhub/ui/profile/setting/SettingFragment.kt
@@ -0,0 +1,15 @@
+package com.semicolon.walkhub.ui.profile.setting
+
+import com.example.nms_android_v1.base.BaseFragment
+import com.semicolon.walkhub.R
+import com.semicolon.walkhub.databinding.FragmentSettingBinding
+
+class SettingFragment : BaseFragment<FragmentSettingBinding>(
+    R.layout.fragment_setting
+) {
+    override fun initView() {
+    }
+
+    override fun observeEvent() {
+    }
+}
