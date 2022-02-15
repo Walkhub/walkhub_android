bba9ca587c7b603e67662f7c1b66d575c53ed76d
minjun8946 <minjun8946@daum.net>
2022-02-15 20:22:24 +0900

    🧪 :: testPostUserSignInUseCase()

--- domain/src/test/java/com/semicolon/domain/usecase/user/PostUserSignInUseCaseUnitTest.kt
+++ domain/src/test/java/com/semicolon/domain/usecase/user/PostUserSignInUseCaseUnitTest.kt
@@ -1,4 +1,28 @@
 package com.semicolon.domain.usecase.user
 
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.semicolon.domain.param.user.PatchUserHealthParam
+import com.semicolon.domain.param.user.PostUserSignInParam
+import com.semicolon.domain.repository.UserRepository
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
 class PostUserSignInUseCaseUnitTest {
+
+    private val userRepository = mock<UserRepository>()
+
+    private val patchUserSignInUseCase = PostUserSignInUseCase(userRepository)
+
+    @Test
+    fun testPostUserSignInUseCase() {
+        val postUserSignInParam = PostUserSignInParam("account_id", "password", "device_token")
+        runBlocking {
+            val useCaseResult = patchUserSignInUseCase.execute(postUserSignInParam)
+            assertEquals(Unit, useCaseResult)
+
+            verify(userRepository).postUserSignIn(postUserSignInParam)
+        }
+    }
 }
