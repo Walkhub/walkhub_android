2f2ec2149fc1c33aae1839e60339778ebd6ecfb1
minjun8946 <minjun8946@daum.net>
2022-02-15 18:38:01 +0900

    🧪 :: add verify

--- domain/src/test/java/com/semicolon/domain/usecase/user/PatchDailyWalkGoalUseCaseUnitTest.kt
+++ domain/src/test/java/com/semicolon/domain/usecase/user/PatchDailyWalkGoalUseCaseUnitTest.kt
@@ -1,6 +1,9 @@
 package com.semicolon.domain.usecase.user
 
+import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
 import com.semicolon.domain.param.user.PatchDailyWalkGoalParam
 import com.semicolon.domain.repository.UserRepository
 import kotlinx.coroutines.runBlocking
@@ -21,6 +24,7 @@ class PatchDailyWalkGoalUseCaseUnitTest {
         runBlocking {
             val useCaseResult = patchDailyWalkGoalUseCase.execute(patchDailyWalkGoalParam)
             assertEquals(Unit,useCaseResult)
+            verify(userRepository, times(1)).patchDailyWalkGoal(patchDailyWalkGoalParam)
         }
     }
 }
