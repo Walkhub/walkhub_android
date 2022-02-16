5a809d1a00fe109e6731589123077ab3b8b49c6a
유현명 <aahspringaa4@naver.com>
2022-02-16 23:55:03 +0900

    ♻️:: createAt 형변환

--- domain/src/main/java/com/semicolon/domain/entity/notice/NoticeEntity.kt
+++ domain/src/main/java/com/semicolon/domain/entity/notice/NoticeEntity.kt
@@ -1,8 +1,10 @@
 package com.semicolon.domain.entity.notice
 
+import java.time.LocalDateTime
+
 data class NoticeEntity (
     val noticeId: Int,
     val title: String,
-    val createdAt: String,
+    val createdAt: LocalDateTime,
     val noticeWriter: NoticeWriterEntity
 )
