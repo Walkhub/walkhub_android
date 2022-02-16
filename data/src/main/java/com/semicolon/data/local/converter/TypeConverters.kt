da55762a304bb5275c2c9b5a88f6fd47913a81a3
유현명 <aahspringaa4@naver.com>
2022-02-16 15:08:11 +0900

    ⚡️:: Converter fromList 구현 & 버그 수정

--- data/src/main/java/com/semicolon/data/local/converter/TypeConverters.kt
+++ data/src/main/java/com/semicolon/data/local/converter/TypeConverters.kt
@@ -2,11 +2,10 @@ package com.semicolon.data.local.converter
 
 import androidx.room.ProvidedTypeConverter
 import androidx.room.TypeConverter
-import com.semicolon.data.local.entity.notice.NoticeListRoomEntity
-import com.semicolon.data.local.entity.user.UserOwnBadgeRoomEntity
 import com.semicolon.data.local.entity.badge.FetchMyBadgesRoomEntity
 import com.semicolon.data.local.entity.badge.FetchNewBadgesRoomEntity
 import com.semicolon.data.local.entity.badge.FetchUserBadgesRoomEntity
+import com.semicolon.data.local.entity.notice.NoticeListRoomEntity
 import com.semicolon.data.local.entity.user.FetchCaloriesLevelRoomEntity
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
@@ -88,12 +87,21 @@ class NewBadgeListTypeConverter(
 class NoticeListTypeConverter(
   private val moshi: Moshi
 ){
-  
-  @TypeConverter
-    fun fromStringToNotice(value: String): List<NoticeListRoomEntity>? {
-        val listType = Types.newParameterizedType(List::class.java, NoticeListRoomEntity::class.java)
-        val adapter: JsonAdapter<List<NoticeListRoomEntity>> = moshi.adapter(listType)
+
+    @TypeConverter
+    fun fromString(value: String): List<NoticeListRoomEntity.NoticeListValue>? {
+        val listType =
+            Types.newParameterizedType(List::class.java, NoticeListRoomEntity.NoticeListValue::class.java)
+        val adapter: JsonAdapter<List<NoticeListRoomEntity.NoticeListValue>> = moshi.adapter(listType)
         return adapter.fromJson(value)
+    }
+
+    @TypeConverter
+    fun fromList(type: List<NoticeListRoomEntity.NoticeListValue>): String {
+        val listType =
+            Types.newParameterizedType(List::class.java, NoticeListRoomEntity.NoticeListValue::class.java)
+        val adapter: JsonAdapter<List<NoticeListRoomEntity.NoticeListValue>> = moshi.adapter(listType)
+        return adapter.toJson(type)
     }
 }
 
