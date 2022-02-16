e63a4f554fc2877f6e0fea56ccbdabbb71f8c2a0
유현명 <aahspringaa4@naver.com>
2022-02-16 15:14:20 +0900

    ♻️:: 파라미터 값 변경

--- data/src/main/java/com/semicolon/data/remote/datasource/RemoteNoticeDataSource.kt
+++ data/src/main/java/com/semicolon/data/remote/datasource/RemoteNoticeDataSource.kt
@@ -4,5 +4,5 @@ import com.semicolon.data.remote.response.notice.NoticeListResponse
 import com.semicolon.domain.enum.NoticeType
 
 interface RemoteNoticeDataSource {
-    suspend fun fetchNoticeList(scope: String): NoticeListResponse
+    suspend fun fetchNoticeList(noticeType: String): NoticeListResponse
 }
