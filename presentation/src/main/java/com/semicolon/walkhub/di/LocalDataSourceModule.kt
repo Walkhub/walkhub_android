ae2d4e4d007decf688fb3e4199478c462d46c3ab
minjun8946 <minjun8946@daum.net>
2022-02-17 03:11:58 +0900

    ⚡️:: local datasource module

---
+++ presentation/src/main/java/com/semicolon/walkhub/di/LocalDataSourceModule.kt
@@ -0,0 +1,50 @@
+package com.semicolon.walkhub.di
+
+import com.semicolon.data.local.datasource.*
+import com.semicolon.data.remote.api.BadgeApi
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocalDataSourceModule {
+
+    @Binds
+    abstract fun provideLocalBadgeDataSource(
+        localBadgeDataSourceImpl: LocalBadgeDataSourceImpl
+    ) : LocalBadgeDataSource
+
+    @Binds
+    abstract fun provideLocalChallengeDataSource(
+        localChallengeDataSourceImpl: LocalChallengeDataSourceImpl
+    ): LocalChallengeDataSource
+
+    @Binds
+    abstract fun provideLocalExerciseDataSource(
+        localExerciseDataSourceImpl: LocalExerciseDataSourceImpl
+    ): LocalExerciseDataSource
+
+    @Binds
+    abstract fun provideLocalNoticeDataSource(
+        localNoticeDataSourceImpl: LocalNoticeDataSourceImpl
+    ): LocalNoticeDataSource
+
+    @Binds
+    abstract fun provideNotificationDataSource(
+        localNotificationDataSourceImpl: LocalNotificationDataSourceImpl
+    ): LocalNotificationDataSource
+
+    @Binds
+    abstract fun provideRankDataSource(
+        localRankDataSourceImpl: LocalRankDataSourceImpl
+    ): LocalRankDataSource
+
+    @Binds
+    abstract fun provideLocalUserDataSource(
+        localUserDataSourceImpl: LocalUserDataSourceImpl
+    ): LocalUserDataSource
+
+}
