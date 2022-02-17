bab2317d6c78c494da6a40c1b781e835a74de8b8
minjun8946 <minjun8946@daum.net>
2022-02-17 14:15:43 +0900

    ⚰️ ::  to entity()

--- data/src/main/java/com/semicolon/data/remote/datasource/RemoteBadgeDataSourceImpl.kt
+++ data/src/main/java/com/semicolon/data/remote/datasource/RemoteBadgeDataSourceImpl.kt
@@ -4,30 +4,34 @@ import com.semicolon.data.remote.api.BadgeApi
 import com.semicolon.data.remote.response.badge.FetchMyBadgesResponse
 import com.semicolon.data.remote.response.badge.FetchNewBadgesResponse
 import com.semicolon.data.remote.response.badge.FetchUserBadgesResponse
+import com.semicolon.data.remote.response.badge.toEntity
 import com.semicolon.data.util.HttpHandler
+import com.semicolon.domain.entity.badge.FetchMyBadgesEntity
+import com.semicolon.domain.entity.badge.FetchNewBadgesEntity
+import com.semicolon.domain.entity.badge.FetchUserBadgesEntity
 import javax.inject.Inject
 
 class RemoteBadgeDataSourceImpl @Inject constructor(
     private val badgeApi: BadgeApi
 ) : RemoteBadgeDataSource {
 
-    override suspend fun fetchUserBadges(userId: Int): FetchUserBadgesResponse =
+    override suspend fun fetchUserBadges(userId: Int): FetchUserBadgesEntity =
         HttpHandler<FetchUserBadgesResponse>()
             .httpRequest { badgeApi.fetchUserBadges(userId) }
-            .sendRequest()
+            .sendRequest().toEntity()
 
-    override suspend fun fetchMyBadges(): FetchMyBadgesResponse =
+    override suspend fun fetchMyBadges(): FetchMyBadgesEntity =
         HttpHandler<FetchMyBadgesResponse>()
             .httpRequest { badgeApi.fetchMyBadges() }
-            .sendRequest()
+            .sendRequest().toEntity()
 
     override suspend fun setBadge(badgeId: Int) =
         HttpHandler<Unit>()
             .httpRequest { badgeApi.setBadge(badgeId) }
             .sendRequest()
 
-    override suspend fun fetchNewBadges(): FetchNewBadgesResponse =
+    override suspend fun fetchNewBadges(): FetchNewBadgesEntity =
         HttpHandler<FetchNewBadgesResponse>()
             .httpRequest { badgeApi.fetchNewBadges() }
-            .sendRequest()
+            .sendRequest().toEntity()
 }
