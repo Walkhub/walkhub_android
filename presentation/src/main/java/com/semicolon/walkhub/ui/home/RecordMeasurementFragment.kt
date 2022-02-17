654e6b4f017a3b5d1ee72d193d33c0ff76a33927
Leesin0222 <dragon02229@naver.com>
2022-02-17 09:19:24 +0900

    ⚡ :: 기록측정  프레그먼트 생성

---
+++ presentation/src/main/java/com/semicolon/walkhub/ui/home/RecordMeasurementFragment.kt
@@ -0,0 +1,15 @@
+package com.semicolon.walkhub.ui.home
+
+import com.example.nms_android_v1.base.BaseFragment
+import com.semicolon.walkhub.R
+import com.semicolon.walkhub.databinding.FragmentRecordMeasurementBinding
+
+class RecordMeasurementFragment : BaseFragment<FragmentRecordMeasurementBinding>(
+    R.layout.fragment_home
+) {
+    override fun initView() {
+    }
+
+    override fun observeEvent() {
+    }
+}
