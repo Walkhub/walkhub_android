d1f4b2806a61c2e261031e5d073f645de50ad2e9
minjun8946 <minjun8946@daum.net>
2022-02-17 14:13:31 +0900

    🧪:: testFetchUserBadges

--- data/src/test/java/com/semicolon/data/datasource/RemoteBadgeDataSourceUnitTest.kt
+++ data/src/test/java/com/semicolon/data/datasource/RemoteBadgeDataSourceUnitTest.kt
@@ -1,15 +1,44 @@
 package com.semicolon.data.datasource
 
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
 import com.semicolon.data.remote.api.BadgeApi
 import com.semicolon.data.remote.datasource.RemoteBadgeDataSource
 import com.semicolon.data.remote.datasource.RemoteBadgeDataSourceImpl
+import com.semicolon.data.remote.response.badge.FetchMyBadgesResponse
+import com.semicolon.data.remote.response.badge.FetchNewBadgesResponse
+import com.semicolon.data.remote.response.badge.FetchUserBadgesResponse
+import com.semicolon.data.remote.response.badge.toEntity
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 class RemoteBadgeDataSourceUnitTest {
 
     private val badgeApi = mock<BadgeApi>()
 
-    private val remoteChallengeDatasource: RemoteBadgeDataSource =
+    private val remoteBadgeDatasource: RemoteBadgeDataSource =
         RemoteBadgeDataSourceImpl(badgeApi)
+
+    private val fetchUserBadgesResponse = mock<FetchUserBadgesResponse>()
+    private val fetchMyBadgesResponse = mock<FetchMyBadgesResponse>()
+    private val fetchNewBadgesResponse = mock<FetchNewBadgesResponse>()
+
+    @Test
+    fun testFetchUserBadges() {
+        val userId = 1
+        runBlocking {
+            whenever(badgeApi.fetchUserBadges(userId)).thenReturn(fetchUserBadgesResponse)
+            val dataSourceResult = remoteBadgeDatasource.fetchUserBadges(userId)
+
+            assertEquals(fetchUserBadgesResponse.toEntity(), dataSourceResult)
+
+            verify(badgeApi, times(1)).fetchUserBadges(userId)
+        }
+    }
+
+
 
 }
