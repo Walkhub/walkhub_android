5a265c4ebb7d54853fe5db3571900114f59653a6
minjun8946 <minjun8946@daum.net>
2022-02-17 14:16:16 +0900

    🐛:: change return value

--- data/src/main/java/com/semicolon/data/remote/datasource/RemoteBadgeDataSource.kt
+++ data/src/main/java/com/semicolon/data/remote/datasource/RemoteBadgeDataSource.kt
@@ -3,14 +3,17 @@ package com.semicolon.data.remote.datasource
 import com.semicolon.data.remote.response.badge.FetchMyBadgesResponse
 import com.semicolon.data.remote.response.badge.FetchNewBadgesResponse
 import com.semicolon.data.remote.response.badge.FetchUserBadgesResponse
+import com.semicolon.domain.entity.badge.FetchMyBadgesEntity
+import com.semicolon.domain.entity.badge.FetchNewBadgesEntity
+import com.semicolon.domain.entity.badge.FetchUserBadgesEntity
 
 interface RemoteBadgeDataSource {
 
-    suspend fun fetchUserBadges(userId: Int): FetchUserBadgesResponse
+    suspend fun fetchUserBadges(userId: Int): FetchUserBadgesEntity
 
-    suspend fun fetchMyBadges(): FetchMyBadgesResponse
+    suspend fun fetchMyBadges(): FetchMyBadgesEntity
 
     suspend fun setBadge(badgeId: Int)
 
-    suspend fun fetchNewBadges(): FetchNewBadgesResponse
+    suspend fun fetchNewBadges(): FetchNewBadgesEntity
 }
