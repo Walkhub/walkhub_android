a10d64d4b1a2765efdb322e3b205ad4f32c786b0
Leesin0222 <dragon02229@naver.com>
2022-02-17 15:56:49 +0900

    ⚡ :: 기록측정 리사이클러뷰 가로로 보여주는 코드

--- presentation/src/main/java/com/semicolon/walkhub/ui/home/RecordMeasurementFragment.kt
+++ presentation/src/main/java/com/semicolon/walkhub/ui/home/RecordMeasurementFragment.kt
@@ -1,13 +1,25 @@
 package com.semicolon.walkhub.ui.home
 
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.nms_android_v1.base.BaseFragment
 import com.semicolon.walkhub.R
 import com.semicolon.walkhub.databinding.FragmentRecordMeasurementBinding
+import com.semicolon.walkhub.ui.MainActivity
 
 class RecordMeasurementFragment : BaseFragment<FragmentRecordMeasurementBinding>(
     R.layout.fragment_home
 ) {
     override fun initView() {
+
+         var mainActivity:MainActivity
+
+        mainActivity = context as MainActivity
+
+            binding.recyclerview.layoutManager =
+                LinearLayoutManager( mainActivity ).also { it.orientation =
+                LinearLayoutManager.HORIZONTAL}
+
     }
 
     override fun observeEvent() {
