36c96f42cdfbb1cdb1d4293dd0d3c5a336e83d78
유현명 <aahspringaa4@naver.com>
2022-02-18 17:35:11 +0900

    ⚡️:: 코드 삭제 및 Login View 구현

--- presentation/src/main/java/com/semicolon/walkhub/ui/login/LoginActivity.kt
+++ presentation/src/main/java/com/semicolon/walkhub/ui/login/LoginActivity.kt
@@ -3,7 +3,10 @@ package com.semicolon.walkhub.ui.login
 import android.Manifest
 import android.content.Intent
 import android.os.Build
-import androidx.lifecycle.MutableLiveData
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.example.nms_android_v1.base.BaseActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.fitness.FitnessOptions
@@ -12,20 +15,40 @@ import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.normal.TedPermission
 import com.semicolon.walkhub.R
 import com.semicolon.walkhub.databinding.ActivityLoginBinding
-import com.semicolon.data.local.datasource.LocalExerciseDataSourceImpl
-import com.semicolon.data.local.param.PeriodParam
-import com.semicolon.data.local.storage.ExerciseInfoDataStorageImpl
-import com.semicolon.data.local.storage.FitnessDataStorageImpl
-import com.semicolon.domain.entity.exercise.DailyExerciseEntity
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import org.threeten.bp.LocalDate
-import org.threeten.bp.LocalDateTime
-import org.threeten.bp.ZoneId
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(
-    R.layout.activity_login
+   R.layout.activity_login
 ) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        binding.btLogin.setOnClickListener {
+            loginToast()
+        }
+
+        binding.btLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun loginToast() {
+        val id = binding.etId.text
+        val password = binding.etPassword.text
+        if (id.isEmpty()) {
+            Toast.makeText(this, "아이디를 입력해주세요", Toast.LENGTH_SHORT).show()
+        }
+
+        else if (id.isNotEmpty() && password.isEmpty()){
+            Toast.makeText(this, "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+        }
+
+        else if (id.isNotEmpty() && password.isNotEmpty()){
+
+        }
+    }
 
     private val fitnessOptions =
         FitnessOptions.builder()
@@ -35,55 +58,6 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(
             .addDataType(DataType.AGGREGATE_CALORIES_EXPENDED)
             .addDataType(DataType.TYPE_LOCATION_SAMPLE)
             .build()
-
-    // TODO: 나중에 지워
-    private val liveData = MutableLiveData<DailyExerciseEntity>()
-
-    override fun initView() {
-        requestPermissions()
-
-        // TODO: 나중에 지워
-        liveData.observe(this) {
-            binding.steps.setText("걸음 수 : ${it.stepCount.toString()}회")
-            binding.times.setText("운동 시간 : ${it.exerciseTimeAsMilli.toString()}ms")
-            binding.distance.setText("이동 거리 : ${it.traveledDistanceAsMeter.toString()}m")
-            binding.calories.setText("소모한 칼로리 : ${it.burnedKilocalories.toString()}cals")
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        // TODO: 이 이래는 테스트하려고 임시로 써놓은 코드임
-        if (requestCode != 0 && resultCode != RESULT_OK) return
-        val a = LocalExerciseDataSourceImpl(
-            FitnessDataStorageImpl(this),
-            ExerciseInfoDataStorageImpl(this)
-        )
-        val period = PeriodParam(
-            LocalDate.now().atStartOfDay().atZone(ZoneId.systemDefault()).toEpochSecond(),
-            LocalDateTime.now().atZone(ZoneId.systemDefault()).toEpochSecond()
-        )
-
-        GlobalScope.launch {
-            a.startRecordExercise()
-        }
-
-        GlobalScope.launch {
-            a.fetchDailyExerciseRecordAsFlow().collect {
-                liveData.postValue(it)
-                println(it.toString())
-            }
-        }
-
-        GlobalScope.launch {
-            println(a.fetchWalkRecord(period).toString() + " walk record with period")
-        }
-
-        GlobalScope.launch {
-            println(a.fetchLocationRecord(period).toString()+ " location record with period")
-        }
-        // TODO: 나중에 로그인 개발할떄 지워줘
-    }
 
     private fun requestPermissions() {
         val permissionListener = object : PermissionListener {
@@ -120,5 +94,9 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(
             GoogleSignIn.getAccountForExtension(this, fitnessOptions),
             fitnessOptions
         )
+    }
+
+    override fun initView() {
+
     }
 }
