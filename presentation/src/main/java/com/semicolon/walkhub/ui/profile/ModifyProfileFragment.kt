43eb915966c18e5a64bd41b9cc485860bbba9ae4
Leesin0222 <dragon02229@naver.com>
2022-02-20 09:41:31 +0900

    ⚡ :: ModifyProfileFragment 파일 생성

---
+++ presentation/src/main/java/com/semicolon/walkhub/ui/profile/ModifyProfileFragment.kt
@@ -0,0 +1,12 @@
+package com.semicolon.walkhub.ui.profile
+
+import com.semicolon.walkhub.R
+import com.semicolon.walkhub.databinding.FragmentModifyProfileBinding
+import com.semicolon.walkhub.ui.base.BaseFragment
+
+class ModifyProfileFragment : BaseFragment<FragmentModifyProfileBinding>(
+    R.layout.fragment_modify_profile
+) {
+    override fun initView() {
+    }
+}
