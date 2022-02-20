268cc3180cb37087ca056dd0a5fcebde10cef435
Leesin0222 <dragon02229@naver.com>
2022-02-20 09:36:35 +0900

    ⚡ :: noticeFragment 파일 생성

---
+++ presentation/src/main/java/com/semicolon/walkhub/ui/profile/NoticeSettingFragment.kt
@@ -0,0 +1,13 @@
+package com.semicolon.walkhub.ui.profile
+
+import com.semicolon.walkhub.R
+import com.semicolon.walkhub.databinding.FragmentHomeBinding
+import com.semicolon.walkhub.databinding.FragmentNoticeSettingBinding
+import com.semicolon.walkhub.ui.base.BaseFragment
+
+class NoticeSettingFragment : BaseFragment<FragmentNoticeSettingBinding>(
+    R.layout.fragment_notice_setting
+) {
+    override fun initView() {
+    }
+}
