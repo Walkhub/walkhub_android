2fa5de962a1977c4d5299eff4956541ed617e72c
minjun8946 <minjun8946@daum.net>
2022-02-20 23:27:34 +0900

    🧪:: SetBadgesUseCaseUnitTest.kt

--- domain/src/test/java/com/semicolon/domain/usecase/badge/SetBadgeUseCaseUnitTest.kt
+++ domain/src/test/java/com/semicolon/domain/usecase/badge/SetBadgeUseCaseUnitTest.kt
@@ -1,4 +1,28 @@
 package com.semicolon.domain.usecase.badge
 
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import com.semicolon.domain.repository.BadgeRepository
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
 class SetBadgeUseCaseUnitTest {
+
+    private val badgeRepository = mock<BadgeRepository>()
+
+    private val setBadgeUseCase = SetBadgeUseCase(badgeRepository)
+
+    @Test
+    fun testSetBadgeUseCase() {
+        val badgeId = 1
+        runBlocking {
+            val useCaseResult = setBadgeUseCase.execute(badgeId)
+            assertEquals(Unit, useCaseResult)
+
+            verify(badgeRepository, times(1)).setBadge(badgeId)
+        }
+    }
 }
