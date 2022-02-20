55e1be8aafbffa337d798f94e25d5e59fb3d559c
shw0471 <shw0471@naver.com>
2022-02-21 04:54:59 +0900

    ⚰️ :: delete useless imports

--- presentation/src/main/java/com/semicolon/walkhub/di/NetWorkModule.kt
+++ presentation/src/main/java/com/semicolon/walkhub/di/NetWorkModule.kt
@@ -1,14 +1,10 @@
 package com.semicolon.walkhub.di
 
-import android.content.Context
 import com.semicolon.data.interceptor.AuthorizationInterceptor
-import com.semicolon.data.local.storage.AuthDataStorage
-import com.semicolon.data.local.storage.AuthDataStorageImpl
 import com.semicolon.data.remote.api.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
