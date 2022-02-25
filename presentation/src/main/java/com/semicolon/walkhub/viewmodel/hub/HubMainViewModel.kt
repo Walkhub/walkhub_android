cf34abe20d73e5baabc0a0ea8dcc278a5b7368bd
saehyun <sh00710000@gmail.com>
2022-02-25 20:07:52 +0900

    ⚡ :: fetch school rank

--- presentation/src/main/java/com/semicolon/walkhub/viewmodel/hub/HubMainViewModel.kt
+++ presentation/src/main/java/com/semicolon/walkhub/viewmodel/hub/HubMainViewModel.kt
@@ -2,32 +2,61 @@ package com.semicolon.walkhub.viewmodel.hub
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.semicolon.domain.entity.rank.SchoolRankEntity
 import com.semicolon.domain.enum.DateType
+import com.semicolon.domain.usecase.rank.FetchSchoolRankUseCase
 import com.semicolon.walkhub.ui.hub.model.HubSchoolRankData
 import com.semicolon.walkhub.util.MutableEventFlow
 import com.semicolon.walkhub.util.asEventFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HubMainViewModel @Inject constructor(
+    private val fetchSchoolRankUseCase: FetchSchoolRankUseCase
 ): ViewModel() {
 
     private val _eventFlow = MutableEventFlow<Event>()
     val eventFlow = _eventFlow.asEventFlow()
 
     fun fetchSchoolRank(dateType: DateType) {
-        val mySchool = HubSchoolRankData.MySchool(1, "대덕소프트웨어마이스터고등학교", "https://cdn.discordapp.com/attachments/813414875241513010/945222326285369374/R9XM6uqHeMKjQAAAABJRU5ErkJggg.png", 1123213, 1, 1)
-        val otherSchool: List<HubSchoolRankData.OtherSchool> = listOf(
-            HubSchoolRankData.OtherSchool(2, "대전문지중학교", 1, 1312, "https://cdn.discordapp.com/attachments/813414875241513010/945222326285369374/R9XM6uqHeMKjQAAAABJRU5ErkJggg.png", 1123123),
-            HubSchoolRankData.OtherSchool(3, "대전가오고등학교", 2, 151, "https://cdn.discordapp.com/attachments/813414875241513010/945222326285369374/R9XM6uqHeMKjQAAAABJRU5ErkJggg.png", 14124),
-            HubSchoolRankData.OtherSchool(4, "대전가오중학교", 3, 918, "https://cdn.discordapp.com/attachments/813414875241513010/945222326285369374/R9XM6uqHeMKjQAAAABJRU5ErkJggg.png", 11614)
-        )
-        val data = HubSchoolRankData(mySchool, otherSchool)
+        viewModelScope.launch {
+            kotlin.runCatching {
+                fetchSchoolRankUseCase.execute(dateType).collect {
+                    event(Event.FetchSchoolRank(it.toData()))
+                }
+            }
+        }
 
-        event(Event.FetchSchoolRank(data))
     }
+
+    private fun SchoolRankEntity.MySchoolRank.toData() =
+        HubSchoolRankData.MySchool (
+            schoolId = schoolId,
+            name = name,
+            logoImageUrl = logoImageUrl,
+            walkCount = walkCount,
+            grade = grade,
+            classNum = classNum
+        )
+
+    private fun SchoolRankEntity.SchoolRank.toData() =
+        HubSchoolRankData.OtherSchool (
+            schoolId = schoolId,
+            name = name,
+            ranking = ranking,
+            studentCount = studentCount,
+            logoImageUrl = logoImageUrl,
+            walkCount = walkCount
+        )
+
+    private fun SchoolRankEntity.toData() =
+        HubSchoolRankData(
+            mySchoolRank.toData(),
+            schoolList = schoolRankList.map { it.toData() }
+        )
 
     private fun event(event: Event) {
         viewModelScope.launch {
