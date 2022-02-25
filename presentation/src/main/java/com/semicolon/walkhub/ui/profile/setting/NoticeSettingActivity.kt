4b154e575e99e3c035840368c61740acfd32f70c
Leesin0222 <dragon02229@naver.com>
2022-02-25 23:16:17 +0900

    ⚙️ :: Activity 파일 설정

---
+++ presentation/src/main/java/com/semicolon/walkhub/ui/profile/setting/NoticeSettingActivity.kt
@@ -0,0 +1,22 @@
+package com.semicolon.walkhub.ui.profile.setting
+
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.semicolon.walkhub.R
+import com.semicolon.walkhub.databinding.ActivityNoticeSettingBinding
+import com.semicolon.walkhub.ui.base.BaseActivity
+
+class NoticeSettingActivity : BaseActivity<ActivityNoticeSettingBinding>(
+    R.layout.activity_notice_setting
+) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_notice_setting)
+
+    }
+
+    override fun initView() {
+
+    }
+}
