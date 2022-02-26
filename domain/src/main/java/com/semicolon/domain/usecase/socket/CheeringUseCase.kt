1862bd694875a757ae4720fafb74b78270fc67f1
minjun8946 <minjun8946@daum.net>
2022-02-26 23:21:37 +0900

    ⚡️:: cheering use case

---
+++ domain/src/main/java/com/semicolon/domain/usecase/socket/CheeringUseCase.kt
@@ -0,0 +1,14 @@
+package com.semicolon.domain.usecase.socket
+
+import com.semicolon.domain.repository.SocketRepository
+import com.semicolon.domain.usecase.UseCase
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class CheeringUseCase @Inject constructor(
+    private val socketRepository: SocketRepository
+): UseCase<Int,Flow<String>>() {
+
+    override suspend fun execute(data: Int): Flow<String> =
+        socketRepository.cheering(data)
+}
