c43ac6a9447a99b5301023efa0b001030eefd5d5
minjun8946 <minjun8946@daum.net>
2022-02-26 23:41:04 +0900

    ⚡️:: add net work module

--- presentation/src/main/java/com/semicolon/walkhub/di/NetWorkModule.kt
+++ presentation/src/main/java/com/semicolon/walkhub/di/NetWorkModule.kt
@@ -2,10 +2,14 @@ package com.semicolon.walkhub.di
 
 import com.semicolon.data.interceptor.AuthorizationInterceptor
 import com.semicolon.data.remote.api.*
+import com.semicolon.data.remote.datasource.RemoteSocketDataSource
+import com.semicolon.data.remote.datasource.RemoteSocketDataSourceImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.socket.client.IO
+import io.socket.client.Socket
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -15,6 +19,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 @InstallIn(SingletonComponent::class)
 object NetWorkModule {
     private const val BASE_URL = "https://server.walkhub.co.kr"
+    private const val SOCKET_BASE_URL = "http://211.38.86.92:8081/socket.io"
 
     @Provides
     fun provideHttpLoggingInterceptor() = HttpLoggingInterceptor()
@@ -34,6 +39,22 @@ object NetWorkModule {
             .build()
 
     @Provides
+    fun provideOptions(
+        okHttpClient: OkHttpClient
+    ): IO.Options {
+        val options = IO.Options()
+        options.webSocketFactory = okHttpClient
+        options.callFactory = okHttpClient
+        return options
+    }
+
+    @Provides
+    fun provideSocket(
+        options: IO.Options
+    ): Socket =
+        IO.socket(SOCKET_BASE_URL,options)
+
+    @Provides
     fun provideRetrofit(
         okHttpClient: OkHttpClient
     ): Retrofit =
@@ -44,38 +65,44 @@ object NetWorkModule {
             .build()
 
     @Provides
-    fun provideChallengeApi(retrofit: Retrofit) : ChallengeApi =
+    fun provideRemoteSocketDataSource(
+        socket: Socket
+    ): RemoteSocketDataSource =
+        RemoteSocketDataSourceImpl(socket)
+
+    @Provides
+    fun provideChallengeApi(retrofit: Retrofit): ChallengeApi =
         retrofit.create(ChallengeApi::class.java)
 
     @Provides
-    fun provideExerciseApi(retrofit: Retrofit) : ExerciseApi =
+    fun provideExerciseApi(retrofit: Retrofit): ExerciseApi =
         retrofit.create(ExerciseApi::class.java)
 
     @Provides
-    fun provideImageApi(retrofit: Retrofit) : ImagesApi =
+    fun provideImageApi(retrofit: Retrofit): ImagesApi =
         retrofit.create(ImagesApi::class.java)
 
     @Provides
-    fun provideNoticeApi(retrofit: Retrofit) : NoticesApi =
+    fun provideNoticeApi(retrofit: Retrofit): NoticesApi =
         retrofit.create(NoticesApi::class.java)
 
     @Provides
-    fun provideNotificationApi(retrofit: Retrofit) : NotificationApi =
+    fun provideNotificationApi(retrofit: Retrofit): NotificationApi =
         retrofit.create(NotificationApi::class.java)
 
     @Provides
-    fun provideRankApi(retrofit: Retrofit) : RankApi =
+    fun provideRankApi(retrofit: Retrofit): RankApi =
         retrofit.create(RankApi::class.java)
 
     @Provides
-    fun provideSchoolApi(retrofit: Retrofit) : SchoolApi =
+    fun provideSchoolApi(retrofit: Retrofit): SchoolApi =
         retrofit.create(SchoolApi::class.java)
 
     @Provides
-    fun provideUserApi(retrofit: Retrofit) : UserApi =
+    fun provideUserApi(retrofit: Retrofit): UserApi =
         retrofit.create(UserApi::class.java)
 
     @Provides
-    fun provideLevelApi(retrofit: Retrofit) : LevelApi =
+    fun provideLevelApi(retrofit: Retrofit): LevelApi =
         retrofit.create(LevelApi::class.java)
 }
