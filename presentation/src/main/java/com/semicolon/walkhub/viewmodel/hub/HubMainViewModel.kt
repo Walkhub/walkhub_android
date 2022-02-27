363d6d43d39051f0385f4c47242dca7fe2d897c1
saehyun <sh00710000@gmail.com>
2022-02-27 22:35:11 +0900

    ⚡ :: hub main viewmodel

--- presentation/src/main/java/com/semicolon/walkhub/viewmodel/hub/HubMainViewModel.kt
+++ presentation/src/main/java/com/semicolon/walkhub/viewmodel/hub/HubMainViewModel.kt
@@ -1,15 +1,21 @@
 package com.semicolon.walkhub.viewmodel.hub
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.semicolon.domain.entity.rank.SchoolRankEntity
 import com.semicolon.domain.enum.DateType
+import com.semicolon.domain.exception.basic.NoInternetException
+import com.semicolon.domain.exception.basic.UnknownException
+import com.semicolon.domain.param.user.PostUserSignInParam
 import com.semicolon.domain.usecase.rank.FetchSchoolRankUseCase
+import com.semicolon.domain.usecase.user.PostUserSignInUseCase
+import com.semicolon.walkhub.R
 import com.semicolon.walkhub.ui.hub.model.HubSchoolRankData
 import com.semicolon.walkhub.util.MutableEventFlow
 import com.semicolon.walkhub.util.asEventFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -27,9 +33,13 @@ class HubMainViewModel @Inject constructor(
                 fetchSchoolRankUseCase.execute(dateType).collect {
                     event(Event.FetchSchoolRank(it.toData()))
                 }
+            }.onFailure {
+                when(it) {
+                    is NoInternetException -> event(Event.ErrorMessage("인터넷을 사용할 수 없습니다"))
+                    else -> event(Event.ErrorMessage("알 수 없는 에러가 발생했습니다.\\n잠시 후 다시 시도해주세요."))
+                }
             }
         }
-
     }
 
     private fun SchoolRankEntity.MySchoolRank.toData() =
@@ -66,5 +76,6 @@ class HubMainViewModel @Inject constructor(
 
     sealed class Event {
         data class FetchSchoolRank(val hubSchoolRankData: HubSchoolRankData) : Event()
+        data class ErrorMessage(val message: String) : Event()
     }
 }
