795a29b1c0a67a2897643ea784336ea70a9593e9
유현명 <aahspringaa4@naver.com>
2022-02-27 22:56:59 +0900

    ⚡️ :: 성별 입력 예외 처리

--- presentation/src/main/java/com/semicolon/walkhub/ui/register/ScanHealthInformationActivity.kt
+++ presentation/src/main/java/com/semicolon/walkhub/ui/register/ScanHealthInformationActivity.kt
@@ -1,20 +1,25 @@
 package com.semicolon.walkhub.ui.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
 import com.semicolon.walkhub.R
 import com.semicolon.walkhub.databinding.ActivityScanHealthInformationBinding
+import com.semicolon.walkhub.ui.MainActivity
 import com.semicolon.walkhub.ui.base.BaseActivity
 
 class ScanHealthInformationActivity : BaseActivity<ActivityScanHealthInformationBinding>(
     R.layout.activity_scan_health_information
 ) {
+
+    private var a: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding.btContinue.setOnClickListener {
-            finishRegister()
+            notFinishRegister()
         }
 
         binding.btMan.setOnClickListener {
@@ -30,9 +35,26 @@ class ScanHealthInformationActivity : BaseActivity<ActivityScanHealthInformation
 
     }
 
-    private fun finishRegister() {
+    private fun notFinishRegister() {
         if(binding.eTcm.length() < 1 || binding.etkg.length() < 1 ) {
             showLongToast("신체 정보를 올바르게 입력해주세요.")
+        }
+
+        else {
+            finishRegister()
+        }
+    }
+
+    private fun finishRegister(){
+        when(a) {
+            true -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+
+            false -> {
+                showShortToast("성별을 선택해주세요")
+            }
         }
     }
 
@@ -48,6 +70,7 @@ class ScanHealthInformationActivity : BaseActivity<ActivityScanHealthInformation
                 applicationContext,
                 R.drawable.buttondesign
             )
+        a = true
     }
 
     private fun changeGenderWo() {
@@ -62,5 +85,7 @@ class ScanHealthInformationActivity : BaseActivity<ActivityScanHealthInformation
                 applicationContext,
                 R.drawable.buttondesign
             )
+
+        a = true
     }
 }
