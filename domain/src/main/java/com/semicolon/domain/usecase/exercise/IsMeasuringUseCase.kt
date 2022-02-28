eae2605987cba1ab2832985289b5821fc6236242
shw0471 <shw0471@naver.com>
2022-02-28 10:11:00 +0900

    ⚡️ :: update IsMeasuringUseCase

--- domain/src/main/java/com/semicolon/domain/usecase/exercise/IsMeasuringUseCase.kt
+++ domain/src/main/java/com/semicolon/domain/usecase/exercise/IsMeasuringUseCase.kt
@@ -1,13 +1,14 @@
 package com.semicolon.domain.usecase.exercise
 
+import com.semicolon.domain.enum.MeasuringState
 import com.semicolon.domain.repository.ExerciseRepository
 import com.semicolon.domain.usecase.UseCase
 import javax.inject.Inject
 
 class IsMeasuringUseCase @Inject constructor(
     private val exerciseRepository: ExerciseRepository
-) : UseCase<Unit, Boolean>() {
+) : UseCase<Unit, MeasuringState>() {
 
-    override suspend fun execute(data: Unit): Boolean =
+    override suspend fun execute(data: Unit): MeasuringState =
         exerciseRepository.isMeasuring()
 }
