a5bea64e164aa9e30fe2a6e42b3f45ad0adfe4e8
shw0471 <shw0471@naver.com>
2022-02-28 10:10:11 +0900

    ⚡️ :: update isMeasuring() function

--- domain/src/main/java/com/semicolon/domain/repository/ExerciseRepository.kt
+++ domain/src/main/java/com/semicolon/domain/repository/ExerciseRepository.kt
@@ -3,6 +3,7 @@ package com.semicolon.domain.repository
 import com.semicolon.domain.entity.exercise.DailyExerciseEntity
 import com.semicolon.domain.entity.exercise.ExerciseAnalysisResultEntity
 import com.semicolon.domain.entity.exercise.ExerciseRecordEntity
+import com.semicolon.domain.enum.MeasuringState
 import com.semicolon.domain.param.exercise.FinishMeasureExerciseParam
 import com.semicolon.domain.param.exercise.StartMeasureExerciseParam
 import kotlinx.coroutines.flow.Flow
@@ -19,7 +20,7 @@ interface ExerciseRepository {
 
     suspend fun finishMeasureExercise(finishMeasureExerciseParam: FinishMeasureExerciseParam)
 
-    suspend fun isMeasuring(): Boolean
+    suspend fun isMeasuring(): MeasuringState
 
     suspend fun startRecordExercise()
 
