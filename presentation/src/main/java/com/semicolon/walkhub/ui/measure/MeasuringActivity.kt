67912084ed2b96ca53b43a5c4306967950d01844
Jaewon <turitus@naver.com>
2022-03-03 01:58:01 +0900

    ⚡️:: measureing activity

---
+++ presentation/src/main/java/com/semicolon/walkhub/ui/measure/MeasuringActivity.kt
@@ -0,0 +1,11 @@
+package com.semicolon.walkhub.ui.measure
+
+import com.semicolon.walkhub.R
+import com.semicolon.walkhub.databinding.ActivityMeasuringBinding
+import com.semicolon.walkhub.ui.base.BaseActivity
+
+class MeasuringActivity : BaseActivity<ActivityMeasuringBinding>(R.layout.activity_measuring) {
+
+    override fun initView() {
+    }
+}
