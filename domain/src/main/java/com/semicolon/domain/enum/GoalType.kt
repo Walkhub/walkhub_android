84b1303deb1c63f11d79231bdab1865fa3ebac88
shw0471 <shw0471@naver.com>
2022-03-02 12:08:03 +0900

    ⚡️ :: add extend function

--- domain/src/main/java/com/semicolon/domain/enum/GoalType.kt
+++ domain/src/main/java/com/semicolon/domain/enum/GoalType.kt
@@ -4,3 +4,9 @@ enum class GoalType(val value: String) {
     DISTANCE("D"),
     WALK_COUNT("W")
 }
+
+fun String.toMeasureGoalType() =
+    when(this) {
+        "D" -> GoalType.DISTANCE
+        else -> GoalType.WALK_COUNT
+    }
