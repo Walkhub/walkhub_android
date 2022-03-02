887c22356320fb9a03d805b396109334ac5a3f58
shw0471 <shw0471@naver.com>
2022-03-02 11:13:00 +0900

    ⚡️ :: add extend functions

--- data/src/main/java/com/semicolon/data/local/entity/exercise/LocationRecordRoomEntity.kt
+++ data/src/main/java/com/semicolon/data/local/entity/exercise/LocationRecordRoomEntity.kt
@@ -5,8 +5,19 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class LocationRecordRoomEntity(
-    @PrimaryKey(autoGenerate = true) val id: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val latitude: Double,
     val longitude: Double
 )
 
+fun LocationRecordRoomEntity.toEntity() =
+    LocationRecordEntity(
+        latitude = latitude,
+        longitude = longitude
+    )
+
+fun LocationRecordEntity.toRoomEntity() =
+    LocationRecordRoomEntity(
+        latitude = latitude,
+        longitude = longitude
+    )
