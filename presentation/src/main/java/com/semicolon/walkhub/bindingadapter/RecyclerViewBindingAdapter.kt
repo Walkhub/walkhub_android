823e724f3d77e01d00b805c2c4dc9b7df274e016
Jaewon <turitus@naver.com>
2022-03-03 01:17:19 +0900

    ⚡️:: recycler view binding adapter

---
+++ presentation/src/main/java/com/semicolon/walkhub/bindingadapter/RecyclerViewBindingAdapter.kt
@@ -0,0 +1,19 @@
+package com.semicolon.walkhub.bindingadapter
+
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.semicolon.walkhub.adapter.RecyclerViewAdapter
+import com.semicolon.walkhub.adapter.RecyclerViewItem
+
+@BindingAdapter("recyclerItems")
+fun RecyclerView.setList(list: List<RecyclerViewItem>?) {
+    if (adapter == null) {
+        adapter = RecyclerViewAdapter()
+        layoutManager = LinearLayoutManager(context)
+    }
+
+    if (list != null) {
+        (adapter as? RecyclerViewAdapter)?.updateData(list)
+    }
+}
