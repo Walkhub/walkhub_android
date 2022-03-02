6727bf87ed4b17cd141395e5626f0b6863fc2ca9
Leesin0222 <dragon02229@naver.com>
2022-03-02 19:12:58 +0900

    ⚡ :: toggleSwitch import 추가

--- presentation/src/main/java/com/semicolon/walkhub/ui/profile/setting/NoticeSettingActivity.kt
+++ presentation/src/main/java/com/semicolon/walkhub/ui/profile/setting/NoticeSettingActivity.kt
@@ -3,6 +3,7 @@ package com.semicolon.walkhub.ui.profile.setting
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.semicolon.walkhub.R
+import com.semicolon.walkhub.customview.ToggleSwitch
 import com.semicolon.walkhub.databinding.ActivityNoticeSettingBinding
 import com.semicolon.walkhub.ui.base.BaseActivity
 
