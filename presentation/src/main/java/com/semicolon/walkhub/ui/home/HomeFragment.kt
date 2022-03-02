001e7e52d1e40da2ef42824ccffb0ec121a73289
유현명 <aahspringaa4@naver.com>
2022-03-03 00:23:48 +0900

    ⚡️:: Home View 구현

--- presentation/src/main/java/com/semicolon/walkhub/ui/home/HomeFragment.kt
+++ presentation/src/main/java/com/semicolon/walkhub/ui/home/HomeFragment.kt
@@ -1,12 +1,82 @@
 package com.semicolon.walkhub.ui.home
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.semicolon.walkhub.R
 import com.semicolon.walkhub.databinding.FragmentHomeBinding
+import com.semicolon.walkhub.extensions.repeatOnStarted
 import com.semicolon.walkhub.ui.base.BaseFragment
+import com.semicolon.walkhub.ui.home.model.HomeData
+import com.semicolon.walkhub.ui.home.model.RankData
+import com.semicolon.walkhub.util.isVisible
+import com.semicolon.walkhub.util.loadCircleFromUrl
+import com.semicolon.walkhub.viewmodel.home.HomeViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class HomeFragment : BaseFragment<FragmentHomeBinding>(
+@AndroidEntryPoint
+class HomeFragment: BaseFragment<FragmentHomeBinding>(
     R.layout.fragment_home
 ) {
+
+    private val vm: HomeViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        vm.fetchHomeValue()
+        vm.fetchSchoolRank()
+
+        repeatOnStarted {
+            vm.eventFlow.collect { event -> handleEvent(event) }
+        }
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    private fun handleEvent(event: HomeViewModel.Event) = when (event) {
+        is HomeViewModel.Event.FetchHomeValue -> {
+            setHomeValue(event.homeData)
+        }
+
+        is HomeViewModel.Event.FetchSchoolRank -> {
+            setSchoolRank(event.rankData)
+        }
+
+        is HomeViewModel.Event.ErrorMessage -> {
+            showShortToast(event.message)
+        }
+    }
+
     override fun initView() {
+
+    }
+
+    private fun setHomeValue(homeData: HomeData) {
+        val calories = String.format("%.1f", homeData.burnedKilocalories)
+        binding.tvWalk.text = homeData.stepCount.toString()
+        binding.tvCalories.text = calories
+        binding.tvDistance.text = homeData.traveledDistanceAsMeter.toString()
+        binding.cpb.progress = homeData.stepCount.toFloat()
+        binding.cpb.progressMax = 7000f
+        binding.tvTime.text = (homeData.exerciseTimeAsMilli/60000).toString()
+    }
+
+    private fun setSchoolRank(rankData: List<RankData>) {
+        binding.third.isVisible()
+        binding.tvName.text = rankData[0].name
+        binding.tvName2.text = rankData[1].name
+        binding.tvName3.text = rankData[2].name
+        binding.tvWalkCount.text = rankData[0].walkCount.toString()
+        binding.tvWalkCount2.text = rankData[1].walkCount.toString()
+        binding.tvWalkCount3.text = rankData[2].walkCount.toString()
+        rankData[0].logoImageUrl?.let { binding.ivProfile.loadCircleFromUrl(it) }
+        rankData[1].logoImageUrl?.let { binding.ivProfile2.loadCircleFromUrl(it) }
+        rankData[2].logoImageUrl?.let { binding.ivProfile3.loadCircleFromUrl(it) }
     }
 }
