3928b9613d425ee6529d5418eacbab5401ed3244
minjun8946 <minjun8946@daum.net>
2022-03-03 09:46:31 +0900

    ⚡️:: remoteSocketDataSourceImpl

--- data/src/main/java/com/semicolon/data/remote/datasource/RemoteSocketDataSourceImpl.kt
+++ data/src/main/java/com/semicolon/data/remote/datasource/RemoteSocketDataSourceImpl.kt
@@ -10,23 +10,42 @@ class RemoteSocketDataSourceImpl @Inject constructor(
     private val socket: Socket
 ) : RemoteSocketDataSource {
 
-    var message: String = ""
+    var receiveMessage: String = ""
+
+    var errorMessage: String = ""
+    var code: String = ""
+    var status: Int = 0
 
     private val onReceiveMessage: Emitter.Listener = Emitter.Listener {
-        message = it[0] as String
+        val json = JSONObject(it[0].toString())
+        receiveMessage = json.getString("message")
+
+    }
+
+    private val onErrorMessage: Emitter.Listener = Emitter.Listener {
+        val json = JSONObject(it[0].toString())
+        status = json.getInt("status")
+        code = json.getString("code")
+        errorMessage = json.getString("message")
     }
 
     override fun sendCheering(userId: Int) {
         val data = JSONObject()
-        data.put("user_id", userId)
+        socket.connect()
+        data.put("user_id", 10)
         socket.emit("cheering", data)
     }
 
     override fun receiveCheering(): String {
         socket.on("new_cheering", onReceiveMessage)
-        socket.on("error", onReceiveMessage)
 
-        return message
+        return receiveMessage
+    }
+
+    override fun receiveError(): List<String> {
+        socket.on("error", onErrorMessage)
+
+        return listOf(errorMessage,code,status.toString())
     }
 
     override fun disconnected() {
