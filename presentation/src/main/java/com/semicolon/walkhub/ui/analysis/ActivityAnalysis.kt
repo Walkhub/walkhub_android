1e4fdd5d11f8b03ac96edeb58b0d688ff1b9907d
shw0471 <shw0471@naver.com>
2022-03-03 10:28:20 +0900

    ⚡️ :: update ActivityAnalysis UI

--- presentation/src/main/java/com/semicolon/walkhub/ui/analysis/ActivityAnalysis.kt
+++ presentation/src/main/java/com/semicolon/walkhub/ui/analysis/ActivityAnalysis.kt
@@ -11,23 +11,76 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.semicolon.domain.entity.exercise.DailyExerciseEntity
+import com.semicolon.domain.entity.exercise.ExerciseAnalysisResultEntity
+import com.semicolon.domain.entity.level.LevelEntity
 import com.semicolon.walkhub.ui.base.Body2
 import com.semicolon.walkhub.ui.base.Body3
 import com.semicolon.walkhub.ui.base.Subtitle4
 import com.semicolon.walkhub.ui.base.gray800
+import com.semicolon.walkhub.viewmodel.analysis.ActivityAnalysisViewModel
+import kotlinx.coroutines.launch
 import org.threeten.bp.LocalDate
-import java.util.*
-import kotlin.collections.ArrayList
+import org.threeten.bp.ZoneId
 
 @Composable
 fun ActivityAnalysis(onBackButtonClick: () -> Unit) {
-    // TODO: ViewModel 완성 후 더미데이터 지우고 값 넣기
+    val viewModel: ActivityAnalysisViewModel = hiltViewModel()
+    viewModel.fetchDailyExerciseRecordUseCase()
+    viewModel.fetchExerciseAnalysisResult()
+    viewModel.fetchLevelList()
+    ActivityAnalysis(onBackButtonClick, viewModel)
+}
+
+@Composable
+fun ActivityAnalysis(onBackButtonClick: () -> Unit, viewModel: ActivityAnalysisViewModel) {
     val scrollState = rememberScrollState()
-    val random = Random()
-    val items = ArrayList<GraphItem>()
-    for (i in 1..28) {
-        items.add(GraphItem(random.nextInt(15000), LocalDate.of(2022, 1, i)))
+    var graphState by remember { mutableStateOf(GraphType.WEEKLY) }
+    var analysisResult by remember {
+        mutableStateOf(
+            ExerciseAnalysisResultEntity(
+                0,
+                listOf(1..28).flatten(),
+                0,
+                0,
+                0,
+                0f,
+                0
+            )
+        )
     }
+    var dailyExercise by remember {
+        mutableStateOf(
+            DailyExerciseEntity(
+                0, 0, 0, 0f
+            )
+        )
+    }
+    var cardList by remember { mutableStateOf(listOf<LevelEntity>()) }
+    var card by remember {
+        mutableStateOf(
+            LevelEntity(
+                0, "", "", 0, "", 0, ""
+            )
+        )
+    }
+    card = if (cardList.isNotEmpty())
+        cardList.last { it.calories <= dailyExercise.burnedKilocalories } else card
+    val composableScope = rememberCoroutineScope()
+    LaunchedEffect(analysisResult, dailyExercise) {
+        composableScope.launch {
+            viewModel.eventFlow.collect {
+                if (it is ActivityAnalysisViewModel.Event.AnalysisResult)
+                    analysisResult = it.result
+                if (it is ActivityAnalysisViewModel.Event.ExerciseRecord)
+                    dailyExercise = it.record
+                if (it is ActivityAnalysisViewModel.Event.Level)
+                    cardList = it.level
+            }
+        }
+    }
+
     Box(
         Modifier
             .background(Color(0xFFF9F9F9))
@@ -47,42 +100,7 @@ fun ActivityAnalysis(onBackButtonClick: () -> Unit) {
                     .wrapContentHeight()
                     .padding(horizontal = 40.dp)
             ) {
-                val date = LocalDate.now()
-                Body2(
-                    text = "${date.month.value}월 ${date.dayOfMonth}일 (${date.dayOfWeek.toKoreanString()})",
-                    color = Color(0xFF8E8E8E)
-                )
-                Spacer(modifier = Modifier.size(12.dp))
-                Body2(text = "걸음 수", color = gray800)
-                Row(
-                    verticalAlignment = Alignment.Bottom
-                ) {
-                    Subtitle4(text = "6472")
-                    Spacer(modifier = Modifier.size(4.dp))
-                    Body2(
-                        text = "/7000 걸음", color = gray800,
-                        modifier = Modifier.padding(bottom = 1.dp)
-                    )
-                }
-                Spacer(modifier = Modifier.size(4.dp))
-                ProgressBar(
-                    percent = 0.924f,
-                    backgroundColor = Color(0xFFE5E5E5),
-                    modifier = Modifier.fillMaxWidth()
-                )
-                Spacer(modifier = Modifier.size(12.dp))
-                Body2(text = "칼로리 소모", color = gray800)
-                Body3(text = "건강정보를 기준으로 측정했어요", color = gray800)
-                Row(
-                    verticalAlignment = Alignment.Bottom
-                ) {
-                    Subtitle4(text = "203")
-                    Spacer(modifier = Modifier.size(4.dp))
-                    Body2(
-                        text = "Kcal", color = gray800,
-                        modifier = Modifier.padding(bottom = 1.dp)
-                    )
-                }
+                StepsAndCalories(dailyExercise, analysisResult)
             }
             Box(
                 modifier = Modifier
@@ -90,50 +108,7 @@ fun ActivityAnalysis(onBackButtonClick: () -> Unit) {
                     .wrapContentHeight(),
                 contentAlignment = Alignment.TopCenter
             ) {
-                Spacer(
-                    modifier = Modifier
-                        .padding(top = 38.dp, bottom = 30.dp)
-                        .height(28.dp)
-                        .width(1.dp)
-                        .background(Color(0xFFE3E3E3))
-                )
-                Row(
-                    modifier = Modifier
-                        .padding(top = 22.dp)
-                        .wrapContentSize(),
-                    verticalAlignment = Alignment.Bottom
-                ) {
-                    Body2(text = "거리", color = gray800)
-                    Spacer(modifier = Modifier.width(138.dp))
-                    Body2(text = "시간", color = gray800)
-                }
-                Row(
-                    modifier = Modifier
-                        .padding(top = 46.dp)
-                        .wrapContentSize(),
-                    verticalAlignment = Alignment.Bottom
-                ) {
-                    Subtitle4(text = "5.24", color = gray800)
-                    Spacer(modifier = Modifier.width(4.dp))
-                    Body2(
-                        text = "km", color = gray800,
-                        modifier = Modifier.padding(bottom = 1.dp)
-                    )
-                    Spacer(modifier = Modifier.width(105.dp))
-                    Subtitle4(text = "1", color = gray800)
-                    Spacer(modifier = Modifier.width(2.dp))
-                    Body2(
-                        text = "h", color = gray800,
-                        modifier = Modifier.padding(bottom = 1.dp)
-                    )
-                    Spacer(modifier = Modifier.width(4.dp))
-                    Subtitle4(text = "10", color = gray800)
-                    Spacer(modifier = Modifier.width(2.dp))
-                    Body2(
-                        text = "m", color = gray800,
-                        modifier = Modifier.padding(bottom = 1.dp)
-                    )
-                }
+                DistanceAndTime(dailyExercise)
             }
             Column(
                 modifier = Modifier
@@ -141,13 +116,12 @@ fun ActivityAnalysis(onBackButtonClick: () -> Unit) {
                     .fillMaxWidth(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                var graphState by remember { mutableStateOf(GraphType.WEEKLY) }
                 GraphToggleButton {
                     graphState =
                         if (graphState == GraphType.WEEKLY) GraphType.MONTHLY else GraphType.WEEKLY
                 }
                 Spacer(modifier = Modifier.height(8.dp))
-                WalkGraph(items = items, type = graphState)
+                WalkGraph(graphState, analysisResult.walkCountList)
             }
             Column(
                 modifier = Modifier
@@ -156,21 +130,7 @@ fun ActivityAnalysis(onBackButtonClick: () -> Unit) {
                     .fillMaxWidth()
                     .wrapContentHeight()
             ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Body2(text = "걸음수 총합", color = gray800)
-                    Subtitle4(text = "145382", color = gray800)
-                }
-                Spacer(modifier = Modifier.height(12.dp))
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Body2(text = "평균 걸음수", color = gray800)
-                    Subtitle4(text = "4214", color = gray800)
-                }
+                WalkCount(graphState, analysisResult)
             }
         }
         Column(
@@ -180,17 +140,148 @@ fun ActivityAnalysis(onBackButtonClick: () -> Unit) {
                 onBackButtonClick()
             }
             Spacer(modifier = Modifier.size(12.dp))
-            CaloriesLevelCard(
-                imageUrl = "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/43380b84-6dc9-44fc-8b51-6aef4d9f1faf/커피icon.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220217%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220217T062841Z&X-Amz-Expires=86400&X-Amz-Signature=1d6b84669e154680843a14e264df803d7cbe67a8d4de592dddb1826fcd6e9daf&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22%25EC%25BB%25A4%25ED%2594%25BCicon.png%22&x-id=GetObject",
-                name = "카페 라떼",
-                calories = 180,
-                size = "355ml",
-                level = 7,
-                progress = 0.5f,
-                message = "커피 한 잔 어때요~?"
-            )
+            LevelCard(card)
         }
     }
+}
+
+@Composable
+fun LevelCard(card: LevelEntity) {
+    CaloriesLevelCard(
+        imageUrl = card.foodImageUrl,
+        name = card.foodName,
+        calories = card.calories,
+        size = card.size,
+        level = card.level,
+        progress = 0.5f,
+        message = card.message
+    )
+}
+
+@Composable
+fun WalkCount(graphType: GraphType, analysisResult: ExerciseAnalysisResultEntity) {
+    val items =
+        if (graphType == GraphType.WEEKLY) analysisResult.walkCountList.takeLast(7)
+        else analysisResult.walkCountList
+    val total = items.sumOf { it }.toString()
+    val avg = items.average().toInt().toString()
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Body2(text = "걸음수 총합", color = gray800)
+        Subtitle4(text = total, color = gray800)
+    }
+    Spacer(modifier = Modifier.height(12.dp))
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Body2(text = "평균 걸음수", color = gray800)
+        Subtitle4(text = avg, color = gray800)
+    }
+}
+
+@Composable
+fun StepsAndCalories(
+    dailyExercise: DailyExerciseEntity,
+    analysisResult: ExerciseAnalysisResultEntity
+) {
+    val date = LocalDate.now(ZoneId.systemDefault())
+    val goal = analysisResult.dailyWalkCountGoal
+    val steps = dailyExercise.stepCount
+    val progress = if (goal == 0) 0f else steps.toFloat() / goal
+    Body2(
+        text = "${date.month.value}월 ${date.dayOfMonth}일 (${date.dayOfWeek.toKoreanString()})",
+        color = Color(0xFF8E8E8E)
+    )
+    Spacer(modifier = Modifier.size(12.dp))
+    Body2(text = "걸음 수", color = gray800)
+    Row(
+        verticalAlignment = Alignment.Bottom
+    ) {
+        Subtitle4(text = steps.toString())
+        Spacer(modifier = Modifier.size(4.dp))
+        Body2(
+            text = "/$goal 걸음", color = gray800,
+            modifier = Modifier.padding(bottom = 1.dp)
+        )
+    }
+    Spacer(modifier = Modifier.size(4.dp))
+    ProgressBar(
+        percent = progress,
+        backgroundColor = Color(0xFFE5E5E5),
+        modifier = Modifier.fillMaxWidth()
+    )
+    Spacer(modifier = Modifier.size(12.dp))
+    Body2(text = "칼로리 소모", color = gray800)
+    Body3(text = "건강정보를 기준으로 측정했어요", color = gray800)
+    Row(
+        verticalAlignment = Alignment.Bottom
+    ) {
+        Subtitle4(text = String.format("%.2f", dailyExercise.burnedKilocalories))
+        Spacer(modifier = Modifier.size(4.dp))
+        Body2(
+            text = "Kcal", color = gray800,
+            modifier = Modifier.padding(bottom = 1.dp)
+        )
+    }
+}
+
+@Composable
+fun DistanceAndTime(exerciseEntity: DailyExerciseEntity) {
+    val distance = String.format("%.1f", (exerciseEntity.traveledDistanceAsMeter.toFloat() / 1000))
+    val hour = (exerciseEntity.exerciseTimeAsMilli / 1000 / 60 / 60).toString()
+    val minute = String.format("%02d", (exerciseEntity.exerciseTimeAsMilli / 1000 / 60) % 60)
+    Spacer(
+        modifier = Modifier
+            .padding(top = 38.dp, bottom = 30.dp)
+            .height(28.dp)
+            .width(1.dp)
+            .background(Color(0xFFE3E3E3))
+    )
+    Row(
+        modifier = Modifier
+            .padding(top = 22.dp)
+            .wrapContentSize(),
+        verticalAlignment = Alignment.Bottom
+    ) {
+        Body2(text = "거리", color = gray800)
+        Spacer(modifier = Modifier.width(138.dp))
+        Body2(text = "시간", color = gray800)
+    }
+    Row(
+        modifier = Modifier
+            .padding(top = 46.dp)
+            .wrapContentSize(),
+        verticalAlignment = Alignment.Bottom
+    ) {
+        Subtitle4(text = distance, color = gray800)
+        Spacer(modifier = Modifier.width(4.dp))
+        Body2(
+            text = "km", color = gray800,
+            modifier = Modifier.padding(bottom = 1.dp)
+        )
+        Spacer(modifier = Modifier.width(105.dp))
+        Subtitle4(text = hour, color = gray800)
+        Spacer(modifier = Modifier.width(2.dp))
+        Body2(
+            text = "h", color = gray800,
+            modifier = Modifier.padding(bottom = 1.dp)
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Subtitle4(text = minute, color = gray800)
+        Spacer(modifier = Modifier.width(2.dp))
+        Body2(
+            text = "m", color = gray800,
+            modifier = Modifier.padding(bottom = 1.dp)
+        )
+    }
+}
+
+@Composable
+fun WalkGraph(graphType: GraphType, items: List<Int>) {
+    WalkGraph(items = items.toGraphItemList(), type = graphType)
 }
 
 @Preview(showBackground = true)
