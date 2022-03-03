027ba29e523d8ab41b164f3fd9dc1ff95ca498eb
Jaewon <turitus@naver.com>
2022-03-03 09:19:28 +0900

    ⚡️:: measuring text ui

--- presentation/src/main/java/com/semicolon/walkhub/ui/measure/MeasuringActivity.kt
+++ presentation/src/main/java/com/semicolon/walkhub/ui/measure/MeasuringActivity.kt
@@ -1,11 +1,29 @@
 package com.semicolon.walkhub.ui.measure
 
+import android.view.View
 import com.semicolon.walkhub.R
 import com.semicolon.walkhub.databinding.ActivityMeasuringBinding
 import com.semicolon.walkhub.ui.base.BaseActivity
+import java.util.*
 
 class MeasuringActivity : BaseActivity<ActivityMeasuringBinding>(R.layout.activity_measuring) {
 
     override fun initView() {
+        countDown()
+    }
+
+    private fun countDown() {
+        var count = 3
+        binding.count = count.toString()
+        Timer().schedule(object : TimerTask() {
+            override fun run() {
+                binding.count = count.toString()
+                count -= 1
+                if(count < 0) {
+                    cancel()
+                    binding.measuringReadyCl.visibility = View.INVISIBLE
+                }
+            }
+        }, 0, 1000)
     }
 }
