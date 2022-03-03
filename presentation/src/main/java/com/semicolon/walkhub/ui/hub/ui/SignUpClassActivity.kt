da6dfe0d66d3b76dbe5460025b6af4a9e5f07228
임세현 <sh00710000@gmail.com>
2022-03-03 11:30:39 +0900

    ⚡️ :: fix layout

--- presentation/src/main/java/com/semicolon/walkhub/ui/hub/ui/SignUpClassActivity.kt
+++ presentation/src/main/java/com/semicolon/walkhub/ui/hub/ui/SignUpClassActivity.kt
@@ -3,14 +3,13 @@ package com.semicolon.walkhub.ui.hub.ui
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.View
-import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.semicolon.walkhub.R
 import com.semicolon.walkhub.databinding.ActivitySignUpClassBinding
 import com.semicolon.walkhub.ui.base.BaseActivity
 import com.semicolon.walkhub.util.invisible
 import com.semicolon.walkhub.util.visible
+import com.semicolon.walkhub.util.SoftKeyBoardListener
 
 class SignUpClassActivity : BaseActivity<ActivitySignUpClassBinding>(
     R.layout.activity_sign_up_class
@@ -37,10 +36,15 @@ class SignUpClassActivity : BaseActivity<ActivitySignUpClassBinding>(
         }
 
         binding.etClassCode.setOnPinEnteredListener { str ->
-            if (str.toString().equals("1234567")) {
-                movePage(++page)
-            } else {
-                Toast.makeText(applicationContext, "failed", Toast.LENGTH_SHORT).show()
+            if (str.length <= 7) {
+                if (str.toString() == "1234567") {
+                    movePage(++page)
+                    binding.tvError.invisible()
+                } else {
+                    binding.tvError.visible()
+                    binding.tvError.text = "반을 찾을 수 없어요. 가입코드를 다시 확인해주세요."
+                    binding.etClassCode.text = null
+                }
             }
         }
 
@@ -51,19 +55,21 @@ class SignUpClassActivity : BaseActivity<ActivitySignUpClassBinding>(
                 if (p0?.length!! >= 4) {
                     binding.tvNext.background =
                         ContextCompat.getDrawable(applicationContext, R.drawable.bg_primary_button)
+                    binding.tvSecondNext.background =
+                        ContextCompat.getDrawable(applicationContext, R.color.primary_400)
                 } else {
                     binding.tvNext.background =
                         ContextCompat.getDrawable(
                             applicationContext,
                             R.drawable.bg_primary_button_off
                         )
+                    binding.tvSecondNext.background =
+                        ContextCompat.getDrawable(applicationContext, R.color.gray_300)
                 }
             }
 
             override fun afterTextChanged(p0: Editable?) {}
-
         })
-
     }
 
     private fun movePage(page: Int) {
@@ -75,6 +81,9 @@ class SignUpClassActivity : BaseActivity<ActivitySignUpClassBinding>(
     }
 
     private fun verifyCode() {
+        binding.tvSecondNext.invisible()
+        binding.tvNext.visible()
+
         binding.etNumber.invisible()
         binding.etClassCode.visible()
 
@@ -83,14 +92,22 @@ class SignUpClassActivity : BaseActivity<ActivitySignUpClassBinding>(
     }
 
     private fun classNum() {
+        binding.tvSecondNext.visible()
+        binding.tvNext.invisible()
+
         binding.etNumber.visible()
         binding.etClassCode.invisible()
 
         binding.etTitle.text = "번호 등록"
         binding.etLore.text = "반에서 사용하는 번호를 입력해주세요."
+
+        binding.tvSecondNext.setOnClickListener {
+            movePage(++page)
+        }
     }
 
     private fun signUpClass() {
         finish()
     }
+
 }
