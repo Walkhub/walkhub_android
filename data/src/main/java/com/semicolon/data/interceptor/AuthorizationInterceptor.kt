53030daf2ddad17315805320761d5540dcd82e9c
shw0471 <shw0471@naver.com>
2022-03-08 08:42:42 +0900

    ⚡️ :: set AuthorizationInterceptor throws subclass of IOException

--- data/src/main/java/com/semicolon/data/interceptor/AuthorizationInterceptor.kt
+++ data/src/main/java/com/semicolon/data/interceptor/AuthorizationInterceptor.kt
@@ -3,7 +3,7 @@ package com.semicolon.data.interceptor
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 import com.semicolon.data.local.storage.AuthDataStorage
-import com.semicolon.domain.exception.basic.UnauthorizedException
+import com.semicolon.domain.exception.user.NeedLoginException
 import okhttp3.*
 import org.threeten.bp.LocalDateTime
 import org.threeten.bp.ZoneId
@@ -48,7 +48,7 @@ class AuthorizationInterceptor @Inject constructor(
                 authDataStorage.setAccessToken(token.accessToken)
                 authDataStorage.setRefreshToken(token.refreshToken)
                 authDataStorage.setExpiredAt(token.expiredAt)
-            } else throw UnauthorizedException()
+            } else throw NeedLoginException()
         }
 
         val accessToken = authDataStorage.fetchAccessToken()
