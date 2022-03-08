da8a72d534ec6436b6ef1f1c9143bf9b91ca337a
shw0471 <shw0471@naver.com>
2022-03-08 10:09:33 +0900

    📑 :: add SplashActivity

---
+++ presentation/src/main/java/com/semicolon/walkhub/ui/splash/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.semicolon.walkhub.ui.splash
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class SplashActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
