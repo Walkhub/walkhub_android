c5e1c0079d4726ad1e6f2c12e64e497eace9c6bc
Leesin0222 <dragon02229@naver.com>
2022-03-09 21:49:23 +0900

    ⚡ :: ToggleSwitch 관련 확장함수 추가

--- presentation/src/main/java/com/semicolon/walkhub/ui/profile/setting/NoticeSettingActivity.kt
+++ presentation/src/main/java/com/semicolon/walkhub/ui/profile/setting/NoticeSettingActivity.kt
@@ -1,8 +1,10 @@
 package com.semicolon.walkhub.ui.profile.setting
 
-import android.os.Bundle
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.databinding.DataBindingUtil
+import androidx.compose.ui.unit.dp
 import com.semicolon.walkhub.R
 import com.semicolon.walkhub.customview.ToggleSwitch
 import com.semicolon.walkhub.databinding.ActivityNoticeSettingBinding
@@ -14,55 +16,47 @@ class NoticeSettingActivity : BaseActivity<ActivityNoticeSettingBinding>(
 
     override fun initView() {
         switch()
+
     }
 
+
+    fun ComposeView.setToggleSwitch(
+        onToggleOn: () -> Unit,
+        onToggleOff: () -> Unit
+    ) = this.apply {
+        setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+        setContent {
+            ToggleSwitch(
+                Modifier.padding(vertical = 16.dp),
+                onToggleOn = { onToggleOn() },
+                onToggleOff = { onToggleOff() }
+            )
+        }
+    }
+
+
     private fun switch() {
-        binding.cheerupSwt.apply {
-            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-            setContent {
-                ToggleSwitch(
-                    onToggleOn = {},
-                    onToggleOff = {}
-                )
-            }
-        }
+        binding.cheerupSwt.setToggleSwitch(
+            onToggleOn = {},
+            onToggleOff = {}
+        )
 
-        binding.noticeSwt.apply {
-            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-            setContent {
-                ToggleSwitch(
-                    onToggleOn = {},
-                    onToggleOff = {}
-                )
-            }
-        }
+        binding.noticeSwt.setToggleSwitch(
+            onToggleOn = {},
+            onToggleOff = {}
+        )
 
-        binding.recommendSwt.apply {
-            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-            setContent {
-                ToggleSwitch(
-                    onToggleOn = {},
-                    onToggleOff = {}
-                )
-            }
-        }
-        binding.challengeGoalSwt.apply {
-            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-            setContent {
-                ToggleSwitch(
-                    onToggleOn = {},
-                    onToggleOff = {}
-                )
-            }
-        }
-        binding.challengeEndSwt.apply {
-            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-            setContent {
-                ToggleSwitch(
-                    onToggleOn = {},
-                    onToggleOff = {}
-                )
-            }
-        }
+        binding.recommendSwt.setToggleSwitch(
+            onToggleOn = {},
+            onToggleOff = {}
+        )
+        binding.challengeGoalSwt.setToggleSwitch(
+            onToggleOn = {},
+            onToggleOff = {}
+        )
+        binding.challengeEndSwt.setToggleSwitch(
+            onToggleOn = {},
+            onToggleOff = {}
+        )
     }
 }
