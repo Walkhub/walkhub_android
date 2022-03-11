6e4e36be131fc15b5c27c54767c8fae395ec792c
shw0471 <shw0471@naver.com>
2022-03-12 03:45:06 +0900

    ⚡️ :: set up SplashActivity

--- presentation/src/main/java/com/semicolon/walkhub/ui/splash/SplashActivity.kt
+++ presentation/src/main/java/com/semicolon/walkhub/ui/splash/SplashActivity.kt
@@ -3,12 +3,14 @@ package com.semicolon.walkhub.ui.splash
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SplashActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Splash()
         }
     }
 }
