da9fdade962caff29cc0e75749a986279a6af1ef
임세현 <sh00710000@gmail.com>
2022-03-14 08:56:59 +0900

    ⚡️ :: response to entity transfer

--- data/src/main/java/com/semicolon/data/remote/response/users/FetchInfoResponse.kt
+++ data/src/main/java/com/semicolon/data/remote/response/users/FetchInfoResponse.kt
@@ -1,6 +1,7 @@
 package com.semicolon.data.remote.response.users
 
 import com.google.gson.annotations.SerializedName
+import com.semicolon.domain.entity.users.FetchInfoEntity
 
 data class FetchInfoResponse(
     @SerializedName("name") val name: String,
@@ -9,3 +10,12 @@ data class FetchInfoResponse(
     @SerializedName("grade") val grade: Int,
     @SerializedName("class_num") val classNum: Int
 )
+
+fun FetchInfoResponse.toEntity() =
+    FetchInfoEntity(
+        name = name,
+        profileImageUrl = profileImageUrl,
+        schoolName = schoolName,
+        grade = grade,
+        classNum = classNum
+    )
