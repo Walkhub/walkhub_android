0d1703d315924f39c23a444f0963fe1bb2bf5902
유현명 <aahspringaa4@naver.com>
2022-03-28 23:04:33 +0900

    ⚡️ :: 학교찾기 뷰 구현

--- presentation/src/main/java/com/semicolon/walkhub/ui/register/SearchSchoolActivity.kt
+++ presentation/src/main/java/com/semicolon/walkhub/ui/register/SearchSchoolActivity.kt
@@ -1,12 +1,93 @@
 package com.semicolon.walkhub.ui.register
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.annotation.SuppressLint
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.semicolon.domain.enums.DateType
 import com.semicolon.walkhub.R
+import com.semicolon.walkhub.databinding.ActivitySearchSchoolBinding
+import com.semicolon.walkhub.extensions.repeatOnStarted
+import com.semicolon.walkhub.ui.base.BaseActivity
+import com.semicolon.walkhub.ui.register.adapter.SearchSchoolAdapter
+import com.semicolon.walkhub.ui.register.model.SecondSearchSchoolData
+import com.semicolon.walkhub.util.onTextChanged
+import com.semicolon.walkhub.viewmodel.register.SearchSchoolViewModel
 
-class SearchSchoolActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search_school)
+class SearchSchoolActivity : BaseActivity<ActivitySearchSchoolBinding>(
+    R.layout.activity_search_school
+) {
+    private val vm: SearchSchoolViewModel by viewModels()
+
+    private val schoolRvData = arrayListOf<SecondSearchSchoolData.SchoolInfo>()
+
+    private lateinit var mAdapter: SearchSchoolAdapter
+
+    private var dateType = DateType.WEEK
+
+    private fun handleEvent(event: SearchSchoolViewModel.Event) = when (event) {
+        is SearchSchoolViewModel.Event.SearchSchoolTwo -> {
+            setSchoolData(event.secondSearchSchoolData)
+        }
+        is SearchSchoolViewModel.Event.ErrorMessage -> {
+            showShortToast(event.message)
+        }
+        else -> {}
+    }
+
+    override fun initView() {
+        setAdapter()
+        setTextChanged()
+
+        binding.btBack.setOnClickListener {
+            finish()
+        }
+
+        binding.etSchool.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                vm.searchSchoolDebounce(p0.toString())
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
+
+        repeatOnStarted {
+            vm.eventFlow.collect { event -> handleEvent(event) }
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun setSchoolData(list: SecondSearchSchoolData) {
+        schoolRvData.clear()
+
+        for(i: Int in list.schoolList.indices) {
+            schoolRvData.add(list.schoolList[i])
+        }
+
+        binding.rvSchoolRank.adapter?.notifyDataSetChanged()
+    }
+
+    private fun setAdapter() {
+        mAdapter = SearchSchoolAdapter(schoolRvData)
+
+        binding.rvSchoolRank.apply {
+            layoutManager = LinearLayoutManager(applicationContext)
+            setHasFixedSize(true)
+            adapter = mAdapter
+        }
+    }
+
+    private fun setTextChanged() {
+        binding.etSchool.onTextChanged { s, start, before, count ->
+            vm.searchSchoolDebounce(s.toString())
+        }
     }
 }
