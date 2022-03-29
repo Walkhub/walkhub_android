57bd6ebbbf0e87477a15e013b48e9d8f70796445
Jaewon <turitus@naver.com>
2022-03-29 19:21:38 +0900

    ⚡️:: challenge fragment

--- presentation/src/main/java/com/semicolon/walkhub/ui/challenge/ChallengeFragment.kt
+++ presentation/src/main/java/com/semicolon/walkhub/ui/challenge/ChallengeFragment.kt
@@ -1,12 +1,19 @@
 package com.semicolon.walkhub.ui.challenge
 
+import androidx.fragment.app.viewModels
 import com.semicolon.walkhub.R
 import com.semicolon.walkhub.databinding.FragmentChallengeBinding
 import com.semicolon.walkhub.ui.base.BaseFragment
+import com.semicolon.walkhub.viewmodel.challenge.ChallengeViewModel
 
 class ChallengeFragment : BaseFragment<FragmentChallengeBinding>(
     R.layout.fragment_challenge
 ) {
+
+    private val vm: ChallengeViewModel by viewModels()
+
     override fun initView() {
+        vm.fetchChallenges()
+        
     }
 }
