f1ec2fe63089007e4a31af032bc20dcec004e15c
임세현 <sh00710000@gmail.com>
2022-04-03 23:09:15 +0900

    ⚡️ :: sign up class viewmodel

---
+++ presentation/src/main/java/com/semicolon/walkhub/viewmodel/hub/SignUpClassViewModel.kt
@@ -0,0 +1,49 @@
+package com.semicolon.walkhub.viewmodel.hub
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.semicolon.domain.exception.ConflictException
+import com.semicolon.domain.exception.NoInternetException
+import com.semicolon.domain.param.user.SignUpClassParam
+import com.semicolon.domain.usecase.user.SignUpClassUseCase
+import com.semicolon.walkhub.util.MutableEventFlow
+import com.semicolon.walkhub.util.asEventFlow
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SignUpClassViewModel @Inject constructor(
+    private val signUpClassUseCase: SignUpClassUseCase
+) : ViewModel() {
+
+    private val _eventFlow = MutableEventFlow<Event>()
+    val eventFlow = _eventFlow.asEventFlow()
+
+    fun signUpClass(classCode: String, number: Int) {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                signUpClassUseCase.execute(SignUpClassParam(classCode, number))
+            }.onFailure {
+                when (it) {
+                    is NoInternetException -> event(Event.ErrorMessage("인터넷을 사용할 수 없습니다"))
+                    is ConflictException -> event(Event.ErrorMessage("이미 반에 가입되어 있습니다."))
+                    else -> event(Event.ErrorMessage("알 수 없는 에러가 발생했습니다."))
+                }
+            }.onSuccess {
+                event(Event.Success(true))
+            }
+        }
+    }
+
+    private fun event(event: Event) {
+        viewModelScope.launch {
+            _eventFlow.emit(event)
+        }
+    }
+
+    sealed class Event {
+        data class Success(val state: Boolean) : Event()
+        data class ErrorMessage(val message: String) : Event()
+    }
+}
