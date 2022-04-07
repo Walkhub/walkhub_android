31ec568ce5c4ad3277005f95f47d3091ab37a7d1
Leesin0222 <dragon02229@naver.com>
2022-04-07 17:01:54 +0900

    ⚡ :: 학교 검색 -> 학교랭킹 조회 및 검색 쿼리 추가 및 주석 이름 변경

--- data/src/main/java/com/semicolon/data/remote/api/RankApi.kt
+++ data/src/main/java/com/semicolon/data/remote/api/RankApi.kt
@@ -17,27 +17,30 @@ interface RankApi {
         @Query("schoolDateType") dateType: String
     ): SchoolRankResponse
 
-    //학교검색
+    //학교 랭킹 조회 및 검색
     @GET("ranks/schools/search")
     suspend fun searchSchool(
-        @Query("name") name: String
+        @Query("name") name: String,
+        @Query("sort") sort: String,
+        @Query("scope") scope: String,
+        @Query("schoolDateType") dateType: String
     ): SearchSchoolResponse
 
-    //유저랭킹조회
+    //다른학교 유저 랭킹 조회
     @GET("ranks/users/{school-id}")
     suspend fun fetchUserRank(
         @Path("school-id") schoolId: Int,
         @Query("dateType") dateType: String
     ): UserRankResponse
 
-    //우리학교 유저 랭킹
+    //우리학교 유저 랭킹 조회
     @GET("ranks/users/my-school")
     suspend fun fetchOurSchoolUserRank(
         @Query("scope") scope: String,
         @Query("dateType") moreDateType: String
     ): OurSchoolUserRankResponse
 
-    //유저검색
+    //유저 랭킹 검색
     @GET("ranks/users/search/{school-id}")
     suspend fun searchUser(
         @Path("school-id") school: Int,
