0d5e3f819f70db4712a2b1ad4610a908387115e0
Leesin0222 <dragon02229@naver.com>
2022-04-07 17:05:15 +0900

    ⚡ :: SearchSchool Param 작성

--- domain/src/main/java/com/semicolon/domain/param/rank/SearchSchoolParam.kt
+++ domain/src/main/java/com/semicolon/domain/param/rank/SearchSchoolParam.kt
@@ -1,7 +1,12 @@
 package com.semicolon.domain.param.rank
 
 import com.semicolon.domain.enums.DateType
+import com.semicolon.domain.enums.RankSort
+import com.semicolon.domain.enums.TheRankScope
 
 data class SearchSchoolParam (
-    val name: String
+    val name: String,
+    val sort: RankSort,
+    val scope: TheRankScope,
+    val dateType: DateType
 )
