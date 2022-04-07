5cd4acd4ed03a08aef8839ba3881cda38000fdcc
Leesin0222 <dragon02229@naver.com>
2022-04-07 10:29:21 +0900

    ♻️ :: 건강 정보 Fragment -> Activity로 변경

---
+++ presentation/src/main/java/com/semicolon/walkhub/ui/profile/setting/ui/ModifyHealthInfoActivity.kt
@@ -0,0 +1,14 @@
+package com.semicolon.walkhub.ui.profile.setting.ui
+
+import com.semicolon.walkhub.R
+import com.semicolon.walkhub.databinding.ActivityModifyHealthInfoBinding
+import com.semicolon.walkhub.ui.base.BaseActivity
+
+class ModifyHealthInfoActivity : BaseActivity<ActivityModifyHealthInfoBinding>(
+    R.layout.activity_modify_health_info
+) {
+
+    override fun initView() {
+
+    }
+}
