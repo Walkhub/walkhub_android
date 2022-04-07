dcaca4b6b7322eae869f2ea77f8a0c0a3e4ed4a6
임세현 <sh00710000@gmail.com>
2022-04-07 18:22:58 +0900

    ⚡️ :: update school remote

--- data/src/main/java/com/semicolon/data/remote/datasource/RemoteSchoolDataSourceImpl.kt
+++ data/src/main/java/com/semicolon/data/remote/datasource/RemoteSchoolDataSourceImpl.kt
@@ -1,9 +1,12 @@
 package com.semicolon.data.remote.datasource
 
 import com.semicolon.data.remote.api.SchoolApi
+import com.semicolon.data.remote.response.school.SchoolDetailResponse
 import com.semicolon.data.remote.response.school.SearchSchoolResponse
+import com.semicolon.data.remote.response.school.toEntity
 import com.semicolon.data.remote.response.school.toListEntity
 import com.semicolon.data.util.HttpHandler
+import com.semicolon.domain.entity.school.SchoolDetailEntity
 import com.semicolon.domain.entity.school.SearchSchoolEntity
 import javax.inject.Inject
 
@@ -15,4 +18,14 @@ class RemoteSchoolDataSourceImpl @Inject constructor(
         HttpHandler<SearchSchoolResponse>()
             .httpRequest { schoolApi.searchSchool(name) }
             .sendRequest().toListEntity()
+
+    override suspend fun setSchoolLogo(imageUrl: String) =
+        HttpHandler<Unit>()
+            .httpRequest { schoolApi.setSchoolLogo(imageUrl) }
+            .sendRequest()
+
+    override suspend fun fetchSchoolDetail(schoolId: Int): SchoolDetailEntity =
+        HttpHandler<SchoolDetailResponse>()
+            .httpRequest { schoolApi.fetchSchoolDetail(schoolId) }
+            .sendRequest().toEntity()
 }
