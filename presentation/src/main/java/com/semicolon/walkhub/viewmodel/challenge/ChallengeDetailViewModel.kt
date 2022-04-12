0d23a3d9a87db3665646b7fd63bf1a5b00ddb357
minjun8946 <minjun8946@gmail.com>
2022-04-12 09:57:26 +0900

    🐛:: add viewmodel()

--- presentation/src/main/java/com/semicolon/walkhub/viewmodel/challenge/ChallengeDetailViewModel.kt
+++ presentation/src/main/java/com/semicolon/walkhub/viewmodel/challenge/ChallengeDetailViewModel.kt
@@ -1,10 +1,11 @@
 package com.semicolon.walkhub.viewmodel.challenge
 
+import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class ChallengeDetailViewModel @Inject constructor(
 
-){
+):ViewModel(){
 }
