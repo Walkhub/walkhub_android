b82d23bafeb8a8ee70c4c0a4456cd5c55d1ef53b
minjun8946 <minjun8946@gmail.com>
2022-04-12 09:32:06 +0900

    📃:: challenge detail viewmodel

---
+++ presentation/src/main/java/com/semicolon/walkhub/viewmodel/challenge/ChallengeDetailViewModel.kt
@@ -0,0 +1,10 @@
+package com.semicolon.walkhub.viewmodel.challenge
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ChallengeDetailViewModel @Inject constructor(
+
+){
+}
