983266ff27d1254d83dfee5dcd4f68e8ff21b298
유현명 <aahspringaa4@naver.com>
2022-04-17 23:25:17 +0900

    ♻️ :: 타입 변경

--- data/src/main/java/com/semicolon/data/remote/request/users/UserSignUpRequest.kt
+++ data/src/main/java/com/semicolon/data/remote/request/users/UserSignUpRequest.kt
@@ -1,6 +1,7 @@
 package com.semicolon.data.remote.request.users
 
 import com.google.gson.annotations.SerializedName
+import com.semicolon.domain.enums.SexType
 
 data class UserSignUpRequest(
     @SerializedName("account_id") val accountId: String,
@@ -8,8 +9,8 @@ data class UserSignUpRequest(
     @SerializedName("name") val name: String,
     @SerializedName("phone_number") val phoneNumber: String,
     @SerializedName("height") val height: Double,
-    @SerializedName("weight") val weight: Double,
-    @SerializedName("sex") val sex: String,
+    @SerializedName("weight") val weight: Int,
+    @SerializedName("sex") val sex: SexType,
     @SerializedName("school_id") val schoolId: Int,
     @SerializedName("auth_code") val authCode: String
 )
