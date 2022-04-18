ce4928bacce72e4c2ff15931a7d7b33ad4ed9882
유현명 <aahspringaa4@naver.com>
2022-04-18 11:59:00 +0900

    🐛 :: School 테스트 코드 수정

--- data/src/test/java/com/semicolon/data/repository/SchoolRepositoryUnitTest.kt
+++ data/src/test/java/com/semicolon/data/repository/SchoolRepositoryUnitTest.kt
@@ -5,6 +5,7 @@ import com.nhaarman.mockitokotlin2.whenever
 import com.semicolon.data.local.datasource.LocalSchoolDataSource
 import com.semicolon.data.remote.datasource.RemoteImagesDataSource
 import com.semicolon.data.remote.datasource.RemoteSchoolDataSource
+import com.semicolon.data.remote.response.school.SearchSchoolResponse
 import com.semicolon.domain.entity.school.SearchSchoolEntity
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
@@ -16,26 +17,35 @@ class SchoolRepositoryUnitTest {
     private val localSchoolDataSource = mock<LocalSchoolDataSource>()
     private val remoteSchoolDataSource = mock<RemoteSchoolDataSource>()
 
-    private val schoolRepository = SchoolRepositoryImpl(remoteImagesDataSource, localSchoolDataSource, remoteSchoolDataSource)
+    private val schoolRepository =
+        SchoolRepositoryImpl(remoteImagesDataSource, localSchoolDataSource, remoteSchoolDataSource)
 
     @Test
     fun testSearchSchool() {
         val schoolName = "my school"
+        val schoolId = 1
+        val image = "image"
+
         val searchSchoolResponse =
-            listOf(
-                SearchSchoolEntity(
-                    "code",
-                    schoolName,
-                    "image"
+            SearchSchoolResponse(
+                schoolList = listOf(
+                    SearchSchoolResponse.SchoolInfo(
+                        schoolId,
+                        image,
+                        schoolName
+                    )
                 )
             )
 
+
         runBlocking {
-            whenever(remoteSchoolDataSource.searchSchool(schoolName)).thenReturn(searchSchoolResponse)
+            whenever(remoteSchoolDataSource.searchSchool(schoolName)).thenReturn(
+                searchSchoolResponse
+            )
 
             val repositoryResult = schoolRepository.searchSchool(schoolName)
             repositoryResult.collect {
-                assertEquals(searchSchoolResponse,it)
+                assertEquals(searchSchoolResponse, it)
             }
         }
     }
