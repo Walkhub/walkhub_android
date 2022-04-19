aba2a2c8104dbdd23819717a34d40179e72c2717
임세현 <sh00710000@gmail.com>
2022-04-19 22:51:21 +0900

    ⚡️ :: update rank usecase

---
+++ domain/src/main/java/com/semicolon/domain/usecase/rank/FetchSchoolRankAndSearchUseCase.kt
@@ -0,0 +1,16 @@
+package com.semicolon.domain.usecase.rank
+
+import com.semicolon.domain.entity.rank.SchoolRankAndSearchEntity
+import com.semicolon.domain.param.user.FetchSchoolRankAndSearchParam
+import com.semicolon.domain.repository.RankRepository
+import com.semicolon.domain.usecase.UseCase
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class FetchSchoolRankAndSearchUseCase @Inject constructor(
+    private val rankRepository: RankRepository
+) : UseCase<FetchSchoolRankAndSearchParam, Flow<SchoolRankAndSearchEntity>>() {
+
+    override suspend fun execute(data: FetchSchoolRankAndSearchParam): Flow<SchoolRankAndSearchEntity> =
+        rankRepository.fetchSchoolRankAndSearch(data)
+}
