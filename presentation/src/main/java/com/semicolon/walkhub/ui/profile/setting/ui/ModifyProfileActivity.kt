cfe2a9592c0b86671fdb41245b009b164b002af4
Leesin0222 <dragon02229@naver.com>
2022-04-19 21:29:43 +0900

    ⚡ :: 임시 커밋

--- presentation/src/main/java/com/semicolon/walkhub/ui/profile/setting/ui/ModifyProfileActivity.kt
+++ presentation/src/main/java/com/semicolon/walkhub/ui/profile/setting/ui/ModifyProfileActivity.kt
@@ -1,19 +1,54 @@
 package com.semicolon.walkhub.ui.profile.setting.ui
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
+import androidx.activity.viewModels
+import com.semicolon.domain.entity.users.FetchInfoEntity
 import com.semicolon.walkhub.R
 import com.semicolon.walkhub.databinding.ActivityModifyProfileBinding
+import com.semicolon.walkhub.extensions.repeatOnStarted
 import com.semicolon.walkhub.ui.base.BaseActivity
+import com.semicolon.walkhub.util.loadCircleFromUrl
+import com.semicolon.walkhub.viewmodel.profile.setting.ModifyProfileViewModel
+import com.semicolon.walkhub.viewmodel.profile.setting.NoticeSettingViewModel
 
 class ModifyProfileActivity : BaseActivity<ActivityModifyProfileBinding>(
     R.layout.activity_modify_profile
 ) {
+    private val vm: ModifyProfileViewModel by viewModels()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        vm.fetchInfo()
+
+        repeatOnStarted {
+            vm.eventFlow.collect { event -> handleEvent(event) }
+        }
+    }
+
+    private fun handleEvent(event: ModifyProfileViewModel.Event) = when (event) {
+        is ModifyProfileViewModel.Event.FetchInfo -> {
+            setProfileInfo(event.fetchInfoData)
+        }
+
+        is ModifyProfileViewModel.Event.ErrorMessage -> {
+            showShortToast(event.message)
+        }
+    }
 
     override fun initView() {
         binding.back.setOnClickListener {
             finish()
         }
     }
+
+    private fun setProfileInfo(fetchInfoData: FetchInfoEntity) {
+        binding.mySchoolName.text = fetchInfoData.schoolName
+        binding.name.text = fetchInfoData.name
+        binding.classes.text = fetchInfoData.classNum.toString()
+        binding.grade.text = fetchInfoData.grade.toString()
+        fetchInfoData.profileImageUrl.let { binding.image.loadCircleFromUrl(it) }
+    }
+
 }
