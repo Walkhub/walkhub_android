9e7a1ff777e576c098c756693582213a893df269
Leesin0222 <dragon02229@naver.com>
2022-04-20 22:51:16 +0900

    ⚡ :: 이미지피커 구현

--- presentation/src/main/java/com/semicolon/walkhub/ui/profile/setting/ui/ModifyProfileActivity.kt
+++ presentation/src/main/java/com/semicolon/walkhub/ui/profile/setting/ui/ModifyProfileActivity.kt
@@ -1,7 +1,10 @@
 package com.semicolon.walkhub.ui.profile.setting.ui
 
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
+import com.bumptech.glide.Glide
 import com.semicolon.domain.entity.users.FetchInfoEntity
 import com.semicolon.walkhub.R
 import com.semicolon.walkhub.databinding.ActivityModifyProfileBinding
@@ -10,6 +13,7 @@ import com.semicolon.walkhub.ui.base.BaseActivity
 import com.semicolon.walkhub.util.loadCircleFromUrl
 import com.semicolon.walkhub.viewmodel.profile.setting.ModifyProfileViewModel
 import com.semicolon.walkhub.viewmodel.profile.setting.NoticeSettingViewModel
+import gun0912.tedimagepicker.builder.TedImagePicker
 
 class ModifyProfileActivity : BaseActivity<ActivityModifyProfileBinding>(
     R.layout.activity_modify_profile
@@ -20,14 +24,18 @@ class ModifyProfileActivity : BaseActivity<ActivityModifyProfileBinding>(
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        vm.fetchInfo()
-
-        repeatOnStarted {
-            vm.eventFlow.collect { event -> handleEvent(event) }
+        //vm.fetchInfo()
+        binding.image.setOnClickListener {
+            setNormalSingleButton()
         }
+
+
+        /*repeatOnStarted {
+            vm.eventFlow.collect { event -> handleEvent(event) }
+        }*/
     }
 
-    private fun handleEvent(event: ModifyProfileViewModel.Event) = when (event) {
+    /*private fun handleEvent(event: ModifyProfileViewModel.Event) = when (event) {
         is ModifyProfileViewModel.Event.FetchInfo -> {
             setProfileInfo(event.fetchInfoData)
         }
@@ -35,7 +43,7 @@ class ModifyProfileActivity : BaseActivity<ActivityModifyProfileBinding>(
         is ModifyProfileViewModel.Event.ErrorMessage -> {
             showShortToast(event.message)
         }
-    }
+    }*/
 
     override fun initView() {
         binding.back.setOnClickListener {
@@ -43,12 +51,26 @@ class ModifyProfileActivity : BaseActivity<ActivityModifyProfileBinding>(
         }
     }
 
-    private fun setProfileInfo(fetchInfoData: FetchInfoEntity) {
+    /*private fun setProfileInfo(fetchInfoData: FetchInfoEntity) {
         binding.mySchoolName.text = fetchInfoData.schoolName
         binding.name.text = fetchInfoData.name
         binding.classes.text = fetchInfoData.classNum.toString()
         binding.grade.text = fetchInfoData.grade.toString()
         fetchInfoData.profileImageUrl.let { binding.image.loadCircleFromUrl(it) }
+    }*/
+
+    private fun setNormalSingleButton() {
+        binding.image.setOnClickListener {
+            TedImagePicker.with(this)
+                .start { uri -> showSingleImage(uri) }
+            binding.hsvImage.visibility = View.VISIBLE
+        }
+    }
+
+    private fun showSingleImage(uri: Uri) {
+        binding.hsvImage.visibility = View.GONE
+        Glide.with(this).load(uri).into(binding.image)
+
     }
 
 }
