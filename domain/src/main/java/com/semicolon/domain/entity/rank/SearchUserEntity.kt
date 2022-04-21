f85aa81b19c4e8d0fccc5c9d9fe7f4ea976c0c73
유현명 <aahspringaa4@naver.com>
2022-04-22 02:01:55 +0900

    ♻️ :: Entity null 허용

--- domain/src/main/java/com/semicolon/domain/entity/rank/SearchUserEntity.kt
+++ domain/src/main/java/com/semicolon/domain/entity/rank/SearchUserEntity.kt
@@ -7,7 +7,7 @@ data class SearchUserEntity(
         val classNum: Int,
         val grade: Int,
         val name: String,
-        val profileImageUrl: String,
+        val profileImageUrl: String?,
         val ranking: Int,
         val userId: Int,
         val walkCount: Int
