ef230e8ff007188f15deef1729f0112a4dc5ac6b
Leesin0222 <dragon02229@naver.com>
2022-04-27 20:18:44 +0900

    ⚡ :: AccountInfoViewModel 에 비밀번호 확인  함수작성

--- presentation/src/main/java/com/semicolon/walkhub/viewmodel/profile/setting/AccountInfoViewModel.kt
+++ presentation/src/main/java/com/semicolon/walkhub/viewmodel/profile/setting/AccountInfoViewModel.kt
@@ -8,20 +8,24 @@ import com.semicolon.domain.exception.NoInternetException
 import com.semicolon.domain.exception.NotFoundException
 import com.semicolon.domain.exception.UnauthorizedException
 import com.semicolon.domain.param.user.PatchUserChangePasswordParam
+import com.semicolon.domain.param.user.VerifyPasswordParam
 import com.semicolon.domain.usecase.user.DeleteAccountUseCase
 import com.semicolon.domain.usecase.user.FetchAuthInfoUseCase
 import com.semicolon.domain.usecase.user.PatchUserChangePasswordUseCase
+import com.semicolon.domain.usecase.user.VerifyPasswordUseCase
 import com.semicolon.walkhub.util.MutableEventFlow
 import com.semicolon.walkhub.util.asEventFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import java.lang.NullPointerException
 import javax.inject.Inject
 
 @HiltViewModel
 class AccountInfoViewModel @Inject constructor(
     private val patchUserChangePasswordUseCase: PatchUserChangePasswordUseCase,
     private val deleteAccountUseCase: DeleteAccountUseCase,
-    private val fetchAuthInfoUseCase: FetchAuthInfoUseCase
+    private val fetchAuthInfoUseCase: FetchAuthInfoUseCase,
+    private val verifyPasswordUseCase: VerifyPasswordUseCase
 ) : ViewModel() {
 
     private val _eventFlow = MutableEventFlow<Event>()
@@ -75,6 +79,28 @@ class AccountInfoViewModel @Inject constructor(
                     is UnauthorizedException -> event(Event.ErrorMessage("세션이 만료되었습니다. 다시 시도해주세요."))
                     is NotFoundException -> event(Event.ErrorMessage("요청하는 대상을 확인할 수 없습니다."))
                     is NoInternetException -> event(Event.ErrorMessage("인터넷에 연결되어있지 않습니다."))
+                    else -> event(Event.ErrorMessage("에러가 발생했습니다."))
+                }
+            }
+        }
+    }
+
+    fun verifyPassword(
+        password: String
+    ) {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                verifyPasswordUseCase.execute(
+                    VerifyPasswordParam(
+                        password
+                    )
+                )
+            }.onFailure {
+                when (it) {
+                    is BadRequestException -> event(Event.ErrorMessage("현재 비밀번호가 틀렸습니다. 알맞은 비밀번호를 입력해주세요."))
+                    is UnauthorizedException -> event(Event.ErrorMessage("세션이 만료되었습니다. 다시 시도해주세요."))
+                    is NoInternetException -> event(Event.ErrorMessage("인터넷에 연결되어있지 않습니다."))
+                    is NullPointerException -> event(Event.ErrorMessage("현재 비밀번호를 입력해주세요."))
                     else -> event(Event.ErrorMessage("에러가 발생했습니다."))
                 }
             }
