3d6066ab4e9e75b577142556853c4ded39af6a35
Leesin0222 <dragon02229@naver.com>
2022-04-27 21:55:58 +0900

    ⚡ :: 비밀번호 확인시 새 비밀번호 입력으로 넘어가는 기능 구현

--- presentation/src/main/java/com/semicolon/walkhub/ui/profile/setting/ui/ChangePwActivity.kt
+++ presentation/src/main/java/com/semicolon/walkhub/ui/profile/setting/ui/ChangePwActivity.kt
@@ -1,18 +1,46 @@
 package com.semicolon.walkhub.ui.profile.setting.ui
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.semicolon.walkhub.R
 import com.semicolon.walkhub.databinding.ActivityChangePwBinding
+import com.semicolon.walkhub.extensions.repeatOnStarted
 import com.semicolon.walkhub.ui.base.BaseActivity
+import com.semicolon.walkhub.ui.register.ui.ServiceInstructionActivity
+import com.semicolon.walkhub.viewmodel.profile.setting.AccountInfoViewModel
+import com.semicolon.walkhub.viewmodel.profile.setting.ChangePwViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ChangePwActivity:  BaseActivity<ActivityChangePwBinding>(
+class ChangePwActivity : BaseActivity<ActivityChangePwBinding>(
     R.layout.activity_change_pw
-){
+) {
+    private val vm: ChangePwViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val password = binding.nowPw.text.toString()
+
+        binding.goNextBtn.setOnClickListener {
+            vm.verifyPassword(password = password)
+        }
+
+        repeatOnStarted {
+            vm.eventFlow.collect { event -> handleEvent(event) }
+        }
+    }
+
+    private fun handleEvent(event: ChangePwViewModel.Event) = when (event) {
+
+        is ChangePwViewModel.Event.ErrorMessage -> {
+            showShortToast(event.message)
+        }
+        ChangePwViewModel.Event.SuccessVerify -> {
+            val intent = Intent(this, RealChangePwActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun initView() {
