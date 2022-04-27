fb82125a269fa6ba914d1e546b37e8f3f46039dd
Leesin0222 <dragon02229@naver.com>
2022-04-27 22:21:20 +0900

    ⚡ :: 비밀번호 변경 val password 빼고 구현함

--- presentation/src/main/java/com/semicolon/walkhub/ui/profile/setting/ui/RealChangePwActivity.kt
+++ presentation/src/main/java/com/semicolon/walkhub/ui/profile/setting/ui/RealChangePwActivity.kt
@@ -1,28 +1,50 @@
 package com.semicolon.walkhub.ui.profile.setting.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.semicolon.walkhub.R
 import com.semicolon.walkhub.databinding.ActivityRealChangePwBinding
+import com.semicolon.walkhub.extensions.repeatOnStarted
 import com.semicolon.walkhub.ui.base.BaseActivity
-import com.semicolon.walkhub.viewmodel.profile.setting.AccountInfoViewModel
+import com.semicolon.walkhub.viewmodel.profile.setting.RealChangePwViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class RealChangePwActivity:  BaseActivity<ActivityRealChangePwBinding>(
+class RealChangePwActivity : BaseActivity<ActivityRealChangePwBinding>(
     R.layout.activity_change_pw
 ) {
 
-    private val vm : AccountInfoViewModel by viewModels()
+    private val vm: RealChangePwViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //TODO: password 안 채우기 val password =
         val newPass = binding.nowPw.text.toString()
-        //vm.patchUserChangePassword(newPassword = newPass)
+        //vm.patchUserChangePassword(password = password, newPassword = newPass)
+
+        repeatOnStarted {
+            vm.eventFlow.collect { event -> handleEvent(event) }
+        }
+    }
+
+    private fun handleEvent(event: RealChangePwViewModel.Event) = when (event) {
+
+        is RealChangePwViewModel.Event.ErrorMessage -> {
+            showShortToast(event.message)
+        }
+        RealChangePwViewModel.Event.SuccessChange -> {
+            val intent = Intent(this, AccountInfoActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun initView() {
-
+        binding.back.setOnClickListener {
+            val intent = Intent(this, AccountInfoActivity::class.java)
+            finish()
+            startActivity(intent)
+        }
     }
 }
