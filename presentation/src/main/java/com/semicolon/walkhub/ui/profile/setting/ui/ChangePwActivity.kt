4884718e06d9eb11885a21f51bece04e5cbc8449
Leesin0222 <dragon02229@naver.com>
2022-05-02 11:58:32 +0900

    ⚡ :: 비번입력에 따라 버튼 활성화 및 배경 이벤트 바뀌는 코드 작성

--- presentation/src/main/java/com/semicolon/walkhub/ui/profile/setting/ui/ChangePwActivity.kt
+++ presentation/src/main/java/com/semicolon/walkhub/ui/profile/setting/ui/ChangePwActivity.kt
@@ -1,15 +1,21 @@
 package com.semicolon.walkhub.ui.profile.setting.ui
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import com.semicolon.domain.param.user.VerifyPasswordParam
 import com.semicolon.walkhub.R
 import com.semicolon.walkhub.databinding.ActivityChangePwBinding
 import com.semicolon.walkhub.extensions.repeatOnStarted
 import com.semicolon.walkhub.ui.base.BaseActivity
 import com.semicolon.walkhub.viewmodel.profile.setting.ChangePwViewModel
+import com.semicolon.walkhub.viewmodel.register.RegisterViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.regex.Pattern
 
 @AndroidEntryPoint
 class ChangePwActivity : BaseActivity<ActivityChangePwBinding>(
@@ -45,8 +51,49 @@ class ChangePwActivity : BaseActivity<ActivityChangePwBinding>(
     }
 
     override fun initView() {
+        setTextWatcher()
+
         binding.back.setOnClickListener {
             finish()
         }
+    }
+
+    private fun setTextWatcher() {
+        binding.nowPw.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.nowPw.setClickable(true)
+            }
+
+            @SuppressLint("SetTextI18n")
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                val password = binding.nowPw.text.toString()
+
+                if (password.length > 7) {
+                    val passwordPattern =
+                        "^.*(?=^.{8,30}\$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#\$%^&+=]).*\$"
+
+                    val pt = Pattern.matches(passwordPattern, password)
+
+                    if (pt) {
+                        binding.goNextBtn.background = ContextCompat.getDrawable(
+                            applicationContext,
+                            R.drawable.bg_primary_button
+                        )
+                        binding.goNextBtn.setClickable(true)
+                    } else {
+                        showShortToast("비밀번호에는의 한개 이상 숫자와 특수문자가 포함되어야합니다.")
+                        binding.goNextBtn.background = ContextCompat.getDrawable(
+                            applicationContext,
+                            R.drawable.registerbuttondesign
+                        )
+                        binding.goNextBtn.setClickable(false)
+                    }
+                }
+            }
+        })
     }
 }
--- presentation/src/main/java/com/semicolon/walkhub/ui/profile/setting/ui/RealChangePwActivity.kt
+++ presentation/src/main/java/com/semicolon/walkhub/ui/profile/setting/ui/RealChangePwActivity.kt
@@ -1,8 +1,12 @@
 package com.semicolon.walkhub.ui.profile.setting.ui
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import com.semicolon.domain.param.user.PatchUserChangePasswordParam
 import com.semicolon.walkhub.R
 import com.semicolon.walkhub.databinding.ActivityRealChangePwBinding
@@ -10,6 +14,7 @@ import com.semicolon.walkhub.extensions.repeatOnStarted
 import com.semicolon.walkhub.ui.base.BaseActivity
 import com.semicolon.walkhub.viewmodel.profile.setting.RealChangePwViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.regex.Pattern
 
 @AndroidEntryPoint
 class RealChangePwActivity : BaseActivity<ActivityRealChangePwBinding>(
@@ -23,12 +28,16 @@ class RealChangePwActivity : BaseActivity<ActivityRealChangePwBinding>(
 
 
         binding.changeBtn.setOnClickListener {
-
             val intent = getIntent()
             val password = intent.getStringExtra("pw")
             val newPass = binding.nowPw.text.toString()
             if (password != null) {
-                vm.patchUserChangePassword(patchUserChangePasswordParam = PatchUserChangePasswordParam(password, newPass))
+                vm.patchUserChangePassword(
+                    patchUserChangePasswordParam = PatchUserChangePasswordParam(
+                        password,
+                        newPass
+                    )
+                )
             }
         }
 
@@ -43,16 +52,57 @@ class RealChangePwActivity : BaseActivity<ActivityRealChangePwBinding>(
             showShortToast(event.message)
         }
         RealChangePwViewModel.Event.SuccessChange -> {
+            showShortToast("비밀번호 변경을 완료했어요.")
             val intent = Intent(this, AccountInfoActivity::class.java)
             startActivity(intent)
         }
     }
 
     override fun initView() {
+        setTextWatcher()
         binding.back.setOnClickListener {
             val intent = Intent(this, AccountInfoActivity::class.java)
             finish()
             startActivity(intent)
         }
+    }
+
+    private fun setTextWatcher() {
+        binding.nowPw.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.nowPw.setClickable(true)
+            }
+
+            @SuppressLint("SetTextI18n")
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                val password = binding.nowPw.text.toString()
+
+                if (password.length > 7) {
+                    val passwordPattern =
+                        "^.*(?=^.{8,30}\$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#\$%^&+=]).*\$"
+
+                    val pt = Pattern.matches(passwordPattern, password)
+
+                    if (pt) {
+                        binding.changeBtn.background = ContextCompat.getDrawable(
+                            applicationContext,
+                            R.drawable.bg_primary_button
+                        )
+                        binding.changeBtn.setClickable(true)
+                    } else {
+                        showShortToast("비밀번호에는의 한개 이상 숫자와 특수문자가 포함되어야합니다.")
+                        binding.changeBtn.background = ContextCompat.getDrawable(
+                            applicationContext,
+                            R.drawable.registerbuttondesign
+                        )
+                        binding.changeBtn.setClickable(false)
+                    }
+                }
+            }
+        })
     }
 }
