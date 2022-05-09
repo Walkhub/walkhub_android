4995018c31499312c294d48ca1d0afb8bba9ea88
Leesin0222 <dragon02229@naver.com>
2022-05-10 00:45:35 +0900

    ⚡ :: Off,On NotificationRequest 작성

---
+++ data/src/main/java/com/semicolon/data/remote/request/notification/OffNotiRequest.kt
@@ -0,0 +1,8 @@
+package com.semicolon.data.remote.request.notification
+
+import com.google.gson.annotations.SerializedName
+
+data class OffNotiRequest(
+    @SerializedName("type") val type: String,
+    @SerializedName("user_id_list") val userIdList: List<Int>
+)
