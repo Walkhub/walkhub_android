8e2b05b693bb328dde11ff5b429f2a1c08934952
Leesin0222 <dragon02229@naver.com>
2022-05-18 00:21:11 +0900

    ⚡ :: Title Type NotificationType으로  변경

--- domain/src/main/java/com/semicolon/domain/entity/notification/NotificationStatusEntity.kt
+++ domain/src/main/java/com/semicolon/domain/entity/notification/NotificationStatusEntity.kt
@@ -1,11 +1,13 @@
 package com.semicolon.domain.entity.notification
 
+import com.semicolon.domain.enums.NotificationType
+
 data class NotificationStatusEntity(
     val topicWhetherList: List<TopicWhether>
 ) {
     data class TopicWhether(
         val isSubscribe: Boolean,
-        val title: String,
+        val title: NotificationType,
         val topicId: Int
     )
 }
