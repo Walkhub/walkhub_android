2df0bba5fb0a79dc73f9a761700959c9e04334dc
shw0471 <shw0471@naver.com>
2022-05-17 15:54:57 +0900

    🐛 :: fix location tracking worker logic

--- data/src/main/java/com/semicolon/data/background/worker/LocationTrackingWorker.kt
+++ data/src/main/java/com/semicolon/data/background/worker/LocationTrackingWorker.kt
@@ -3,17 +3,23 @@ package com.semicolon.data.background.worker
 import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.pm.PackageManager
 import android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION
+import android.location.Location
+import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Build
+import android.os.Handler
+import android.os.Looper
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.ForegroundInfo
 import androidx.work.WorkerParameters
+import com.semicolon.data.R
 import com.semicolon.data.local.datasource.LocalExerciseDataSource
 import com.semicolon.data.local.entity.exercise.LocationRecordEntity
 import dagger.assisted.Assisted
@@ -21,6 +27,7 @@ import dagger.assisted.AssistedInject
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 @HiltWorker
@@ -36,29 +43,39 @@ class LocationTrackingWorker @AssistedInject constructor(
         context.getSystemService(Context.NOTIFICATION_SERVICE) as
                 NotificationManager
 
-    private val locationManager =
-        context.getSystemService(Context.LOCATION_SERVICE) as
-                LocationManager
-
     override suspend fun doWork(): Result {
         setForeground(createForegroundInfo())
         recordLocation()
         return Result.success()
     }
 
-    private suspend fun recordLocation() = suspendCoroutine<Unit> {
-        if (checkPermission())
-            locationManager.requestLocationUpdates(
-                LocationManager.GPS_PROVIDER, 0, 0F
-            ) {
+    private suspend fun recordLocation() = suspendCoroutine<Unit> { continuation ->
+        val locationManager: LocationManager =
+            mContext.getSystemService(Context.LOCATION_SERVICE) as
+                    LocationManager
+        if (checkPermission()) {
+            var listener: LocationListener? = null
+            listener = LocationListener {
+                if (isStopped) {
+                    locationManager.removeUpdates(listener!!)
+                }
                 val location = LocationRecordEntity(
                     latitude = it.latitude,
                     longitude = it.longitude
                 )
+                println(location)
                 CoroutineScope(Dispatchers.IO).launch {
                     localExerciseDataSource.addLocationRecord(location)
                 }
             }
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                0,
+                5F,
+                listener,
+                Looper.getMainLooper()
+            )
+        }
     }
 
     private fun checkPermission() =
@@ -68,6 +85,9 @@ class LocationTrackingWorker @AssistedInject constructor(
             mContext, Manifest.permission.ACCESS_COARSE_LOCATION
         ) == PackageManager.PERMISSION_GRANTED
 
+    override suspend fun getForegroundInfo(): ForegroundInfo =
+        createForegroundInfo()
+
     private fun createForegroundInfo(): ForegroundInfo {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val notificationChannel = NotificationChannel(
@@ -76,10 +96,12 @@ class LocationTrackingWorker @AssistedInject constructor(
             notificationManager.createNotificationChannel(notificationChannel)
         }
         val notification = NotificationCompat.Builder(mContext, CHANNEL_ID)
+            .setSmallIcon(R.drawable.logo_ok)
             .setContentTitle(TITLE)
             .setContentText(MESSAGE)
             .setOngoing(true)
             .build()
+        notificationManager.notify(NOTIFICATION_ID, notification)
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
             ForegroundInfo(NOTIFICATION_ID, notification, FOREGROUND_SERVICE_TYPE_LOCATION)
         else ForegroundInfo(NOTIFICATION_ID, notification)
